7b8113fd4282c43b275e4c825af1f7e9b0265fec
Victor Ruehle <ruehle@votca.org>
2012-04-02 17:07:50 +0100

    basin hopping class + ljscript

---
+++ basinhopping.py
@@ -0,0 +1,98 @@
+# -*- coding: iso-8859-1 -*-
+import numpy as np
+import scipy
+from math import *
+import numpy.random as RNG
+
+
+class BasinHopping:
+  def __init__(self, coords, potential, takestep, temperature=1.0, storage=None, manstep=None):
+    self.coords = coords
+    self.temperature = temperature
+    self.storage = storage
+    self.potential = potential
+    self.takestep = takestep
+    self.manstep = manstep
+
+  def run(self, nsteps):
+    #########################################################################
+    #do initial quench
+    #########################################################################
+    print "doing monteCarlo, nsteps", nsteps
+    potel = self.potential.getEnergy(self.coords)
+    print "initial energy", potel
+
+    if(self.storage):
+      self.storage(potel, self.coords)
+      
+    potel, V = self.potential.getEnergyGradient(self.coords)
+    print "max gradient", np.max(V), potel
+    print "minimizing initial coords"
+
+    newcoords, newE = self.quench(self.coords)
+    Equench, V = self.potential.getEnergyGradient(newcoords)
+    print "newcoords max V", np.max(V), Equench
+
+    coords = newcoords
+
+    if(self.storage):
+      self.storage(Equench, coords)
+    
+    for istep in xrange(nsteps):
+        print "step number ", istep
+        acceptstep, newcoords, Equench_new = self.mcStep(self.coords, Equench)
+        if self.manstep:
+          self.manstep.insertStep(acceptstep)
+        if acceptstep:
+            if(self.storage):
+              self.storage(Equench_new, newcoords)
+        self.coords = newcoords
+        Equench = Equench_new
+    
+  def quench(self, coords):
+    import scipy.optimize.lbfgsb
+    #newcoords, newE = steepest_descent.steepestDescent(potential.getEnergyGradient, coords, 100)
+    newcoords, newE, dictionary = scipy.optimize.fmin_l_bfgs_b(self.potential.getEnergyGradient, coords, iprint=-1, pgtol=1e-3)
+
+    V = dictionary["grad"]
+    funcalls = dictionary["funcalls"]
+    warnflag = dictionary['warnflag']
+    if warnflag > 0:
+        print "warning: problem with quench: ",
+        if warnflag == 1:
+            print "too many function evaluations"
+        else:
+            print dictionary['task']
+
+    print "quench: Ef=", newE, "steps=", funcalls, "max(V)=", np.max(np.abs(V))
+    return newcoords, newE
+    
+  def mcStep(self, coordsold, Equench_old):
+    """take one monte carlo basin hopping step"""
+    #########################################################################
+    #take step
+    #########################################################################
+    coords = coordsold.copy() #make  a working copy
+    self.takestep.takeStep(coords)
+
+    #########################################################################
+    #quench
+    #########################################################################
+    qcoords, Equench = self.quench (coords)
+
+    #########################################################################
+    #check whether step is accepted using metropolis algorithm.
+    #Use quenched energies.
+    #########################################################################
+    acceptstep = True
+    wcomp = (Equench - Equench_old)/self.temperature
+    w=min(1.0,exp(-wcomp))
+    rand = RNG.rand()
+    if (rand > w): acceptstep = False
+
+    print "mc step: Eo", Equench_old, "Ef", Equench, "accepted", acceptstep
+
+    if acceptstep:
+        return acceptstep, qcoords, Equench
+    else:
+        return acceptstep, coordsold, Equench_old
--- ljscript.py
+++ ljscript.py
@@ -1,27 +1,61 @@
 # -*- coding: iso-8859-1 -*-
 import numpy as np
 import potentials.lj as lj
-import mingmin
-import saveit
-import mingmin as gmin
-import adaptive_step
+import basinhopping as bh
 import take_step
+
+natoms = 12
+############################################################
+#Example 1: Simple basin hopping using defaults
+############################################################
+
+# random initial coordinates
+coords=np.random.random(3*natoms)
 
 potential = lj.LJ(1.0, 1.0, None)
 
+step = take_step.takeStep( stepsize=0.3)
+
+opt = bh.BasinHopping(coords, potential, step)
+opt.run(100)
+
+############################################################
+#Example 2: reading coords from file
+############################################################
 coords=np.loadtxt('coords')
-# avoid this is this really needed?
-natoms=coords.shape[0]
 coords = coords.reshape(coords.size)
 
-# run monte carlo
-savelowest = saveit.saveit()
-manstep = adaptive_step.manageStepSize (0.3, 0.5, 100)
-takeStep = take_step.takeStep( RNG = np.random.rand, getStep = manstep.getStepSize )
-# potential, coords, natoms, mcsteps, temperature, stepsize, equi steps, savelowest
-gmin.monteCarlo(potential, coords, natoms, 1000, 1.0, savelowest, manstep, takeStep)
+step = take_step.takeStep(  stepsize=0.3)
+opt = bh.BasinHopping(coords, potential, step)
 
-# print results
+opt.run(100)
+
+
+############################################################
+#Example 3: Saving the coordinates as an xyz file
+############################################################
+coords=np.random.random(3*natoms)
+import saveit
+
+storage = saveit.saveit()
+opt = bh.BasinHopping(coords, potential, step, storage=storage.insert)
+opt.run(100)
+
 with open("lowest", "w") as fout:
-  gmin.printxyz(fout,  savelowest.lowestcoords, natoms, savelowest.lowestE)
+  fout.write( str(natoms) + "\n")
+  fout.write( str(storage.lowestE) + "\n")
+  atoms=storage.lowestcoords.reshape(natoms, 3)  
+  for a in atoms: 
+      fout.write( "LA "+ str(a[0])+ " " +  str(a[1]) + " " + str(a[2]) + "\n" ) 
+
+############################################################
+#Example 4: adaptive step size
+############################################################
+
+import adaptive_step
+
+manstep = adaptive_step.manageStepSize (0.3, 0.5, 100)
+takeStep = take_step.takeStep( getStep = manstep.getStepSize )
+opt = bh.BasinHopping(coords, potential, takeStep)
+opt.run(100)
 
