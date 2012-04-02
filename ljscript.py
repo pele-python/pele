3794e51af95414f48ab13d147f27027a0250c76c
Victor Ruehle <ruehle@votca.org>
2012-04-02 14:53:15 +0100

    ljscript running again

--- ljscript.py
+++ ljscript.py
@@ -1,8 +1,11 @@
+# -*- coding: iso-8859-1 -*-
 import numpy as np
 import potentials.lj as lj
 import mingmin
 import saveit
 import mingmin as gmin
+import adaptive_step
+import take_step
 
 potential = lj.LJ(1.0, 1.0, None)
 
@@ -13,8 +16,10 @@ coords = coords.reshape(coords.size)
 
 # run monte carlo
 savelowest = saveit.saveit()
+manstep = adaptive_step.manageStepSize (0.3, 0.5, 100)
+takeStep = take_step.takeStep( RNG = np.random.rand, getStep = manstep.getStepSize )
 # potential, coords, natoms, mcsteps, temperature, stepsize, equi steps, savelowest
-gmin.monteCarlo(potential, coords, natoms, 1000, 1.0, 0.3, 100, savelowest)
+gmin.monteCarlo(potential, coords, natoms, 1000, 1.0, savelowest, manstep, takeStep)
 
 # print results
 with open("lowest", "w") as fout:
