32f2fbce261ed93e23bec53140ea94e5b8bc6c63
Jacob Stevenson <jstevenson131@gmail.com>
2012-04-03 00:32:28 +0100

    mykeywords.py can now return an initialized BasinHopping class

--- mingmin.py
+++ mingmin.py
@@ -56,43 +56,24 @@ def main():
     keys.setDefaults()
     with open("data", "r") as fin:
         keys.readKeywords(fin)
-    nstepsequil = 100
+
+
 
     #########################################################################
-    #initialize potential, etc
+    #initialize basin hopping class
     #########################################################################
-    if keys.potential == "lj":
-        import potentials.lj as lj
-        potential = lj.LJ(keys.eps, keys.sig, keys.boxl)
-    if keys.potential == "ljcpp":
-        import potentials.ljcpp as ljcpp
-        potential = ljcpp.LJ()
-    if keys.potential == "binary":
-        usefortran = True
-        if usefortran:
-            import potentials.ljpshiftfast as ljpshift
-        else:
-            import potentials.ljpshift as ljpshift
-        potential = ljpshift.LJpshift( natoms, keys.ntypeA, keys.boxl, keys.cutoff, keys.epsBB, keys.sigBB, keys.epsAB, keys.sigAB)
 
-    #########################################################################
-    #initialize basing hopping class
-    #########################################################################
-    savelowest = saveit.saveit() #class to save the lowest energy structure
-    manstep = adaptive_step.manageStepSize (keys.stepsize, keys.accrat, keys.accrat_frq) #class to do step size adjustment
-    takeStep = take_step.takeStep( RNG = np.random.rand, getStep = manstep.getStepSize ) #class to impliment the take step routine
+    #now initialize the basin hopping class
+    opt = keys.getBasinHopping( coords, natoms )
+
+    #optional ...
     #class to dump xyz files after each successful step
+    #add to list of event_after_step
     fdump = open("dump.q.xyz", "w")
     dumper = printWrapper(fdump)
-    event_after_step = [dumper.printIt]
-    #class to impiment acceptence criterion
-    metrop_test = metropolis.Metropolis(keys.temperature)
-    acceptTests=[metrop_test.acceptReject]
-    #now initialize the basin hopping class
-    opt = bh.BasinHopping(coords, potential, takeStep, storage = savelowest.insert, manstep = manstep, \
-            event_after_step=event_after_step, \
-            acceptTests=acceptTests, \
-            )
+    opt.event_after_step.append(dumper.printIt)
+
+    #now run basin hopping
     opt.run(keys.nmcsteps)
     fdump.close()
 
@@ -100,7 +81,7 @@ def main():
     #print results
     #########################################################################
     with open("lowest", "w") as fout:
-        printxyz(fout,  savelowest.lowestcoords, savelowest.lowestE)
+        printxyz(fout,  keys.savelowest.lowestcoords, keys.savelowest.lowestE)
 
 if __name__ == "__main__":
     main()
--- mykeyword.py
+++ mykeyword.py
@@ -1,10 +1,16 @@
+import numpy as np
+import basinhopping as bh
+import saveit
+import adaptive_step
+import take_step
+import metropolis
 
 class myKeywordClass():
     def __init__(self):
-        a=1
+        self.setDefaults()
 
     def setDefaults(self):
-        self.potential = "lj"
+        self.pot_type = "lj"
         self.sig = 1. 
         self.eps = 1.
         self.periodic = False
@@ -15,25 +21,70 @@ class myKeywordClass():
         self.accrat = 0.5
         self.accrat_frq = 50
 
+    def getBasinHopping(self, coords, natoms):
+        #########################################################################
+        #initialize potential, etc
+        #########################################################################
+        if self.pot_type == "lj":
+            import potentials.lj as lj
+            potential = lj.LJ(self.eps, self.sig, self.boxl)
+        if self.pot_type == "ljcpp":
+            import potentials.ljcpp as ljcpp
+            potential = ljcpp.LJ()
+        if self.pot_type == "binary":
+            usefortran = True
+            if usefortran:
+                import potentials.ljpshiftfast as ljpshift
+            else:
+                import potentials.ljpshift as ljpshift
+            potential = ljpshift.LJpshift( natoms, self.ntypeA, self.boxl, self.cutoff, self.epsBB, self.sigBB, self.epsAB, self.sigAB)
+
+        #initialize minima saving routine
+        self.savelowest = saveit.saveit() #class to save the lowest energy structure
+
+        #initialize adaptive step size routine
+        manstep = adaptive_step.manageStepSize (self.stepsize, self.accrat, self.accrat_frq) #class to do step size adjustment
+
+        #initialize step taking routine
+        takeStep = take_step.takeStep( RNG = np.random.rand, getStep = manstep.getStepSize ) #class to impliment the take step routine
+
+        #class to impiment acceptence criterion
+        metrop_test = metropolis.Metropolis(self.temperature)
+        acceptTests=[metrop_test.acceptReject]
+        #add further tests here, e.g    acceptTests.append( cold_fusion_test )
+
+        #add optional events
+        event_after_step = []
+
+        #initialize basing hopping class and return it
+        opt = bh.BasinHopping(coords, potential, takeStep, storage = self.savelowest.insert, manstep = manstep, \
+                event_after_step=event_after_step, \
+                acceptTests=acceptTests, \
+                )
+        return opt
+
     def readKeywords(self, fin):
         for line in fin:
+            self.addKeyword(line)
+
+    def addKeyword(self, line):
             line = line.lower() #put in lower case
             words = line.split()
             if words[0] == "comment":
-                continue
+                return
             elif line[0] == "#":
-                continue
+                return
 
             elif words[0] == "lj":
-                self.potential = "lj"
+                self.pot_type = "lj"
                 self.eps = float(words[1])
                 self.sig = float(words[2])
 
             elif words[0] == "ljcpp":
-                self.potential = "ljcpp"
+                self.pot_type = "ljcpp"
 
             elif words[0] == "blj":
-                self.potential = "blj"
+                self.pot_type = "blj"
                 self.ntypeA = int(words[1])
                 self.epsAB = float(words[2])
                 self.epsBB = float(words[3])
@@ -41,7 +92,7 @@ class myKeywordClass():
                 self.sigBB = float(words[5])
 
             elif words[0] == "binary":
-                self.potential = "binary"
+                self.pot_type = "binary"
                 self.ntypeA = int(words[1])
                 self.epsAB = float(words[2])
                 self.epsBB = float(words[3])
