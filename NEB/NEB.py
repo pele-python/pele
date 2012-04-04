15b3e4ce34a40c4e3449d2548e51b2cf22aaf486
Victor Ruehle <ruehle@votca.org>
2012-04-05 00:57:26 +0200

    cleanup & comments NEB

--- NEB/NEB.py
+++ NEB/NEB.py
@@ -2,31 +2,59 @@ import numpy as np
 
 def distance_cart(x1, x2):
     return x2 - x1
-         
+
 class NEB:
-    def __init__(self, initial, final, potential, distance=distance_cart, nimages=10, k=1.0):
+    """Nudged elastic band implementation
+
+    intial: 
+        first point of band
+        
+    final: last point of band
+    
+    distance  (distance_cart):
+        distance function for the elastic band
+    
+    nimages (10):
+        number of moving images for the band. The number includes 
+        the endpoints and must be bigger than 2
+        
+    k (1.0):
+        elastic constant for band
+        
+    """
+    def __init__(self, initial, final, potential, distance=distance_cart, nimages=10, k=20.0):
         self.distance = distance
         self.potential = potential
         self.k = k
+        self.nimages = nimages
         
         #initialiye coordinate&gradient array
         self.coords = np.zeros([nimages, initial.size])
-        self.grad = np.zeros([nimages, initial.size])
+        self.grad = np.zeros([nimages-2, initial.size])
         
         #interpolate initial points
         self.interpolate(initial, final, nimages)        
         
         # copy initial and final structure
-        self.initial = initial
-        self.final = final
+        self.coords[0,:] = initial
+        self.coords[-1,:] = final
         
+        # the active range of the coords, endpoints are fixed
+        self.active = self.coords[1:nimages-1,:]  
     
     # do a stupid steepest descent, have to add scipy interface for minimizer first
+    """
+        Optimize the band
+    
+        quench (None):
+            quench algorithm to use for optimization. If None is given,
+            default_quench is used,
+    """
     def optimize(self, quench=None):
         if(quench==None):
             quench = self.default_quench
-        tmp,E = quench(self.coords)
-        self.coords = tmp.reshape(self.coords.shape)
+        tmp,E = quench(self.active)
+        self.active[:,:] = tmp.reshape(self.active.shape)
         
     def default_quench(self, coords):
         import scipy.optimize.lbfgsb
@@ -43,41 +71,44 @@ class NEB:
                 print dictionary['task']            
         return newcoords, newE
 
+    # Calculate gradient for the while NEB
     def getEnergyGradient(self, coords1d):
-        coords = coords1d.reshape([10,coords1d.size/10])
+        # make array access a bit simpler
+        tmp = self.coords.copy()
+        tmp[1:self.nimages-1,:] = coords1d.reshape(self.active.shape)
+        grad = self.grad.copy()
         E = 0.
-        for i in xrange(0, 10):
-            pl = self.initial
-            pr = self.final
-            if i != 0:
-                pl = coords[i - 1, :]
-            if i != 9:
-                pr = coords[i + 1, :]
-            Ei, g = self.NEBForce(coords[i, :], pl, pr)
-            self.grad[i,:]=g
-            print Ei
+        # build forces for all images
+        for i in xrange(1, self.nimages-1):
+            Ei, g = self.NEBForce(tmp[i, :], tmp[i-1, :], tmp[i+1, :])
+            grad[i-1,:] = g
             E += Ei
-        return E,self.grad.reshape(self.grad.size)
+        return E,grad.reshape(self.grad.size)
             
     # update force for one image
     def NEBForce(self, p, pl, pr):
+            # construct tangent vector, TODO: implement newer method
             d1 = p - pl
             d2 = pr - p
             t = d1 / np.linalg.norm(d1) + d2 / np.linalg.norm(d2)
             t = t / np.linalg.norm(t)
             
+            # get real gradient for image
             E, g = self.potential.getEnergyGradient(p)
             
+            # project out parallel part
             gpar = g - np.dot(g, t) * t
-            gspring = np.dot(self.k * (d2 - d1), t) * t
-            print "Energy:", E
+            # calculate parallel spring force and energy
+            gspring = self.k * (np.linalg.norm(d2) - np.linalg.norm(d1)) * t
+            E+=0.5*self.k*(np.linalg.norm(d1)-np.linalg.norm(d2))**2
+            
             return E, -(gpar + gspring)
     
     # initial interpolation    
     def interpolate(self, initial, final, nimages):
-        delta = (final - initial) / (nimages + 1)
-        for i in xrange(0, nimages):
-            self.coords[i, :] = initial + delta * (i + 1)
+        delta = (final - initial) / (nimages-1)
+        for i in xrange(1, nimages):
+            self.coords[i, :] =  initial + delta * i
 
             
 import nebtesting as test
@@ -94,13 +125,13 @@ if __name__ == "__main__":
     print "done"
     #pl.imshow(z)
     #pl.show()
-    initial = np.array([1., 2.]) #np.random.random(3)
-    final = np.array([2., .7]) #np.random.random(3)
+    initial = np.array([.75, 2.]) #np.random.random(3)
+    final = np.array([2., .75]) #np.random.random(3)
     print "Initial: ", initial
     print "Final: ", final
     #pl.imshow(z)
     
-    neb = NEB(initial, final, potential)
+    neb = NEB(initial, final, potential, nimages=20)
     tmp = neb.coords
     
     pl.contourf(x, y, z)
@@ -110,7 +141,7 @@ if __name__ == "__main__":
 
         
     tmp = neb.coords
-    pl.plot(tmp[:, 0], tmp[:, 1], 'rx-')
+    pl.plot(tmp[:, 0], tmp[:, 1], 'ro-')
     pl.show()
     
         
