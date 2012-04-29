34857178c2349e0c5bebd1a8222a04463fc79db1
Jacob Stevenson <jstevenson131@gmail.com>
2012-04-29 15:36:59 +0100

    clean up and document sandbox

--- src/potentials/rigid_bodies/molecule.py
+++ src/potentials/rigid_bodies/molecule.py
@@ -3,7 +3,7 @@ import rotations as rot
 from potentials.fortran.rmdrvt import rmdrvt as rotMatDeriv
 
 def vec2aa( v2, v1 = np.array( [0.,0.,1.]) ):
-    """ 
+    """
     return the angle-axis rotation which transforms
     v1 into v2
     """
@@ -11,7 +11,7 @@ def vec2aa( v2, v1 = np.array( [0.,0.,1.]) ):
     vperp = -np.cross(v1,v2)
     aa = vperp / np.linalg.norm(vperp) * np.dot(v1,v2)
     return aa
-    
+
 
 class Site:
     """
@@ -25,17 +25,17 @@ class Site:
         self.drdp = np.zeros([3,3])
         #self.aa = vec2aa( position )
         #self.rotation_matrix = rot.aa2mx(self.aa)
-        
+
 
 
 class Molecule:
     """
-    this class defines a type of rigid body
+    this class defines a rigid body molecule
     """
     def __init__(self):
         self.sitelist=[]
         self.nsites = 0
-        
+
         self.aa = np.zeros(3)
         self.com = np.zeros(3)
         self.rotation_mat = np.zeros([3,3])
@@ -45,20 +45,27 @@ class Molecule:
 
 
 
-    
+
     def insert_site(self, type, position):
+        """
+        add a site to the definition of the molecule
+        """
         self.sitelist.append( Site(type, position) )
         self.nsites += 1
-    
+
     def getxyz(self, aa=np.array([0.,0.,1e-6])):
+        """return the xyz positions of all sites in the molecule-frame"""
         nsites = len(self.sitelist)
         xyz = np.zeros(nsites*3, np.float64)
         mx = rot.aa2mx(aa)
         for i,site in enumerate(self.sitelist):
             xyz[i*3:i*3+3] = np.dot(mx, site.position)
         return xyz
-    
+
     def update(self, com, aa, do_derivatives = True):
+        """
+        Update the position and orientation of the molecule and things dependent on these.
+        """
         self.aa[:] = aa
         self.com[:] = com
         self.rotation_mat, self.drmat[0], self.drmat[1], self.drmat[2] = rotMatDeriv(aa, do_derivatives)
@@ -66,12 +73,17 @@ class Molecule:
             site.abs_position = np.dot( self.rotation_mat, site.position ) + self.com
             for k in range(3):
                 site.drdp[k,:] = np.dot(self.drmat[k], site.position)
+
+        #zero interaction dependent things
         self.E = 0.
         self.comgrad[:] = 0.
         self.aagrad[:] = 0.
 
 
 def setupLWOTP():
+    """
+    return a molecule of OTP (ortho terphenyl) using the model by Lewis and Wahnstrom
+    """
     from numpy import sin, cos, pi
     pos1 = [0.0, -2./3 * np.sin( 7.*pi/24.), 0.0]
     pos2 = [cos( 7.*pi/24.),  1./3. * sin( 7.* pi/24.), 0.0]
@@ -81,19 +93,19 @@ def setupLWOTP():
     otp.insert_site(0, pos2 )
     otp.insert_site(0, pos3 )
     return otp
-                
-            
+
+
 def test_molecule():
     from numpy import sin, cos, pi
     otp = setupLWOTP()
-        
+
     xyz = otp.getxyz()
     from printing.print_atoms_xyz import printAtomsXYZ as printxyz
     import sys
     #with open("out.xyz", "w") as fout:
     printxyz(sys.stdout, xyz)
-    
-    
-    
+
+
+
 if __name__ == "__main__":
     test_molecule()
--- src/potentials/rigid_bodies/sandbox.py
+++ src/potentials/rigid_bodies/sandbox.py
@@ -8,12 +8,12 @@ import copy
 
 class RBSandbox(potential):
     """
-    a system of molecule_list
+    Defines a system of rigid body molecules
     """
     def __init__(self, molecule_list, interaction_matrix):
         """
         molecule_list:  a list of Molecule objects that define the system
-        
+
         interaction_matrix:  a matrix that defines the interactions between site types.
             inter = interaction_matrix[site1.type][site2.type] is the interaction between site1 and site2
             inter.getEnergy(coords) returns the energy of the interaction
@@ -21,7 +21,7 @@ class RBSandbox(potential):
         self.molecule_list = [copy.deepcopy(mol) for mol in molecule_list]
         self.interaction_matrix = interaction_matrix
         self.nmol = len(self.molecule_list)
-        
+
         self.nsites = 0
         self.typelist = []
         self.nsites_cum = np.zeros(self.nmol, np.int)
@@ -30,20 +30,21 @@ class RBSandbox(potential):
             self.nsites += mol.nsites
             for site in mol.sitelist:
                 self.typelist.append( site.type )
-        
+
+        self.oldcoords = np.zeros(3*2*self.nmol)
+
     def getxyz(self, coords):
-        nmol = self.nmol
+        """convert center of mass + angle-axis coords into xyz coordinates"""
+        self.update(coords)
         xyz = np.zeros(self.nsites*3)
-        for imol, mol in enumerate(self.molecule_list):
-            molxyz = mol.getxyz( coords[3*nmol + imol*3 : 3*nmol + imol*3+3] )
-            for j in range(mol.nsites):
-                molxyz[j*3:j*3+3] += coords[imol*3 : imol*3+3]
-            
-            k = self.nsites_cum[imol]*3
-            xyz[k:k+mol.nsites*3] = molxyz
+        isite = 0
+        for mol in self.molecule_list:
+            for site in mol.sitelist:
+                xyz[isite*3 : isite*3 + 3] = site.abs_position
+                isite += 1
         return xyz
-    
-    def molmolEnergy(self, imol1, imol2, xyz):
+
+    def molmolEnergyOld(self, imol1, imol2, xyz):
         E = 0.
         mol1 = self.molecule_list[imol1]
         mol2 = self.molecule_list[imol2]
@@ -63,25 +64,48 @@ class RBSandbox(potential):
 
         return E
 
-    
-    def getEnergy(self, coords):
-        xyz = self.getxyz(coords)
+    def molmolEnergy(self, mol1, mol2):
+        """
+        return the energy of interaction between mol1 and mol2.
+
+        don't update the molecules
+        """
         E = 0.
-        for imol1 in range(self.nmol):
-            for imol2 in range(0,imol1):
-                E += self.molmolEnergy(imol1, imol2, xyz)            
+        coords2 = np.zeros(6, np.float64)
+        for site1 in mol1.sitelist:
+            coords2[0:3] = site1.abs_position
+            for site2 in mol2.sitelist:
+                coords2[3:6] = site2.abs_position
+                #print coords2
+                interaction = self.interaction_matrix[site1.type][site2.type]
+                dE = interaction.getEnergy(coords2)
+                E += dE
         return E
 
 
-    
+    def getEnergy(self, coords):
+        self.update(coords)
+        E = 0.
+        for imol1 in range(self.nmol):
+            mol1 = self.molecule_list[imol1]
+            for imol2 in range(0,imol1):
+                mol2 = self.molecule_list[imol2]
+                E += self.molmolEnergy(mol1, mol2)
+        return E
+
+
+
     def molmolEnergyGradient(self, mol1, mol2):
+        """
+        Update the energy and gradients on mol1 and mol2
+        """
         E = 0.
         coords2 = np.zeros(6, np.float64)
         for i1, site1 in enumerate(mol1.sitelist):
-            coords2[0:3] = site1.abs_position 
+            coords2[0:3] = site1.abs_position
             for i2, site2 in enumerate(mol2.sitelist):
                 coords2[3:6] = site2.abs_position
-                #print coords2 
+                #print coords2
                 interaction = self.interaction_matrix[site1.type][site2.type]
                 dE, V2 = interaction.getEnergyGradient(coords2)
                 E += dE
@@ -93,10 +117,25 @@ class RBSandbox(potential):
                 #calculate drdp
                 mol1.aagrad += np.dot( site1.drdp, V2[0:3])
                 mol2.aagrad += np.dot( site2.drdp, V2[3:6])
-                
 
+    def coords_compare(self, coords):
+        """ return true if coords is the same as oldcoords"""
+        maxdif = np.max( np.abs(coords - self.oldcoords))
+        return maxdif < 1e-16
+            #print "maxdif", maxdif, coords[0:3]
+            #return True
+        #return False
 
     def update(self, coords):
+        """
+        update the com and angle-axis coords and dependents on all molecules
+
+        only do it if coords is different.
+        """
+        #using coords_compare makes quench almost always fail for some reason.
+        #if self.coords_compare( coords):
+            #return
+        self.oldcoords[:] = coords
         nmol= self.nmol
         for imol, mol in enumerate(self.molecule_list):
             com = coords[         imol*3 :          imol*3 + 3]
@@ -107,6 +146,11 @@ class RBSandbox(potential):
 
 
     def getEnergyGradient(self, coords):
+        """
+        return energy and gradient
+
+        update energy and gradient on all the molecules
+        """
         nmol = self.nmol
         self.update(coords)
         Etot = 0.
@@ -118,83 +162,85 @@ class RBSandbox(potential):
                 mol2 = self.molecule_list[imol2]
                 self.molmolEnergyGradient(mol1, mol2)
         for imol, mol in enumerate(self.molecule_list):
-            Etot += mol.E 
+            Etot += mol.E
             grad[         3*imol :          3*imol + 3] = mol.comgrad
             #print "aagrad", mol.aagrad
             grad[3*nmol + 3*imol : 3*nmol + 3*imol + 3] = mol.aagrad
         Etot /= 2.
         return Etot, grad
-        
 
 
-def test_sandbox():
+
+def test_sandbox(nmol = 4):
     from numpy import sin, cos, pi
     import copy
     from potentials.lj import LJ
+
+    #define the molecule types.
+    #here use only one type, LWOTP
+    otp = molecule.setupLWOTP()
+
+
+    # define the interaction matrix for the system.
+    # for LWOTP there is only one atom type, so this is trivial
     lj = LJ()
     interaction_matrix = [[lj]]
 
-    
-    otp = molecule.setupLWOTP()
-        
-    xyz = otp.getxyz()
-    from printing.print_atoms_xyz import printAtomsXYZ as printxyz
-    import sys
-    #with open("out.xyz", "w") as fout:
-    printxyz(sys.stdout, xyz)
-    
-    nmol = 4
+
+    #set up a list of molecules
     mols = [otp for i in range(nmol)]
+
+    #set up the RBSandbox object
     mysys = RBSandbox(mols, interaction_matrix)
     nsites = mysys.nsites
-    comcoords = np.random.uniform(-1,1,[nmol*3]) * 1.3*(nsites)**(1./3)
-    aacoords = np.array( [copy.copy(rot.random_aa()) for i in range(nmol)] )
-    print comcoords
-    print aacoords
-    aacoords = aacoords.reshape(3*nmol)
-    print aacoords
-    coords = np.zeros(2*3*nmol, np.float64)
-    print "lencoords, len aacoords", len (coords), len(aacoords), len(comcoords)
 
+    #get an initial set of coordinates
+    comcoords = np.random.uniform(-1,1,[nmol*3]) * 1.*(nsites)**(1./3)
+    aacoords = np.array( [copy.copy(rot.random_aa()) for i in range(nmol)] )
+    aacoords = aacoords.reshape(3*nmol)
+    coords = np.zeros(2*3*nmol, np.float64)
     coords[0:3*nmol] = comcoords[:]
     coords[3*nmol:2*3*nmol] = aacoords[:]
     print "lencoords, len aacoords", len (coords), len(aacoords), len(comcoords)
-    
-    xyz = mysys.getxyz( coords )
-    
-    printlist = []
-    printlist.append( (xyz.copy(), "initial"))
-    
-            
-    E = mysys.getEnergy(coords)
-    print "energy", E
-    
-    numericV = mysys.NumericalDerivative(coords, 1e-4)
-    print "numeric V", numericV
-    
-    E, V = mysys.getEnergyGradient(coords)
-    print "energy from gradient", E
-    print "analytic gradient", V
-    print "max error in gradient", np.max(np.abs(V - numericV))
-    print "exiting"
 
-    
+
+    #save the initial set of coords
+    printlist = []
+    xyz = mysys.getxyz( coords )
+    printlist.append( (xyz.copy(), "initial"))
+
+    #calculate the initial energy
+    Einit = mysys.getEnergy(coords)
+    print "initial energy", Einit
+
+    #test the gradient
+    numericV = mysys.NumericalDerivative(coords, 1e-4)
+    numericV = numericV.copy()
+    print "numeric V", numericV
+
+    E, V = mysys.getEnergyGradient(coords)
+    print "energy from gradient", E, "difference", E - Einit
+    #print "analytic gradient", V
+    print "max error in gradient", np.max(np.abs(V - numericV))
+
+    #do a quench to make sure everything is working well
     import quench
-    coords, E, rms, funcalls = quench.quench(coords, mysys.getEnergyGradient, iprint=1)
-    print "postquench", E, rms, funcalls
-    print coords
+    coords, E, rms, funcalls = quench.quench(coords, mysys.getEnergyGradient, iprint=-1)
+    print "postquench E", E, "rms", rms, "funtion calls", funcalls
     xyz = mysys.getxyz(coords )
     printlist.append( (xyz.copy(), "post quench"))
-    
 
-    
-    #from printing.print_atoms_xyz import printAtomsXYZ as printxyz
-    with open("otp.xyz", "w") as fout:
+
+    #print the saved coords
+    fname = "otp.xyz"
+    print "saving xyz coords to", fname
+    from printing.print_atoms_xyz import printAtomsXYZ as printxyz
+    with open(fname, "w") as fout:
         for xyz, line2 in printlist:
-            printxyz( fout, xyz, line2=line2)
+            printxyz( fout, xyz, line2=line2, atom_type=["N", "O", "O"])
 
-    
-    
+
+
 if __name__ == "__main__":
     test_sandbox()
-    
+
