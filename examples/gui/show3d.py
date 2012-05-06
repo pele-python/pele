b252c0c7c34775d62c2aa4cecbdac298ec7d7915
Victor Ruehle <ruehle@votca.org>
2012-05-06 22:36:29 +0200

    played with light settings

--- examples/gui/show3d.py
+++ examples/gui/show3d.py
@@ -36,8 +36,14 @@ class Show3D(QGLWidget):
         '''
         
     	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-    	color = [1.0,0.,0.,1.]
-        glMaterialfv(GL_FRONT,GL_DIFFUSE,color)
+    	color = [0.65,0.0,0.0,1.]
+        amb = [0.,0.0,0.0,1.]
+        spec=[1.0,1.,1.,1]
+        shine=80.
+        glMaterialfv(GL_FRONT_AND_BACK,GL_DIFFUSE,color)
+        glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT,amb)
+        glMaterialfv(GL_FRONT_AND_BACK,GL_SHININESS,shine)
+        glMaterialfv(GL_FRONT_AND_BACK,GL_SPECULAR,spec)
         glMatrixMode(GL_MODELVIEW)
         glPushMatrix()
         glRotate(self.rotation[0], 1., 0.,0.)
@@ -48,7 +54,7 @@ class Show3D(QGLWidget):
             x=xx-com
             glPushMatrix()            
             glTranslate(x[0],x[1],x[2])
-    	    glutSolidSphere(0.5,40,40)
+    	    glutSolidSphere(0.5,100,100)
     	    glPopMatrix()
         glPopMatrix()	
         glFlush()
@@ -81,14 +87,27 @@ class Show3D(QGLWidget):
     	glEnable(GL_CULL_FACE)
     	glEnable(GL_DEPTH_TEST)
     	glEnable(GL_LIGHTING)
-    	lightZeroPosition = [10.,4.,10.,1.]
-    	lightZeroColor = [0.8,1.0,0.8,1.0] #green tinged
+    	lightZeroPosition = [-80.,50.,150.,1.]
+    	lightZeroColor = [1.0, 1.,1.,1.0] #green tinged
     	glLightfv(GL_LIGHT0, GL_POSITION, lightZeroPosition)
     	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightZeroColor)
-    	glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.1)
-    	glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.05)
+        glLightfv(GL_LIGHT0, GL_AMBIENT, [0.5,0.5,0.5,1.0])
+        
+        
+        glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 1.000)
+    	#glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.01)
     	glEnable(GL_LIGHT0)
-    	glMatrixMode(GL_PROJECTION)
+    	
+        glLightfv(GL_LIGHT1, GL_POSITION, [10.,20.,-2.,1.])
+        glLightfv(GL_LIGHT1, GL_DIFFUSE, [1.,1.,1.,1.0])
+        glLightfv(GL_LIGHT1, GL_SPECULAR, [1.0,1.0,1.0,1.0])
+        glLightfv(GL_LIGHT1, GL_AMBIENT, [0.,0.,0.,1.0])
+        glLightf(GL_LIGHT1, GL_CONSTANT_ATTENUATION, 1)
+        #glLightf(GL_LIGHT1, GL_LINEAR_ATTENUATION, 0.01)
+        glEnable(GL_LIGHT1)
+        
+        glShadeModel(GL_SMOOTH);
+        glMatrixMode(GL_PROJECTION)
     	gluPerspective(40.,1.,1.,40.)
     	glMatrixMode(GL_MODELVIEW)
     	gluLookAt(0,0,10,
