774bbf9e49fa0d96e857f6011feb85e02da874dc
Victor Ruehle <ruehle@votca.org>
2012-05-13 01:51:55 +0200

    ewald compiles

--- src/potentials/salt_/ewald.h
+++ src/potentials/salt_/ewald.h
@@ -4,6 +4,7 @@
 #include <complex>
 #include "vec.h"
 
+using namespace votca::tools;
 
 /**
  * \brief Class for ewald electrostatics
@@ -12,6 +13,8 @@
  * interactions using ewald summation. Iterating over pairs for
  * the real space part has to be done by user, summing over
  * PairEnergy for all pairs.
+ *
+ *
  */
 class Ewald
 {
@@ -23,20 +26,24 @@ public:
 	/**
 	 * Calculate the K-space contribution of the energy
 	 *
+	 * \f[
+	 *   E^{(k)} = \frac{1}{2V} \sum\limits_{k \neq 0} \frac{4\pi}{k^2}e^{-k^2/4\alpha^2} | S({\bf k}) |^2
+	 * \f]
+	 *
 	 * \param charges container/adapter for charge iteration
 	 */
-	template<container>
-	double EnergyKSpace(container &charges);
+	template<typename container>
+	double EnergyKSpace(container &charges) const;
 
 	/**
 	 * Calculate real space energy for a pair
 	 */
-	double pair_energy(const vec3 &r_ij) const;
+	double PairEnergy(double r, double q) const;
 
 	/**
 	 * Calculate reals space gradient for a pair
 	 */
-	double pair_gradient(const vec3 r_ij, vec3 &g) const;
+	//double pair_gradient(const vec r_ij, vec &g) const;
 
 	/**
 	 * set the box size
@@ -45,31 +52,43 @@ public:
 	 * \param b box vector b
 	 * \param c box vector c
 	 */
-	void setBox(vec3 a, vec3 b, vec3 c);
+	void setBox(vec a, vec b, vec c);
 
 protected:
-	double _eps0;
+	double _alpha;
 	double _sigma;
 
 	// reciprocal lattice vectors
 	vec _ar, _br, _cr;
+	double _lmax, _mmax, _nmax;
 	// box volume
 	double _V;
 
 	/**
 	 * Calculate structure factor
+	 *
+	 * \f[
+	 *   S({\bf k}) = \sum\limits_{j=1}^N q_j e^{i{\bf k} \dot {\bf r_j}}
+	 * \f]
 	 */
-	template<container>
+	template<typename container>
 	std::complex<double> S(const vec &k, container &charges) const;
 
-	template<container>
-	double SelfEnergy(container &charges);
+	/**
+	 * Calculate self energy
+	 *
+	 * \f[
+	 *   E^{(s)} = - \frac{\alpha}{\sqrt{\pi}} \sum\limits_i q_i^2
+	 * \f]
+	 */
+	template<typename container>
+	double SelfEnergy(container &charges) const;
 };
 
 inline Ewald::Ewald()
-	: _sigma(0.), _eps0(1.0), _V(0.0) {}
+	: _alpha(1.), _V(0.0) {}
 
-inline void Ewal::setBox(vec3 a, vec3 b, vec3 c)
+inline void Ewald::setBox(vec a, vec b, vec c)
 {
 	_V = abs((a^b)*c);
 	_ar = b^c / _V;
@@ -78,54 +97,55 @@ inline void Ewal::setBox(vec3 a, vec3 b, vec3 c)
 }
 
 
-inline double Ewald::PairEnergy(double r, double q)
+inline double Ewald::PairEnergy(double r, double q) const
 {
-	double z = r/(sqrt(2.)*_sigma);
-	return 1./(4.*M_PI*_eps0)*q/r*erfc(z);
+	double z = _alpha * r;
+	return q/r*erfc(z);
 }
 
-template<container>
-inline double Ewald::EnergyKSpace(container &charges)
+template<typename container>
+inline double Ewald::EnergyKSpace(container &charges) const
 {
 	vec a,b,c;
 	double E = 0;
 	double V;
 
-	for(int l=0; l<lmax; ++l) {
-		for(int m=0; m<mmax; ++m) {
-			for(int n=0; n<nmax; ++n) {
+	for(int l=0; l<_lmax; ++l) {
+		for(int m=0; m<_mmax; ++m) {
+			for(int n=0; n<_nmax; ++n) {
 				vec k = double(l)*a + double(m)*b + double(n)*c;
 				double k2 = k*k;
-				std::complex<double> S=S(k, charge);
+				std::complex<double> S=S(k, charges);
 				double S2 = S.real()*S.real() + S.imag()*S.imag();
-				E+= exp(-0.5*_sigma*_sigma *k2) * S2 / k2;
+				E+= exp(-0.25*k2/(_alpha*_alpha)) / k2 * S2;
 			}
 		}
 	}
-	E = E/(2.*V*_eps0) - SelfEnergy(charges);
+	E = 2.*M_PI/V*E - SelfEnergy(charges);
 }
 
 
-template<container>
+template<typename container>
 inline std::complex<double> Ewald::S(const vec &k, container &charges) const
 {
 	std::complex<double> S(0.,0.);
-	for(container::iterator crg=charges.begin();
+	for(typename container::iterator crg=charges.begin();
 			crg!=charges.end();
 			++crg) {
-		S.real()+=crg.q()*exp(complex<double(0.,k*crg.r()));
+		S.real()+=crg.q()*exp(complex<double>(0.,-k*crg.r()));
 	}
 	return S;
 }
 
-template<container>
-inline double SelfEnergy(container &charges)
+template<typename container>
+inline double Ewald::SelfEnergy(container &charges) const
 {
 	double E=0;
-	for(container::iterator crg=charges.begin();
+	for(typename container::iterator crg=charges.begin();
 		crg!=charges.end(); ++crg) {
 		E+=crg.q()*crg.q();
 	}
-	return E/(3.*sqrt(M_PI)*_eps0*_sigma)/sqrt(2.*M_PI);
+	return E*_alpha/sqrt(M_PI);
 }
+
 #endif
