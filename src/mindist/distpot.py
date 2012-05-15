9fd2a324342c4da4657118d676cca4c782bfc38a
Jacob Stevenson <jstevenson131@gmail.com>
2012-05-15 09:24:53 +0100

    Add analytical gradients for distpot.
    I used the rigid body framework to get the gradients in angle-axis
    coordinates.

--- src/mindist/distpot.py
+++ src/mindist/distpot.py
@@ -14,7 +14,24 @@ def overlap_slow( XA, XB, L2, atomlist, nlist = [] ):
             E += np.exp(-r2/L2)
     return E
 
+def overlap_gradient_slow( XA, XB, L2, atomlist, nlist = [] ):
+    E = 0.
+    grad = np.zeros(len(XA))
+    iL2 = 1./L2
+    for i1 in atomlist:
+        i = i1*3
+        for j1 in atomlist:
+            j = j1*3
+            dr = (XB[i:i+3] - XA[j:j+3])
+            r2 = np.sum( dr**2 )
+            de = np.exp(-r2/L2)
+            E += de
+            grad[ i:i+3 ] -= dr[:] * (de * 2.0 * iL2)
+            #grad[ :j+3 ] += dr[:] * (de * 2.0 * iL2)
+    return E, grad
+
 def overlap_fast( XA, XB, L2, atomlist, nlist = [] ):
+    print "using victor's overlap"
     xa = XA.reshape(XA.size/3,3)[atomlist,:]
     xb = XB.reshape(XB.size/3,3)[atomlist,:]
     return np.sum(np.exp(-np.sum((xa[:,np.newaxis] - xb[:])**2, axis=2)/L2))
@@ -28,10 +45,8 @@ class MinPermDistPotential(potential.potential):
 
     E = - sum_i sum_j exp( -|x_Ai - X_Bj|**2 / L**2 )
 
-    There are other options for the overlap which are not implimented.  For
+    There are other options for the overlap which are not implemented.  For
     instance something with a slower decay than exp(-R**2).
-
-    TODO: impliment analytical Gradients
     """
     def __init__(self, XA, XB, L=0.2, permlist = []):
         self.XA0 = copy.copy(XA)
@@ -43,13 +58,27 @@ class MinPermDistPotential(potential.potential):
         if len(self.permlist) == 0:
             self.permlist = [range( self.nsites)]
         try:
-            from overlap import overlap
+            from overlap import overlap, overlap_gradient
             self.overlap = overlap
+            self.overlap_gradient = overlap_gradient
         except:
+            #self.overlap = overlap_fast
             self.overlap = overlap_fast
+            self.overlap_gradient = overlap_gradient_slow
             print "Using python energy calculation. Compile overlap.f90 to speed things up (a little)"
-        
-    def getEnergy(self, AA ):
+
+        self.setUpRigidBodies()
+
+    def setUpRigidBodies(self):
+        #set up one rigid body
+        from potentials.rigid_bodies.molecule import Molecule
+        self.rb = Molecule()
+        for atomlist in self.permlist:
+            #make a rigid body from atomlist
+            for i in atomlist:
+                self.rb.insert_site( 0, self.XB0[i*3:i*3+3] )
+
+    def getEnergy_no_rigid_body(self, AA ):
         # Rotate XB0 according to angle axis AA
         rot_mx = rot.aa2mx( AA )
         for j in range(self.nsites):
@@ -63,6 +92,36 @@ class MinPermDistPotential(potential.potential):
             #   print E, atomlist, [self.nsites, len(atomlist)], len(self.XA0), len(self.XB)
         return E
 
+    def getEnergy(self, AA):
+        # Rotate rigid body according to angle axis AA
+        self.XB = self.rb.getxyz( aa = AA )
+        #get energy and gradient for each site
+        E = 0.
+        for atomlist in self.permlist:
+            E -= self.overlap( self.XA0, self.XB, self.L2, atomlist, [self.nsites, len(atomlist)])
+        return E
+
+
+    def getEnergyGradient(self, AA):
+        # Rotate XB0 according to angle axis AA
+        # Rotate rigid body according to angle axis AA
+        self.XB = self.rb.getxyz( aa = AA )
+        #get energy and gradient for each site
+        E = 0.
+        grad = np.zeros(len(self.XB))
+        for atomlist in self.permlist:
+            de, dg = self.overlap_gradient( self.XA0, self.XB, self.L2, atomlist, [self.nsites, len(atomlist)])
+            E -= de
+            grad -= dg
+        #convert site-gradients into angle -axis gradients
+        #first calculate the rotation matrix and derivatives and drdp for each site
+        self.rb.update_coords( np.zeros(3), AA )
+        aagrad = np.zeros(3)
+        for i,site in enumerate( self.rb.sitelist ):
+            aagrad += np.dot( site.drdp, grad[i*3:i*3+3] )
+        return E, aagrad
+
+
     def globalEnergyMin(self):
         """
         return the lowest energy theoretically possible.  This will happen if XB == XA
@@ -73,9 +132,46 @@ class MinPermDistPotential(potential.potential):
         return E
 
 
-
 def random_rotation( aa):
     aanew = rot.random_aa()
     #print "aanew ", aanew
     aa[:] = aanew[:]
     #print "aa    ", aa
+    
+
+def test_distpot():
+    #define two structures
+    natoms = 12
+    X1 = np.random.uniform(-1,1,[natoms*3])*(float(natoms))**(1./3)
+    
+    X2 = np.random.uniform(-1,1,[natoms*3])*(float(natoms))**(1./3)
+    #make X2 a rotation of X1
+    print "testing with", natoms, "atoms, with X2 a rotated and permuted isomer of X1"
+    aa = rot.random_aa()
+    rot_mx = rot.aa2mx( aa )
+    for j in range(natoms):
+        i = 3*j
+        X2[i:i+3] = np.dot( rot_mx, X1[i:i+3] )
+    #import random, mindistutils
+    #perm = range(natoms)
+    #random.shuffle( perm )
+    #print perm
+    #X2 = mindistutils.permuteArray( X2, perm)
+
+    pot = MinPermDistPotential(X1, X2, L = .2)
+    
+    aa = rot.random_aa()
+    e = pot.getEnergy(aa)
+    print "energy", e
+    de, dg = pot.getEnergyGradient(aa)
+    print "energy from gradient", de, "diff", e-de
+    
+    den, dgn = pot.getEnergyGradientNumerical(aa)
+    maxgrad= np.max( np.abs( dg ) )
+    maxdiff = np.max( np.abs( dg - dgn ) )
+    print "maximum gradient", maxgrad
+    print "max difference in analytical vs numerical gradient", maxdiff
+
+
+if __name__ == "__main__":
+    test_distpot()
