fb2b34ceca9c7f05a2f1005f7fa006016302aa75
Jacob Stevenson <jstevenson131@gmail.com>
2012-05-17 00:38:38 +0100

    Make BasinHopping a derrived class of a new MonteCarlo class

--- src/mc.py
+++ src/mc.py
@@ -1,134 +1,142 @@
 # -*- coding: iso-8859-1 -*-
 import numpy as np
-import scipy
 from math import *
 import sys
 import accept_tests.metropolis as metropolis
 import copy
-import optimize.quench as quench
-from mc import MonteCarlo
 
-class BasinHopping(MonteCarlo):
-    """A class to run the basin hopping algorithm
 
+class MonteCarlo:
+    """A class to run the monte carlo algorithm
+    
       coords: 
           The initial set of coordinates.  A one dimensional list or numpy array
-
+  
       potential: 
           A class implimenting the potential.  The class must have the
           following functions implimented
-
+  
           energy = potential.getEnergy( coords )
           energy, gradient = potential.getEnergyGradient( coords )
-
+  
       takeStep: 
           The function which randomly perterbs the system, e.g. random
           dispacement.  It takes the form
-
+  
           takeStep(coords)
-
+  
       acceptTests:  ([]) 
           An optional list of functions which return False if a quench should be
           rejected.  The Metropolis test is added to this list by default unless
           the input "nometropolis" is set to False. Each test in the list takes
           the form
-
+   
           accept = test(Eold, Enew, new_coords):
-
+  
       temperature:  (1.0)
           The temperature used in the metropolis criterion.  If no temperature is
           passed, the default 1.0 is used unless the flag "nometropolis" is set
           to False
-
+  
       nometropolis: (False)
           Flag to disable the Metropolis accept reject test.
-
+      
       event_after_step:  ([])
           An optional list of functions which act just after each monte carlo
           round.  Each even in the list takes the form
-
+  
           event(Equench_new, newcoords, acceptstep)
-
-      quenchRoutine:  (quench.quench)
-          Optionally pass a non-default quench routine.
-
+  
       outstream: (stdout)
           the file stream to print quench information to
     """
-
+  
     def __init__(self, coords, potential, takeStep, storage=None, event_after_step=[], \
             acceptTests=[],  \
             temperature=1.0, \
             nometropolis=False, \
-            quenchRoutine = quench.quench, \
             outstream = sys.stdout
             ):
+        #note: make a local copy of lists of events so that an inputted list is not modified.
+        self.coords = copy.copy(coords)
+        self.storage = storage
+        self.potential = potential
+        self.takeStep = takeStep
+        self.event_after_step = copy.copy(event_after_step)
+        self.acceptTests = copy.copy(acceptTests)
+        self.temperature = temperature
+        self.nometropolis = nometropolis
+        self.naccepted = 0
+    
+        self.outstream = outstream
+    
+        if not self.nometropolis:
+            self.metrop_test = metropolis.Metropolis(self.temperature)
+            self.acceptTests.append( self.metrop_test.acceptReject )
+    
+        self.stepnum = 0
+    
         #########################################################################
-        #initialize MonteCarlo base class
+        #store intial structure
         #########################################################################
-        MonteCarlo.__init__(self, coords, potential, takeStep, storage, event_after_step, \
-              acceptTests,  \
-              temperature, \
-              nometropolis, \
-              outstream)
-
-        self.quenchRoutine = quenchRoutine
-
-        #########################################################################
-        #do initial quench
-        #########################################################################
-        self.markovE_old = self.markovE
-        newcoords, Equench, self.rms, self.funcalls = self.quenchRoutine(self.coords, self.potential.getEnergyGradient)
-
-        self.coords = newcoords
-        self.markovE = Equench
-
+        energy = self.potential.getEnergy(self.coords)
         if(self.storage):
-            self.storage(self.markovE, self.coords)
+            self.storage(energy, self.coords)
+          
+        self.markovE = energy
         
-        #print the initial quench
-        self.acceptstep = True
-        self.trial_energy = self.markovE
-        self.printStep()
-
-
     def mcStep(self):
-        """
-        take one monte carlo basin hopping step
-
-        redefine the MonteCarlo base class step
-        """
-        self.coords_after_step = self.coords.copy() #make  a working copy
+        """take one monte carlo basin hopping step"""
+        print "in mc mcStep"
+        self.trial_coords = self.coords.copy() #make  a working copy
         #########################################################################
         #take step
         #########################################################################
-        self.takeStep(self.coords_after_step)
-
+        self.takeStep(self.trial_coords)
+        
         #########################################################################
-        #quench
+        #calculate new energy
         #########################################################################
-        ret = self.quenchRoutine(self.coords_after_step, self.potential.getEnergyGradient)
-        self.trial_coords = ret[0]
-        self.trial_energy = ret[1] 
-        self.rms = ret[2]
-        self.funcalls = ret[3]
-
+        self.trial_energy = self.potential.getEnergy(self.trial_coords)
+        
+        
         #########################################################################
         #check whether step is accepted with user defined tests.  If any returns
         #false then reject step.
         #########################################################################
         self.acceptstep = True
         for test in self.acceptTests:
-            if not test(self.markovE, self.trial_energy, self.trial_coords, self.coords_after_step):
+            if not test(self.markovE, self.trial_energy, self.coords, self.trial_coords):
                 self.acceptstep = False
-
+    
         #########################################################################
         #return new coords and energy and whether or not they were accepted
         #########################################################################
         return self.acceptstep, self.trial_coords, self.trial_energy
-
+  
+    def run(self, nsteps):
+        #take nsteps
+        for istep in xrange(nsteps):
+            self.takeOneStep()
+    
+    def takeOneStep(self):
+        self.stepnum += 1
+        self.markovE_old = self.markovE
+        acceptstep, newcoords, newE = self.mcStep()
+        #self.outstream.write( "Qu   " + str(self.stepnum) + " E= " + str(newE) + " quench_steps= " + str(self.funcalls) + " RMS= " + str(self.rms) + " Markov E= " + str(self.markovE) + " accepted= " + str(acceptstep) + "\n" )
+        self.printStep()
+        if acceptstep:
+            if(self.storage):
+                self.storage(newE, newcoords)
+            self.coords = newcoords
+            self.markovE = newE
+            self.naccepted += 1
+        for event in self.event_after_step:
+            event(self.markovE, self.coords, acceptstep)
 
     def printStep(self):
-        if self.outstream != None:
-            self.outstream.write( "Qu   " + str(self.stepnum) + " E= " + str(self.trial_energy) + " quench_steps= " + str(self.funcalls) + " RMS= " + str(self.rms) + " Markov E= " + str(self.markovE_old) + " accepted= " + str(self.acceptstep) + "\n" )
+        self.outstream.write( "MCstep    %12d  E= %20.12g  markov E= %20.12g accepted= %s\n" % (self.stepnum, self.trial_energy, self.markovE_old, str(self.acceptstep) )  )
+
+
+
 
