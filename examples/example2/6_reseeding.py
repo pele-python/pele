5f08ee4643446a63bc6f9368cb304d40954d6418
Victor Ruehle <ruehle@votca.org>
2012-06-12 03:11:13 +0200

    added class for reseeding + example

---
+++ examples/example2/6_reseeding.py
@@ -0,0 +1,53 @@
+# -*- coding: iso-8859-1 -*-
+############################################################
+#Example 5: Adding a custom takestep routine.  This example
+#takes 100 monte carlo steps as one basin hopping step
+############################################################
+import numpy as np
+import potentials.lj as lj
+import basinhopping as bh
+from takestep import displace
+from takestep import group
+from mc import MonteCarlo
+
+class TakeStepMonteCarlo:
+    def __init__(self, pot, T = 10., nsteps = 100, stepsize=0.1):
+        self.potential = pot
+        self.T =  T
+        self.nsteps = nsteps
+        
+        self.mcstep = displace.RandomDisplacement(stepsize=stepsize)
+    
+    def takeStep(self, coords, **kwargs):
+        #make a new monte carlo class
+        mc = MonteCarlo(coords, self.potential, self.mcstep, 
+                        temperature = self.T, outstream=None)
+        mc.run( self.nsteps )
+        coords[:] = mc.coords[:]
+    
+    def updateStep(self, acc, **kwargs):
+        pass
+        
+
+
+natoms = 12
+
+# random initial coordinates
+coords=np.random.random(3*natoms)
+potential = lj.LJ()
+
+reseed = TakeStepMonteCarlo(potential, T=100, nsteps=1000)
+takestep = displace.RandomDisplacement(stepsize=0.5)
+
+stepGroup = group.Reseeding(takestep, reseed, maxnoimprove=20)
+
+opt = bh.BasinHopping(coords, potential, takeStep=stepGroup)
+opt.run(100)
+
+# some visualization
+try: 
+    import utils.pymolwrapper as pym
+    pym.start()
+    pym.draw_spheres(opt.coords, "A", 1)
+except:
+    print "Could not draw using pymol, skipping this step"
--- src/takestep/group.py
+++ src/takestep/group.py
@@ -10,11 +10,11 @@ class GroupSteps(object):
 
     def takeStep(self, coords, **kwargs):
         for step in self.steptakers:
-            step.takeStep(coords)
+            step.takeStep(coords, **kwargs)
        
     def updateStep(self, accepted, **kwargs):
         for step in self.steptakers:
-            step.updateStep(accepted)
+            step.updateStep(accepted, **kwargs)
 
 class BlockMoves(object):
     def __init__(self):
@@ -32,9 +32,39 @@ class BlockMoves(object):
             self._counter = 0
             if(self._current >= len(self._steptakers)):
                 self._current = 0
-        self._steptakers[self._current][1].takeStep(coords)
+        self._steptakers[self._current][1].takeStep(coords, **kwargs)
        
     def updateStep(self, accepted, **kwargs):
-        self._steptakers[self._current][1].updateStep(accepted)
-    
-    
+        self._steptakers[self._current][1].updateStep(accepted, **kwargs)
+
+class Reseeding(object):
+    def __init__(self, takestep, reseed, maxnoimprove=100):
+        self.takestep = takestep
+        self.reseed = reseed
+        self.maxnoimprove = maxnoimprove
+        self._noimprove = 0
+        self.lowest = None
+        
+    def takeStep(self, coords, **kwargs):
+        if self._noimprove >= self.maxnoimprove:
+            print "The energy did not improve after " + str(self._noimprove) + \
+                " steps, reseeding"
+            self.reseed.takeStep(coords, **kwargs)
+        else:
+            self.takestep.takeStep(coords, **kwargs)
+       
+    def updateStep(self, accepted, **kwargs):
+        driver = kwargs["driver"]
+        if self.lowest == None:
+            self.lowest = driver.markovE
+        if self._noimprove >= self.maxnoimprove:
+            self.reseed.updateStep(accepted, **kwargs)
+            self._noimprove=1
+        else:
+            self.takestep.updateStep(accepted, **kwargs)
+            if driver.markovE >= self.lowest:
+                self._noimprove+=1
+            else:
+                self.lowest = driver.markovE
+                self._noimprove=1
+            
