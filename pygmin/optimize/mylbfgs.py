5ef44204c521d1b53a3973e4bb8a985926a0aa4d
Jacob Stevenson <jstevenson131@gmail.com>
2012-06-13 19:34:21 +0100

    Add DJW's mylbfgs to pygmin
    Currently I use the fortran version to update the hessian, but the
    checks to make sure the energy doesn't rise, etc is still in python.
    Not all of these checks and restarts are implemented

---
+++ pygmin/optimize/mylbfgs.py
@@ -0,0 +1,254 @@
+import numpy as np
+from bfgs import lineSearch, BFGS
+from mylbfgs_updatestep import mylbfgs_updatestep
+
+class LBFGS:
+    def __init__(self, X, pot, maxstep = 0.1, maxErise = 1e-4, M=10):
+        self.X = X
+        self.pot = pot
+        e, self.G = self.pot.getEnergyGradient(self.X)
+        self.funcalls = 1
+        self.maxstep = maxstep
+        self.maxErise = maxErise
+        self.events = []
+    
+        self.N = len(X)
+        self.M = M 
+        N = self.N
+        M = self.M
+        
+        #self.beta = np.zeros(M) #working space
+        
+        
+        self.H0 = np.ones(N) * 1. #initial guess for the hessian
+        
+        self.W = np.zeros(N*(2*M+1)+2*M) #mylbfgs working space
+        self.iter = 0
+        self.point = 0
+        
+        
+        self.stp = np.zeros(N)
+        self.Xold = self.X.copy()
+        self.Gold = self.G.copy()
+        
+        self.nfailed = 0
+    
+    def step(self, X, G):
+        """
+        """
+        self.X = X
+        self.G = G
+        #save the position and gradient change
+        if self.iter > 0:
+            N = self.N
+            M = self.M
+            
+            #
+            # need write the change in position and the change in gradient
+            # to W.  This should be done more elegantly
+            #
+            ISPT= N + 2*M     # index for storage of search steps
+            IYPT= ISPT + N*M  # index for storage of gradient differences
+
+            NPT = N*((self.point + M - 1) % M)  
+            s = X - self.Xold
+            y = G - self.Gold
+            #print "YS YY py", np.dot( y, s ), np.dot( y,y ), ISPT+NPT
+            self.W[ISPT+NPT : ISPT+NPT +N] = X - self.Xold
+            self.W[IYPT+NPT : IYPT+NPT +N] = G - self.Gold    
+        self.Xold = X.copy()
+        self.Gold = G.copy()
+
+        
+        #print self.iter, self.point
+        self.stp = mylbfgs_updatestep(self.iter, self.M, G, self.W, self.H0, self.point, [self.N])
+        
+        #print "stp", np.linalg.norm(self.stp), self.stp
+        #print "G", self.G
+        #print "overlap", np.dot(self.stp, self.G)
+        #print "H0", self.H0
+        self.iter += 1
+        self.point = self.iter % self.M
+        
+        return self.stp
+
+    def takeStepNoLineSearch(self, X, E, G, stp):
+        f = 1.
+        X0 = X.copy()
+        G0 = G.copy()
+        E0 = E
+        maxErise = self.maxErise
+        
+        if np.dot(G, stp) > 0:
+            #print "overlap was negative, reversing step direction"
+            stp = -stp
+            self.stp = stp
+        
+        stepsize = np.linalg.norm(stp)
+        
+        if f*stepsize > self.maxstep:
+            f = self.maxstep / stepsize
+        
+        nincrease = 0
+        while True:
+            X = X0 + f * stp
+            E, G = self.pot.getEnergyGradient(X)
+            self.funcalls += 1
+            
+            if (E - E0) <= maxErise:
+                break
+            else:
+                print "warning: energy increased, trying a smaller step", E, E0, f*stepsize
+                f /= 10.
+                nincrease += 1
+                if nincrease > 5:
+                    break
+
+        if nincrease <= 1:
+            self.nfailed = 0
+        else:
+            self.nfailed += 1
+            if True and self.nfailed > 10:
+                print "too many failures, exiting"
+                exit(1) 
+        
+        if False and self.k <= 1:
+            print G0
+            print stp
+            print G
+            
+        
+        return X, E, G
+    
+    def reset(self):
+        self.H0[:] = 1.
+        self.k = 0
+    
+    def attachEvent(self, event):
+        self.events.append(event)
+                
+    def run(self, nsteps = 1000, tol = 1e-6, iprint = -1):
+        self.tol = tol
+        X = self.X
+        sqrtN = np.sqrt(self.N)
+        
+        
+        i = 1
+        self.funcalls += 1
+        e, G = self.pot.getEnergyGradient(X)
+        while i < nsteps:
+            stp = self.step(X, G)
+            
+            X, e, G = self.takeStepNoLineSearch(X, e, G, stp)
+            #e, G = self.pot.getEnergyGradient(X)
+            
+            rms = np.linalg.norm(G) / sqrtN
+
+            i += 1
+            
+            if iprint > 0:
+                if i % iprint == 0:
+                    print "quench step", i, e, rms, self.funcalls
+            for event in self.events:
+                event( coords=X, energy=e, rms=rms )
+      
+            if rms < self.tol:
+                break
+        return X, e, rms, self.funcalls, G , i
+   
+
+class PrintEvent:
+    def __init__(self, fname):
+        self.fout = open(fname, "w")
+        self.coordslist = []
+
+    def __call__(self, coords, **kwargs):
+        from pygmin.printing.print_atoms_xyz import printAtomsXYZ as printxyz 
+        printxyz(self.fout, coords)
+        self.coordslist.append( coords.copy() )
+        
+    
+
+def test(pot, natoms = 100, iprint=-1):
+    import bfgs
+    
+    
+    #X = bfgs.getInitialCoords(natoms, pot)
+    #X += np.random.uniform(-1,1,[3*natoms]) * 0.3
+    X = np.random.uniform(-1,1,[natoms*3])*(1.*natoms)**(1./3)*1.
+    
+    runtest(X, pot, natoms, iprint)
+
+def runtest(X, pot, natoms = 100, iprint=-1):
+    tol = 1e-5
+
+    Xinit = np.copy(X)
+    e, g = pot.getEnergyGradient(X)
+    print "energy", e
+    
+    lbfgs = LBFGS(X, pot, maxstep = 0.1)
+    printevent = PrintEvent( "debugout.xyz")
+    lbfgs.attachEvent(printevent)
+    
+    ret = lbfgs.run(10000, tol = tol, iprint=iprint)
+    print "done", ret[1], ret[2], ret[3], ret[5]
+    
+    print "now do the same with scipy lbfgs"
+    from pygmin.optimize.quench import quench
+    ret = quench(Xinit, pot.getEnergyGradient, tol = tol)
+    print ret[1], ret[2], ret[3]    
+    
+    if False:
+        print "now do the same with scipy bfgs"
+        from pygmin.optimize.quench import bfgs as oldbfgs
+        ret = oldbfgs(Xinit, pot.getEnergyGradient, tol = tol)
+        print ret[1], ret[2], ret[3]    
+    
+    if False:
+        print "now do the same with gradient + linesearch"
+        import bfgs
+        gpl = bfgs.GradientPlusLinesearch(Xinit, pot, maxstep = 0.1)  
+        ret = gpl.run(1000, tol = 1e-6)
+        print ret[1], ret[2], ret[3]    
+            
+    if False:
+        print "calling from wrapper function"
+        from pygmin.optimize.quench import lbfgs_py
+        ret = lbfgs_py(Xinit, pot.getEnergyGradient, tol = tol)
+        print ret[1], ret[2], ret[3]    
+
+
+    if False:
+        import pygmin.utils.pymolwrapper as pym
+        pym.start()
+        for n, coords in enumerate(printevent.coordslist):
+            coords=coords.reshape(natoms, 3)
+            pym.draw_spheres(coords, "A", n)
+
+        
+if __name__ == "__main__":
+    from pygmin.potentials.lj import LJ as Pot
+    #from pygmin.potentials.ATLJ import ATLJ as Pot
+    pot = Pot()
+
+    test(pot, natoms=3, iprint=1)
+    exit(1)
+    
+    coords = np.loadtxt("coords")
+    print coords.size
+    coords = np.reshape(coords, coords.size)
+    print coords
+    runtest(coords, pot, natoms=3, iprint=1)
+    
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pygmin/optimize/mylbfgs_updatestep.f90
@@ -0,0 +1,248 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!start a new subroutine here
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      SUBROUTINE MYLBFGS_UPDATESTEP(ITER, N, M, GRAD, W, DIAG, POINT, &
+         STPOUT )
+      IMPLICIT NONE
+      INTEGER, INTENT(IN)             :: N
+      INTEGER, INTENT(IN)             :: M
+      INTEGER, INTENT(IN)             :: ITER
+      INTEGER, INTENT(INOUT)          :: POINT
+      !DOUBLE PRECISION, INTENT(IN)    :: X(N)
+      DOUBLE PRECISION, INTENT(IN)    :: GRAD(N)
+      DOUBLE PRECISION, INTENT(INOUT) :: W(N*(2*M+1)+2*M)
+      DOUBLE PRECISION, INTENT(INOUT) :: DIAG(N)
+      DOUBLE PRECISION                :: STP  !not passed
+      DOUBLE PRECISION, INTENT(OUT)   :: STPOUT(N)
+
+      INTEGER J1
+      INTEGER NPT  !js850>  I think this is (POINT-1)*N
+      INTEGER ISPT, IYPT, CP, INMC, ISCN, IYCN, BOUND
+      DOUBLE PRECISION DGUESS, DUMMY, WTEMP(N), DOT1, DOT2, GNORM
+      DOUBLE PRECISION OVERLAP, SLENGTH, SQ, BETA
+      DOUBLE PRECISION YR, YS, YY
+
+      LOGICAL DIAGCO !this could be passed. It would be used to specify what the diagonal components are
+      DOUBLE PRECISION DDOT ! A FUNCTION
+!
+!     THE WORK VECTOR W IS DIVIDED AS FOLLOWS:
+!     ---------------------------------------
+!     THE FIRST N LOCATIONS ARE USED TO STORE THE GRADIENT AND
+!         OTHER TEMPORARY INFORMATION.
+!     LOCATIONS (N+1)...(N+M) STORE THE SCALARS RHO.
+!     LOCATIONS (N+M+1)...(N+2M) STORE THE NUMBERS ALPHA USED
+!         IN THE FORMULA THAT COMPUTES H*G.
+!     LOCATIONS (N+2M+1)...(N+2M+NM) STORE THE LAST M SEARCH
+!         STEPS.
+!     LOCATIONS (N+2M+NM+1)...(N+2M+2NM) STORE THE LAST M
+!         GRADIENT DIFFERENCES.
+!
+!     THE SEARCH STEPS AND GRADIENT DIFFERENCES ARE STORED IN A
+!     CIRCULAR ORDER CONTROLLED BY THE PARAMETER POINT.
+!
+
+      ISPT= N+2*M    ! index for storage of search steps
+      IYPT= ISPT+N*M ! index for storage of gradient differences
+
+      DGUESS=1.0d0
+      DIAGCO = .FALSE.
+
+ 240        FORMAT(' IMPROPER INPUT PARAMETERS (N OR M ARE NOT POSITIVE)')
+ 235              FORMAT(' THE',I5,'-TH DIAGONAL ELEMENT OF THE',/, &
+                        ' INVERSE HESSIAN APPROXIMATION IS NOT POSITIVE')
+
+      IF (ITER.EQ.0) THEN
+
+         !a bug check
+         IF (N.LE.0.OR.M.LE.0) THEN
+            WRITE(*,240)
+            STOP
+         ENDIF
+
+         POINT=0
+
+         !set the inverse hessian
+         IF (DIAGCO) THEN
+            WRITE(*,'(A)') 'using estimate of the inverse diagonal elements'
+            DO J1=1,N
+               IF (DIAG(J1).LE.0.0D0) THEN
+                  WRITE(*,235) J1
+                  STOP
+               ENDIF
+            ENDDO
+         ELSE
+            DO J1=1,N
+               DIAG(J1)=DGUESS
+            ENDDO
+         ENDIF
+
+!
+!  NR step for diagonal inverse Hessian
+!
+         DO J1=1,N
+            DUMMY=-GRAD(J1)*DIAG(J1)
+            W(ISPT+J1)=DUMMY
+            W(J1)=DUMMY
+         ENDDO
+         GNORM=DSQRT(DDOT(N,GRAD,1,GRAD,1))
+!
+!  Make the first guess for the step length cautious.
+!
+         STP=MIN(1.0D0/GNORM,GNORM)
+
+      ELSE !iter > 0
+         !IF (POINT == 0) THEN
+            !NPT = (POINT-1)*N !js850> I'm not sure about this
+         !ELSE
+            !ENDIF
+         NPT = N*MOD(POINT + M - 1, M)
+
+         BOUND=ITER
+         IF (ITER.GT.M) BOUND=M
+         CALL FLUSH()
+         YS= DDOT(N,W(IYPT+NPT+1),1,W(ISPT+NPT+1),1)
+!
+!  Update estimate of diagonal inverse Hessian elements
+!
+         IF (.NOT.DIAGCO) THEN
+            YY= DDOT(N,W(IYPT+NPT+1),1,W(IYPT+NPT+1),1)
+            !YY = SUM( W( IYPT+NPT+1 : IYPT+NPT+N )**2 )
+            !write(*,*) "YS YY fo",  YS, YY, ISPT+NPT+1
+            IF (YY.EQ.0.0D0) THEN
+               WRITE(*,'(A)') 'WARNING, resetting YY to one in mylbfgs'
+               YY=1.0D0
+            ENDIF
+            IF (YS.EQ.0.0D0) THEN
+               WRITE(*,'(A)') 'WARNING, resetting YS to one in mylbfgs'
+               YS=1.0D0
+            ENDIF
+            !write(*,*) W(IYPT+NPT+1:IYPT+NPT+N)
+!           WRITE(*,'(A,2F20.10)') 'YS/YY,STP=',YS/YY,STP
+            DO J1=1,N
+!              DIAG(J1)= ABS(YS/YY) ! messes up after step reversals!
+               DIAG(J1)= YS/YY
+            ENDDO
+         ELSE
+            WRITE(*,'(A)') 'using estimate of the inverse diagonal elements'
+            DO J1=1,N
+               IF (DIAG(J1).LE.0.0D0) THEN
+                  WRITE(*,235) J1
+                  STOP
+               ENDIF
+            ENDDO
+         ENDIF
+!
+!     COMPUTE -H*G USING THE FORMULA GIVEN IN: Nocedal, J. 1980,
+!     "Updating quasi-Newton matrices with limited storage",
+!     Mathematics of Computation, Vol.24, No.151, pp. 773-782.
+!     ---------------------------------------------------------
+!
+
+         CP= POINT
+         IF (POINT.EQ.0) CP=M
+         W(N+CP)= 1.0D0/YS
+         DO J1=1,N
+            W(J1)= -GRAD(J1)
+         ENDDO
+         CP= POINT
+         DO J1= 1,BOUND
+            CP=CP-1
+            IF (CP.EQ.-1) CP=M-1
+            SQ=DDOT(N,W(ISPT+CP*N+1),1,W,1)
+            INMC=N+M+CP+1
+            IYCN=IYPT+CP*N
+            W(INMC)=W(N+CP+1)*SQ
+            CALL DAXPY(N,-W(INMC),W(IYCN+1),1,W,1)
+         ENDDO
+        
+         DO J1=1,N
+            W(J1)=DIAG(J1)*W(J1)
+         ENDDO
+
+         DO J1=1,BOUND
+            YR= DDOT(N,W(IYPT+CP*N+1),1,W,1)
+            BETA= W(N+CP+1)*YR
+            INMC=N+M+CP+1
+            BETA= W(INMC)-BETA
+            ISCN=ISPT+CP*N
+            CALL DAXPY(N,BETA,W(ISCN+1),1,W,1)
+            CP=CP+1
+            IF (CP.EQ.M) CP=0
+         ENDDO
+         STP=1.0D0  
+      ENDIF
+!
+!  Store the new search direction
+!
+      IF (ITER.GT.0) THEN
+         DO J1=1,N
+            W(ISPT+POINT*N+J1)= W(J1)
+         ENDDO
+      ENDIF
+
+      DOT1=SQRT(DDOT(N,GRAD,1,GRAD,1))
+!
+!  Overflow has occasionally occurred here.
+!  We only need the sign of the overlap, so use a temporary array with
+!  reduced elements.
+!
+      DUMMY=1.0D0
+      DO J1=1,N
+         IF (ABS(W(J1)).GT.DUMMY) DUMMY=ABS(W(J1))
+      ENDDO
+      DO J1=1,N
+         WTEMP(J1)=W(J1)/DUMMY
+      ENDDO
+      DOT2=SQRT(DDOT(N,WTEMP,1,WTEMP,1))
+      OVERLAP=0.0D0
+      IF (DOT1*DOT2.NE.0.0D0) THEN
+        OVERLAP=DDOT(N,GRAD,1,WTEMP,1)/(DOT1*DOT2)
+      ENDIF
+      IF (OVERLAP.GT.0.0D0) THEN
+         DO J1=1,N
+            W(ISPT+POINT*N+J1)= -W(J1)  !!! DJW, reverses step
+         ENDDO
+      ENDIF
+
+      DO J1=1,N
+         W(J1)=GRAD(J1)
+      ENDDO
+
+      DO J1=1,N
+         STPOUT(J1) = STP*W(ISPT+POINT*N+J1)
+      ENDDO
+
+
+   END SUBROUTINE MYLBFGS_UPDATESTEP
+
+      DOUBLE PRECISION FUNCTION DDOT (N, DX, INCX, DY, INCY)
+         !js850> my simple implimentation
+!     --Input--
+!        N  number of elements in input vector(s)
+!       DX  double precision vector with N elements
+! ignored     INCX  storage spacing between elements of DX
+!       DY  double precision vector with N elements
+! ignored     INCY  storage spacing between elements of DY
+         IMPLICIT NONE
+         INTEGER, INTENT(IN) :: N, INCX, INCY
+         DOUBLE PRECISION, INTENT(IN) :: DX(N), DY(N)
+         INTEGER J1
+         DDOT = SUM( DX(:) * DY(:) )
+         return
+         DDOT = 0.0D0
+         IF (N .LE. 0) RETURN
+         DO J1=1,N
+            DDOT = DDOT + DX(J1) * DY(J1)
+         ENDDO
+         RETURN
+      END FUNCTION DDOT
+
+      SUBROUTINE DAXPY(N,DA,DX,INCX,DY,INCY)
+         IMPLICIT NONE
+         INTEGER, INTENT(IN) ::  INCX, INCY, N
+         DOUBLE PRECISION, INTENT(IN):: DA
+         DOUBLE PRECISION, INTENT(IN) :: DX(N)
+         DOUBLE PRECISION, INTENT(INOUT)::  DY(N)
+         DY(:) = DY(:) + DA*DX(:)
+      END SUBROUTINE DAXPY
+
