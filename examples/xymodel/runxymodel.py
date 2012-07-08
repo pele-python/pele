50157a1028bd88d8667c16c6452ae4afc22a8343
Jacob Stevenson <jstevenson131@gmail.com>
2012-07-08 17:06:34 +0100

    Add an example for the xy spin model

---
+++ examples/xymodel/runxymodel.py
@@ -0,0 +1,87 @@
+import numpy as np
+from numpy import cos, sin
+from copy import copy
+from pygmin.potentials.xyspin import XYModel
+
+
+
+
+def angle2vec(a):
+    return np.array( [cos(a), sin(a)] )
+
+def printspins(fout, pot, angles):
+    nspins = len(angles)
+    for node in pot.G.nodes():
+        i = pot.indices[node]
+        s = angle2vec(angles[i])
+        fout.write("%g %g " % (node[0], node[1]) )
+        fout.write("%g %g\n" % (s[0], s[1]) )
+        
+
+
+pi = np.pi
+L = 8
+nspins = L**2
+
+#phases = np.zeros(nspins)
+pot = XYModel( dim = [L,L], phi = np.pi) #, phases=phases)
+
+
+angles = np.random.uniform(-pi, pi, nspins)
+print angles
+
+e = pot.getEnergy(angles)
+print "energy ", e
+
+if False:
+    print "numerical gradient"
+    ret = pot.getEnergyGradientNumerical(angles)
+    print ret[1]
+    print "analytical gradient"
+    ret2 = pot.getEnergyGradient(angles)
+    print ret2[1]
+    print ret[0]
+    print ret2[0]
+
+
+
+#try a quench
+from pygmin.optimize.quench import mylbfgs
+ret = mylbfgs(angles, pot.getEnergyGradient)
+
+print "quenched e = ", ret[1]
+print ret[0]
+
+
+
+from pygmin.basinhopping import BasinHopping
+from pygmin.takestep.displace import RandomDisplacement
+from pygmin.takestep.adaptive import AdaptiveStepsize
+
+takestep = RandomDisplacement(stepsize = np.pi/4)
+takestepa = AdaptiveStepsize(takestep, frequency = 20)
+
+bh = BasinHopping( angles, pot, takestepa, temperature = 1.01)
+bh.run(20)
+
+
+with open("out.spin", "w") as fout:
+    printspins(fout, pot, ret[0])
+    """
+    view this in gnuplot with the command
+    plot 'out.spin' u 1:2 w p pt 5, '' u 1:2:($3*0.5):($4*0.5) w vectors
+    """
+    
+import matplotlib.pyplot as plt
+x = [node[0] for node in pot.G.nodes()]
+y = [node[1] for node in pot.G.nodes()]
+ilist = [pot.indices[node] for node in pot.G.nodes()]
+v0 = cos(angles)
+v1 = sin(angles)
+plt.quiver(x, y, v0, v1)
+a = plt.gca()
+a.set_xlim([-1, max(x)+1])
+a.set_ylim([-1, max(y)+1])
+plt.show()
+
+
