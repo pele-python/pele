75b40fa48ce5b5443c81dafa3a4d40b4185829af
Victor Ruehle <ruehle@votca.org>
2012-07-10 14:59:33 +0100

    reseeding for crystals

--- pygmin/utils/lattice.py
+++ pygmin/utils/lattice.py
@@ -4,3 +4,9 @@ def lowerTriangular(coords):
         [[coords[0],    0.,        0.],
         [ coords[1], coords[3],    0.],
         [ coords[2], coords[4], coords[5]]])
+
+def volume(coords):
+    ml = lowerTriangular(coords)
+    return np.dot(ml[:,0],np.cross(ml[:,1], ml[:,2]))
+
+    
