2ff1b1782373b0e1226383ed4b5c8fa93b3d0883
Victor Ruehle <ruehle@votca.org>
2012-07-11 18:21:13 +0100

    a bit more cleanup tetrol

--- examples/dmacrys/tetrol.py
+++ examples/dmacrys/tetrol.py
@@ -7,6 +7,19 @@ from pygmin.potentials.coldfusioncheck import addColdFusionCheck
 from pygmin.storage import savenlowest
 from pygmin.utils import crystals
 import pickle
+
+nsave = 1000
+nsteps = 1000
+dump_frequency=10
+temperature=0.4
+maxnoimprove=20
+
+quenchParameters={}
+quenchParameters["tol"]=1e-4
+quenchParameters["nsteps"]=1000
+quenchParameters["maxErise"]=2e-2
+quenchParameters["maxstep"]=0.1
+quenchParameters["M"]=100
      
 class DMACRYSTakestep(generic.TakestepInterface):
     def takeStep(self, coords, **kwargs):
@@ -37,25 +50,31 @@ print "initial energy ", pot.getEnergy(coords)
 
 step1 = MyStep()
 reseed = crystals.GenRandomCrystal(CoordsAdapter(nrigid=GMIN.getNRigidBody(), nlattice=6, coords=coords))
-step = group.Reseeding(step1, reseed, maxnoimprove=20)
+step = group.Reseeding(step1, reseed, maxnoimprove=maxnoimprove)
 
 #save = pickle.load(open("storage"))
 #save.nsave = 1000
 save = savenlowest.SaveN(nsave=100, accuracy=1e-3)
 save.compareMinima = compareMinima
 
-opt = bh.BasinHopping(coords, pot, takeStep=step, quenchRoutine=crystals.quenchCrystal, temperature=0.4, storage=save)
+opt = bh.BasinHopping(coords, pot, takeStep=step, 
+                      quenchRoutine=crystals.quenchCrystal,
+                      temperature=temperature, storage=save,
+                      quenchParameters=quenchParameters)
 addColdFusionCheck(opt)
 
-opt.quenchParameters["tol"]=1e-4
-opt.quenchParameters["nsteps"]=1000
-opt.quenchParameters["maxErise"]=2e-2
-opt.quenchParameters["maxstep"]=0.1
-opt.quenchParameters["M"]=100
+# create cif directory if it does not exist
+import os, errno
+try:
+    os.makedirs('cif')
+except OSError, e:
+    if e.errno != errno.EEXIST:
+        raise
 
-for i in xrange(1,50):
-    opt.run(100)
-    pickle.dump(save, open("storage."+str(i), "w"))
+
+for i in xrange(1,nsteps/dump_frequency):
+    opt.run(dump_frequency)
+    #pickle.dump(save, open("storage."+str(i), "w"))
     pickle.dump(save, open("storage", "w"))
     i=0
     for m in save.data:
