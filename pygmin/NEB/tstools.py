c6bb37dfe7b45b1b978125ebe392a673fc27290e
Victor Ruehle <ruehle@votca.org>
2012-08-07 03:21:46 +0200

    bugfix with defaults in tstools

--- pygmin/NEB/tstools.py
+++ pygmin/NEB/tstools.py
@@ -3,14 +3,22 @@ from pygmin import defaults
 
 
 def minima_from_ts(getEnergyGradient, xt, n=None, displace=1e-3,
-                   quenchRoutine=defaults.quenchRoutine, quenchParameters=defaults.quenchParams):
+                   quenchRoutine=None, quenchParameters=None):
     # if no direction is given, choose random direction
     if n==None:
         # TODO: replace by better algorithm with uniform sampling
         n = np.random.random(xt.shape)-0.5
+    
+    if quenchRoutine==None:
+        quenchRoutine = defaults.quenchRoutine
+    if quenchParameters==None:
+        quenchParameters = defaults.quenchParams
         
     x1 = xt - displace*n
     x2 = xt + displace*n
+    e1,g1 = getEnergyGradient(x1)
+    e2,g2 = getEnergyGradient(x2)
+    print np.dot(g1,g2)
     minimum1 = quenchRoutine(x1, getEnergyGradient, **quenchParameters)
     minimum2 = quenchRoutine(x2, getEnergyGradient, **quenchParameters)
     return minimum1, minimum2
