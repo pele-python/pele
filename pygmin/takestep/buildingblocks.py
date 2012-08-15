c66a545e21eaf3918d3f3d2a99d7acc8fbff9802
Victor Ruehle <ruehle@votca.org>
2012-08-15 01:04:58 +0100

    added indexing for takestep building blocks

--- pygmin/takestep/buildingblocks.py
+++ pygmin/takestep/buildingblocks.py
@@ -2,16 +2,31 @@ import numpy as np
 from pygmin.utils import rotations
 from pygmin.utils import vec3
 
-def uniform_displace(stepsize, coords):
+def uniform_displace(stepsize, coords, indices=None):
+    if(indices):
+        for i in indices:
+            coords[i] += stepsize*rotations.vec_random()
+        return
+    
     for x in coords:
         x += stepsize*rotations.vec_random()
         
-def rotate(stepsize, coords):
+def rotate(stepsize, coords, indices=None):
+    if(indices):
+        for i in indices:
+            rotations.takestep_aa( coords[i], stepsize )
+        return
+    
     for x in coords:
         rotations.takestep_aa( x, stepsize )
         
-def reduced_coordinates_displace(stepsize, lattice_matrix, coords):
+def reduced_coordinates_displace(stepsize, lattice_matrix, coords, indices=None):
     ilattice = vec3.invert3x3(lattice_matrix) # inverse_lattice
+    if(indices):
+        for i in indices:
+            coords[i] += np.dot(ilattice, stepsize*rotations.vec_random())
+        return
+            
     for x in coords:
         x += np.dot(ilattice, stepsize*rotations.vec_random())
    
