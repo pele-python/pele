837e4a151e5aa79a3323b47f18928c57ce6b8e20
Jacob Stevenson <jstevenson131@gmail.com>
2012-08-16 20:02:39 +0100

    Add ability to use c++ version of hungarian algorithm
    The pypi package "hungarian" implements the hungarian algorithm (aka
    munkres) in c++.  It should be significantly faster.  hungarian gave me
    segfaults when I first downloaded it.  I may add the corrected version
    to pygmin at some point.

--- pygmin/run_tests.py
+++ pygmin/run_tests.py
@@ -3,6 +3,7 @@ import unittest
 from pygmin.mindist.aamindist import aaDistTest
 from pygmin.mindist.minpermdist_stochastic import TestMinPermDistStochastic_BLJ
 from pygmin.mindist.minpermdist_rbmol import TestMinPermDistRBMol_OTP
+from pygmin.mindist.mindistutils import TestMinDistUtils
 from pygmin.potentials.ATLJ import TestATLJ
 
 
