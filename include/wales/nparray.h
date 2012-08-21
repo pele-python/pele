f45d94f07b8750c72a82d6dba2f5d2284f68ee4e
Victor Ruehle <ruehle@votca.org>
2012-08-21 14:38:16 +0100

    fixed memory leak in nparray.h

--- include/wales/nparray.h
+++ include/wales/nparray.h
@@ -12,12 +12,14 @@ class NPArray
 {
 public:
   NPArray(boost::python::numeric::array& array);
+  ~NPArray();
   
   size_t size(int dim) { return _size[dim]; }
   double &operator[](size_t i) { return _data[i]; }
 private:
   double *_data;
   size_t _size[ndims];
+  PyArrayObject *_py_object;
 };
 
 template<int ndims>
@@ -25,6 +27,7 @@ NPArray<ndims>::NPArray(boost::python::numeric::array& array)
 {
   // Get pointer to np array
   PyArrayObject* a = (PyArrayObject*)PyArray_FROM_O(array.ptr());
+  _py_object = a;
   if (a == NULL) {
     throw std::runtime_error("Could not get NP array.");
   }
@@ -39,6 +42,13 @@ NPArray<ndims>::NPArray(boost::python::numeric::array& array)
   _size[0] = *(a->dimensions);
 }
 
+template<int ndims>
+NPArray<ndims>::~NPArray()
+{
+	if(_py_object)
+		Py_DECREF(_py_object);
+	_py_object = NULL;
+}
 }
 
 #endif
