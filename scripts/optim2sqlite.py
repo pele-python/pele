44ce248fe8d5eefd5a3710131c2ed31f0ee63f4f
Victor Ruehle <ruehle@votca.org>
2012-09-07 18:02:52 +0100

    reading ts coords

--- scripts/optim2sqlite.py
+++ scripts/optim2sqlite.py
@@ -5,9 +5,16 @@ from pygmin.potentials.lj import LJ
 
 natoms = 38
 pot = LJ()
-
 # open database
-db = Database(db="storagetmp.sqlite")
+db = Database(db="storage.sqlite")
+
+def read_coords(file):
+    coords = np.zeros(3*natoms)
+    for i in xrange(natoms):
+        x = file.readline().split()
+        coords[i*3:i*3+3] = [float(y) for y in x]
+    return coords 
+
 
 # counter to keep track of added minima
 mini=1
@@ -22,16 +29,12 @@ fcoords = open("extractedmin")
 print "Reading minima"
 # loop over all lines in min
 for line in open("min.data"):
-    coords = np.zeros(3*natoms)
-    for i in xrange(natoms):
-        x = fcoords.readline().split()
-        coords[i*3:i*3+3] = [float(y) for y in x] 
+    coords = read_coords(fcoords) 
     energy, frequency, pgorder, itx, ity, itz = line.split()    
-    print energy, pot.getEnergy(coords)
     min1 = Minimum(float(energy), coords)    
     db.session.add(min1)
     minima[mini]=min1
-    mini+=1    
+    mini+=1        
 
 print "%.1f seconds"%(time.time() - tt)
 tt = time.time()
@@ -42,9 +45,11 @@ tt = time.time()
 
 
 print "Reading transition states"
+fcoords = open("extractedts")
 for line in open("ts.data"):
+    coords = read_coords(fcoords) 
     energy, frequency, pgorder, min1, min2, itx, ity, itz = line.split()
-    ts = TransitionState(float(energy), None, minima[int(min1)], minima[int(min2)])
+    ts = TransitionState(float(energy), coords, minima[int(min1)], minima[int(min2)])
     db.session.add(ts)
     #db.addTransitionState(float(energy), None, minima[int(min1)], minima[int(min2)], commit=False)
 print "%.1f seconds"%(time.time() - tt)
@@ -53,13 +58,3 @@ print "Commiting changes to database"
 db.session.commit()
 print "%.1f seconds"%(time.time() - tt)
 print "Done after %.1f seconds"%(time.time() - t0)
-
-from  pygmin.NEB.graph import Graph
-print "generating graph"
-tt = time.time()
-graph = Graph(db)
-print "%.1f seconds"%(time.time() - tt)
-
-#print "printing minima"
-#for i in db.minima():
-#    print i
