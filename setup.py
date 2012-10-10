2cf79468f3a26aa7b69f29c7ad93644fa5e7c266
Jacob Stevenson <js850@cam.ac.uk>
2012-10-10 14:21:09 +0100

    setup.py compiles fortran modules

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from distutils.core import setup
+from numpy.distutils.core import setup
+from numpy.distutils.core import Extension
 
 setup(name='PyGMIN', 
         version='0.1', 
@@ -13,6 +14,31 @@ setup(name='PyGMIN',
             "pygmin.takestep",
             "pygmin.utils",
             "pygmin.wham"
-            ]
+            ],
+      ext_modules= [
+            Extension("pygmin.mindist.overlap", 
+                      ["pygmin/mindist/overlap.f90"]),
+                    
+            Extension("pygmin.optimize.mylbfgs_fort", 
+                      ["pygmin/optimize/mylbfgs_fort.f90"]),
+            Extension("pygmin.optimize.mylbfgs_updatestep", 
+                      ["pygmin/optimize/mylbfgs_updatestep.f90"]),
+                    
+            Extension("pygmin.potentials.fortran.AT", 
+                      ["pygmin/potentials/fortran/AT.f"]),
+            #Extension("pygmin.potentials.fortran.lj_hess", 
+            #          ["pygmin/potentials/fortran/lj_hess.f"]),
+            Extension("pygmin.potentials.fortran.ljpshiftfort", 
+                      ["pygmin/potentials/fortran/ljpshiftfort.f"]),
+            Extension("pygmin.potentials.fortran.lj", 
+                      ["pygmin/potentials/fortran/lj.f90"]),
+            Extension("pygmin.potentials.fortran.rmdrvt", 
+                      ["pygmin/potentials/fortran/rmdrvt.f90"]),
+            Extension("pygmin.potentials.fortran.soft_sphere_pot", 
+                      ["pygmin/potentials/fortran/soft_sphere_pot.f90"]),
+                    
+            Extension("pygmin.utils._fortran_utils", 
+                      ["pygmin/utils/_fortran_utils.f90"]),
+                ]
         )
 
