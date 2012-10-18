c760ea7c30610d2342410a245b2ba9004b3d3544
Jacob Stevenson <js850@cam.ac.uk>
2012-10-18 17:18:25 +0100

    Add lots of comment to defaults.py
    also, set default NEB nsteps to 100

--- pygmin/defaults.py
+++ pygmin/defaults.py
@@ -1,6 +1,69 @@
 from pygmin.optimize import quench
 from pygmin.NEB import dimer
 
+"""
+These are an easy way to change the default parameters of the various features of PyGMIN. 
+
+to change the default quenchRoutine to FIRE, simply run
+>>> import pygmin.utils.defaults as defaults
+>>> from pygmin.optimize.quench import fire
+>>> defaults.quenchRoutine = fire
+
+to further change the tolerance for all quenches do
+
+>>> defaults.quenchRoutine["tol"] = 0.01
+"""
+
+"""
+notes
+-----
+The following are sections of the code which should have default settings in this way
+
+    normal quenching :
+        which routine
+        parameters
+        
+    lowest eigenvector search :
+        which routine
+            eventually this can become untied to the transition state search
+        parameters
+            important parameters: 
+                specify zero eigenmodes
+    
+    minimization in the space tangent to lowest eigenvector
+        which routine
+        parameters
+    
+    Transition state search:
+        which routine
+        parameters
+            transition state search uses lowest egenvector search and tangent space minimization, so 
+            these parameters would be in addition to those
+
+    NEB:
+        which routine:
+            NEB, DNEB, etc
+        parameters
+            important parameters:
+                nsteps : should be small (100).  It often will minimize forever without reaching 
+                         the required tolerance
+
+    double ended connect : 
+        parameters:
+            This uses NEB, Transition state search, and mindist, so these 
+            parameters will be in addition to those.
+            important parameters:
+          
+    single ended connect : 
+        parameters
+
+    best alignment between two structures (mindist):
+        parameters
+            here the parameters would be, e.g. rotational symmetry, translational symmetry,
+            permutational symmetry, etc.  In principle the routine is determined by the parameters
+
+"""
+
 quenchRoutine = quench.lbfgs_py
 quenchParams = dict()
 
@@ -11,3 +74,4 @@ lowestEigenvectorQuenchParams = dict()
 
 NEBquenchRoutine = quench.lbfgs_py
 NEBquenchParams = dict()
+NEBquenchParams["nsteps"] = 100
