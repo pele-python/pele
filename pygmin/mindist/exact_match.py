e3d07fb55fcc1b4356111560ce097059ba4e3eb5
ruehle <ruehle@votca.org>
2012-11-08 00:51:12 +0000

    made exact match for clusters more robust + faster

--- pygmin/mindist/exact_match.py
+++ pygmin/mindist/exact_match.py
@@ -2,28 +2,57 @@ import numpy as np
 from mindistutils import findBestPermutation, CoMToOrigin
 import rmsfit
 
-class PerfectMatch(object):
+
+__all__= ["ExactMatchCluster"]
+
+class ExactMatchCluster(object):
     ''' Deterministic check if 2 clusters are a perfect match
     
-        Determines quickly if 2 clusters are a perfect match. The algorithm
-        does the following:
+        Determines quickly if 2 clusters are a perfect match. It uses the 
+        atoms which are far away from the center to determine possible
+        rotations. The algorithm does the following:
         
-        1) get list of 1st matching candidates in most outer shell
-        2) if 1st candidate is already uniquely determined, look 
-           in second shell outer for more candidates
+        1) Get 2 reference atoms from structure 1 which are farest away from center
+           and are not linear
+        2) Determine candidates from structure 2 which are in same shell
+           as reference atoms from structure 1 (+- accuracy)
         3) loop over all candidate combinations to determine
-           orientation and check for match
+           orientation and check for match. Skip directly if angle of candidates
+           does not match angle of reference atoms in structure 1.
+           
+        Parameters
+        ----------
+        
+        accuracy: float, optional
+            maximum deviation of atoms to still consider cluster as a match
+            
+        permlist: iteratable, optional
+            list of allowed permutations. Default is None which means all
+            particles can be permuted
+            
+        align_com: boolean, optional
+            Flag if com should be removed before comparison
+            
+        Examples
+        --------
+        
+        >>> x1 = np.random.random(3*natoms)
+        >>> x2 = x1 + 1e-4*np.random.random(x1.shape)
+        >>> matches = ExactClusterMatch(accuracy=1e-3)
+        >>> if match(x1, x2):
+        >>>     print "the two structures are identical
            
         Notes
         -----
-        - The current implementation will fail if the 2 farest atoms are on
-        a line
+        - The current implementation will detect clusters with inversion
+          symmetry as different
+          
     '''
     
     def __init__(self, accuracy=0.01, permlist=None, align_com = True):
         self.accuracy = accuracy
         self.align_com = align_com
-        self.permlist=permlist
+        self.permlist = permlist
         
     def __call__(self, x1, x2):
         natoms = x1.shape[0]
@@ -38,52 +67,88 @@ class PerfectMatch(object):
         
         # at least 2 atoms are needed
         # get atom most outer atom
-        R1max, R2max = R1.max(), R2.max()
         
-        # do a very quick check
-        if np.abs(R1max - R2max) > self.accuracy:
+        # get 1. reference atom in configuration 1
+        # use the atom with biggest distance to com
+        idx1_1 = R1.argmax()
+        
+        # find second atom which is not in a line
+        Ri = R1[idx1_1]
+        while True:
+            idx1_2 = R1[R1<Ri].argmax()
+            Ri = R1[idx1_2]
+            # stop if angle is larger than threshold
+            cos_theta1 = np.dot(x1[idx1_1], x2[idx1_2]) / \
+                (np.linalg.norm(x1[idx1_1])*np.linalg.norm(x1[idx1_2])) 
+            if cos_theta1 < 0.9:
+                break
+        
+        # do a very quick check if most distant atom from
+        # center are within accuracy
+        if np.abs(R1[idx1_1] - R2.max()) > self.accuracy:
             return False
         
-        # get atoms in outer shell
-        candidates1_1 = np.arange(natoms)[R1 > R1max - self.accuracy]
-        candidates2_1 = np.arange(natoms)[R2 > R2max - self.accuracy]
+        # get indices of atoms in shell of thickness 2*accuracy
+        candidates1 = np.arange(len(R2))[ \
+             (R2 > R1[idx1_1] - self.accuracy)*(R2 < R1[idx1_1] + self.accuracy)] 
+        candidates2 = np.arange(len(R2))[ \
+             (R2 > R1[idx1_2] - self.accuracy)*(R2 < R1[idx1_2] + self.accuracy)] 
         
-        candidates1_2 = candidates1_1
-        # need to check second shell for more candidates?
-        if(len(candidates1_2) < 2):
-            R1max2 = R1[R1 < R1max].max()
-            candidates1_2 = np.arange(natoms)[(R1 > R1max2 - self.accuracy) * (R1 < R1max)]
-        
-        candidates2_2 = candidates2_1
-        # need to check second shell for more candidates?
-        if(len(candidates2_2) < 2):
-            R2max2 = R2[R2 < R2max].max()
-            candidates2_2 = np.arange(natoms)[(R2 > R2max2 - self.accuracy) * (R2 < R2max)]
-         
         # now loop over all cobinations
-        for i1_1 in candidates1_1:
-            for i1_2 in candidates1_2:
-                if i1_1 == i1_2:
+        for idx2_1 in candidates1:
+            for idx2_2 in candidates2:
+                if idx2_1 == idx2_2:
                     continue
-                for i2_1 in candidates2_1:
-                    for i2_2 in candidates2_2:
-                        if i2_1 == i2_2:
-                            continue
-                        rot = rmsfit.findrotation_kabsch( \
-                                      x1[[i1_1, i1_2]], x2[[i2_1, i2_2]], False)
-                        if self.check_match(x1, x2, rot):
-                            return True
+                
+                # we can immediately trash the match if angle does not match
+                cos_theta2 = np.dot(x1[idx2_1], x2[idx2_2]) / \
+                    (np.linalg.norm(x2[idx2_1])*np.linalg.norm(x2[idx2_2]))
+                if(np.abs(cos_theta2 - cos_theta2) > 0.1):
+                    continue
+
+                # get rotation for current atom match candidates
+                rot = rmsfit.findrotation_kabsch( \
+                              x1[[idx1_1, idx1_2]], x2[[idx2_1, idx2_2]], False)
+                # pass on the match for a closer check
+                if self.check_match(x1, x2, rot):
+                    return True
                         
         return False
     
     def check_match(self, x1, x2, rot):
+        ''' Make a more detailed comparison if the 2 structures match
+        
+        Parameters
+        ----------
+        
+        x1: np.array
+            coordinates of structure 1
+        x2: np.array
+            coordinates of structure 2
+        rot: np.array, 3x3
+            guessed rotation based on reference atoms
+            
+        returns: boolean
+            True or False for match
+            
+        '''    
+        # apply the rotation
         x1_trial = np.dot(rot, x1.transpose()).transpose()
+        # make a copy since findBestPermutation will mess up order
         x2_trial = x2.copy()
+        # get the best permutation
         dist, x1n, x2n = findBestPermutation(x1_trial, x2_trial)
+        
+        # now find best rotational alignment, this is more reliable than just
+        # aligning the 2 reference atoms
+        rot2 = rmsfit.findrotation_kabsch(x1n, x2n)
+        x1n = np.dot(rot2, x1n.transpose()).transpose()
+        
+        # use the maximum distance, not rms as cutoff criterion
         max_dist = np.sqrt(np.sum((x1n - x2n)*(x1n - x2n), axis=1)).max()
         if  max_dist < self.accuracy:
+            self.best_rotation = np.dot(rot2, rot)
             return True
-        print max_dist
         return False
                         
     
@@ -96,7 +161,7 @@ if __name__ == '__main__':
         xx1 = xx1.reshape([-1,3])
         mx = rotations.q2mx(rotations.random_q())
         xx2 = np.dot(mx, xx1.transpose()).transpose()
-        xx2 +=2.*(np.random.random(xx2.shape)-0.5)*0.01
+        xx2 +=2.*(np.random.random(xx2.shape)-0.5)*0.003
 
-        print i,PerfectMatch()(xx1, xx2)
+        print i,ExactMatchCluster()(xx1, xx2)
     
