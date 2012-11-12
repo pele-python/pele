106ce004326ea13a5ad1197e4c0e708b6c06fabd
nh364 <nh364@cam.ac.uk>
2012-11-12 16:59:22 +0000

    added number of torsion moves as parameter to OXDNAScrewStep

--- pygmin/systems/oxdna.py
+++ pygmin/systems/oxdna.py
@@ -31,9 +31,10 @@ class OXDNATakestep(takestep.TakestepInterface):
         return [self.rotate, self.displace]
             
 class OXDNAScrewStep(takestep.TakestepInterface):
-    def __init__(self, rotate_backbone=0.5*pi, rotate_base=0.):
+    def __init__(self, rotate_backbone=0.5*pi, rotate_base=0., ntorsionmoves=1):
         self.rotate_backbone = rotate_backbone
         self.rotate_base = rotate_base
+        self.ntorsionmoves=1
 
     def takeStep(self, coords, **kwargs):
         # easy access to coordinates
@@ -45,34 +46,35 @@ class OXDNAScrewStep(takestep.TakestepInterface):
             ca.rotRigid[i] = rotations.rotate_aa(ca.rotRigid[i], a)
                
          # random rotation for angle-axis vectors
-        for j in xrange(1):
-            # choose bond to rotate around, index is first bead that changes
-            index = np.random.randint(1, ca.nrigid)
-            
-            # determine backbone beads
-            a1 = np.dot(rotations.aa2mx(ca.rotRigid[index-1]), np.array([1., 0., 0.]))
-            a2 = np.dot(rotations.aa2mx(ca.rotRigid[index]), np.array([1., 0., 0.]))
-            x1 = ca.posRigid[index-1] - 0.4*a1 # backbone bead
-            x2 = ca.posRigid[index]   - 0.4*a2 # backbone bead
-            
-            # get bond vector as axis of rotation + random magnitude
-            p = x2 - x1
-            p /= np.linalg.norm(p)
-            p *= 2.*(np.random.random()-0.5)*self.rotate_backbone
-            # convert random rotation to a matrix
-            mx = rotations.aa2mx(p)
-            # center of rotation is in middle of backbone bond
-            center = 0.5*(x1 + x2)
-            
-            # apply rotation to positions and orientations
-            for i in xrange(index, ca.nrigid):
-                a = np.dot(rotations.aa2mx(ca.rotRigid[i]), np.array([1., 0., 0.]))
-                ca.rotRigid[i] = rotations.rotate_aa(ca.rotRigid[i], p)
-                x = ca.posRigid[i] - 0.4*a
-                ca.posRigid[i] = np.dot(mx, x - center) + center
-                a = np.dot(rotations.aa2mx(ca.rotRigid[i]), np.array([1., 0., 0.]))
-                ca.posRigid[i]+=0.4*a
-            
+        if self.rotate_backbone != 0.:
+            for j in xrange(self.ntorsionmoves):
+                # choose bond to rotate around, index is first bead that changes
+                index = np.random.randint(1, ca.nrigid)
+                
+                # determine backbone beads
+                a1 = np.dot(rotations.aa2mx(ca.rotRigid[index-1]), np.array([1., 0., 0.]))
+                a2 = np.dot(rotations.aa2mx(ca.rotRigid[index]), np.array([1., 0., 0.]))
+                x1 = ca.posRigid[index-1] - 0.4*a1 # backbone bead
+                x2 = ca.posRigid[index]   - 0.4*a2 # backbone bead
+                
+                # get bond vector as axis of rotation + random magnitude
+                p = x2 - x1
+                p /= np.linalg.norm(p)
+                p *= 2.*(np.random.random()-0.5)*self.rotate_backbone
+                # convert random rotation to a matrix
+                mx = rotations.aa2mx(p)
+                # center of rotation is in middle of backbone bond
+                center = 0.5*(x1 + x2)
+                
+                # apply rotation to positions and orientations
+                for i in xrange(index, ca.nrigid):
+                    a = np.dot(rotations.aa2mx(ca.rotRigid[i]), np.array([1., 0., 0.]))
+                    ca.rotRigid[i] = rotations.rotate_aa(ca.rotRigid[i], p)
+                    x = ca.posRigid[i] - 0.4*a
+                    ca.posRigid[i] = np.dot(mx, x - center) + center
+                    a = np.dot(rotations.aa2mx(ca.rotRigid[i]), np.array([1., 0., 0.]))
+                    ca.posRigid[i]+=0.4*a
+                
 # this is necessary for adaptive step taking
     def scale(self, factor):
         self.rotate_backbone *= factor
