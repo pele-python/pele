6364fb4d2a595f494801a5ca9904f89143f73ade
Jacob Stevenson <js850@cam.ac.uk>
2012-11-13 09:22:34 +0000

    reorganize and simplify setup.py

--- setup.py
+++ setup.py
@@ -1,42 +1,41 @@
 from numpy.distutils.core import setup
 from numpy.distutils.core import Extension
 from numpy.distutils.misc_util import has_cxx_sources
+import os
 
-fortran_modules = [
-    Extension("pygmin.mindist.overlap", 
-              ["pygmin/mindist/overlap.f90"]),
-            
-    Extension("pygmin.optimize.mylbfgs_fort", 
-              ["pygmin/optimize/mylbfgs_fort.f90"]),
-    Extension("pygmin.optimize.mylbfgs_updatestep", 
-              ["pygmin/optimize/mylbfgs_updatestep.f90"]),
-            
-    Extension("pygmin.potentials.fortran.AT", 
-              ["pygmin/potentials/fortran/AT.f"]),
-    #Extension("pygmin.potentials.fortran.lj_hess", 
-    #          ["pygmin/potentials/fortran/lj_hess.f"]),
-    Extension("pygmin.potentials.fortran.ljpshiftfort", 
-              ["pygmin/potentials/fortran/ljpshiftfort.f"]),
-    Extension("pygmin.potentials.fortran.lj", 
-              ["pygmin/potentials/fortran/lj.f90"]),
-    Extension("pygmin.potentials.fortran.ljcut", 
-              ["pygmin/potentials/fortran/ljcut.f90"]),
-    Extension("pygmin.potentials.fortran.rmdrvt", 
-              ["pygmin/potentials/fortran/rmdrvt.f90"]),
-    Extension("pygmin.potentials.fortran.soft_sphere_pot", 
-              ["pygmin/potentials/fortran/soft_sphere_pot.f90"]),
-            
-    Extension("pygmin.utils._fortran_utils", 
-              ["pygmin/utils/_fortran_utils.f90"]),
+class ModuleList:
+    def __init__(self, **kwargs):
+        self.module_list = []
+        self.kwargs = kwargs
+    def add_module(self, filename):
+        modname = filename.replace("/", ".")
+        modname, ext = os.path.splitext(modname)
+        self.module_list.append(Extension(modname, [filename], **self.kwargs))
 
-    Extension("pygmin.transition_states._orthogoptf", 
-              ["pygmin/transition_states/_orthogoptf.f90"]),
-    Extension("pygmin.transition_states._NEB_utils", 
-              ["pygmin/transition_states/_NEB_utils.f90"]),
-                ]
+f2py_options=[]
+if False:
+    #for bug testing
+    f2py_options=["-DF2PY_REPORT_ON_ARRAY_COPY"]
+
+fmodules = ModuleList(f2py_options=f2py_options)
+fmodules.add_module("pygmin/mindist/overlap.f90")
+fmodules.add_module("pygmin/optimize/mylbfgs_fort.f90")
+fmodules.add_module("pygmin/optimize/mylbfgs_updatestep.f90")
+fmodules.add_module("pygmin/potentials/fortran/AT.f")
+fmodules.add_module("pygmin/potentials/fortran/ljpshiftfort.f")
+fmodules.add_module("pygmin/potentials/fortran/lj.f90")
+fmodules.add_module("pygmin/potentials/fortran/ljcut.f90")
+fmodules.add_module("pygmin/potentials/fortran/rmdrvt.f90")
+fmodules.add_module("pygmin/potentials/fortran/soft_sphere_pot.f90")
+#fmodules.add_module("pygmin/potentials/fortran/lj_hess.f")
+#fmodules.add_module("pygmin/potentials/rigid_bodies/rbutils.f90")
+fmodules.add_module("pygmin/utils/_fortran_utils.f90")
+fmodules.add_module("pygmin/transition_states/_orthogoptf.f90")
+fmodules.add_module("pygmin/transition_states/_NEB_utils.f90")
 
 cxx_modules = [ ]
 
+fortran_modules = fmodules.module_list
 hasboost = True
 if hasboost:
     ext_modules = fortran_modules + cxx_modules
@@ -45,8 +44,8 @@ else:
 
 setup(name='pygmin', 
       version='0.1', 
-      description="Pythin implementation of GMIN, OPTIM, and PATHSAMPLE",
-      url='http://github.com/js850/minGMIN.git',
+      description="Python implementation of GMIN, OPTIM, and PATHSAMPLE",
+      url='http://github.com/js850/PyGMIN.git',
       packages=["pygmin",
                 "pygmin.application",
                 "pygmin.potentials",
