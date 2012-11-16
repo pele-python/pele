27a7148cdf01e5dda1844b8c3f075e1fbc77e274
Jacob Stevenson <js850@cam.ac.uk>
2012-11-16 16:20:43 +0000

    lj.py now only has the fortran implementation
    the other implementations are deleted.  LJ is now pickle-able

--- pygmin/potentials/lj.py
+++ pygmin/potentials/lj.py
@@ -1,6 +1,9 @@
 from math import *
 import numpy as np #to access np.exp() not built int exp
-import potential
+
+from pygmin.potentials.potential import potential as basepot
+import fortran.lj as ljf
+
 
 __all__ = ["LJ"]
 
@@ -14,18 +17,18 @@ def apply_periodic( dr, boxl ):
     #print "dr ",dr
     return dr;
 
-class LJ(potential.potential):
+class LJ(basepot):
     def __init__(self, eps=1.0, sig=1.0, boxl=None):
         """ simple lennard jones potential"""
         self.sig = sig
         self.eps = eps
         self.boxl = boxl
-        if self.boxl == None:
-            self.getSep = self.getSep_abs
+        if self.boxl is None:
+#            self.getSep = self.getSep_abs
             self.periodic = False
             self.boxl = 10000.
         else:
-            self.getSep = self.getSep_periodic
+#            self.getSep = self.getSep_periodic
             self.periodic = True
         print "using Lennard-Jones potential", self.sig, self.eps, 
         if self.periodic: 
@@ -33,136 +36,136 @@ class LJ(potential.potential):
         else:
             print ""
         
-        self.getEnergy = self.getEnergySlow
-        self.getEnergyGradient = self.getEnergyGradientSlow
-        if not self.periodic:
-            try: 
-                import fortran.lj as ljf
-                print "using fast fortran LJ implementation"
-                self.ljf = ljf
-                self.getEnergy = self.getEnergyFortran
-                self.getEnergyGradient = self.getEnergyGradientFortran
-                self.getEnergyGradientList = self.getEnergyGradientListFortran
-                self.getEnergyList = self.getEnergyListFortran
-            except ImportError:
-                try: 
-                    import cpp.ljcpp_ as ljc
-                    print "using fast cpp LJ implementation"
-                    self.ljc = ljc
-                    self.getEnergy = self.getEnergyFast
-                    self.getEnergyGradient = self.getEnergyGradientFast
-                except ImportError:
-                    print "using slow python LJ implementation"
-            #js850> getEnergyGradientList only implemented for fortran
-            try:
-                import fortran.lj as ljf
-                self.ljf = ljf
-                self.getEnergyGradientList = self.getEnergyGradientListFortran
-            except ImportError:
-                pass
-        else:
-            try: 
-                import fortran.lj as ljf
-                print "using fast fortran LJ implementation"
-                self.ljf = ljf
-                self.getEnergy = self.getEnergyFortran
-                self.getEnergyGradient = self.getEnergyGradientFortran
-                self.getEnergyGradientList = self.getEnergyGradientListFortran
-                self.getEnergyList = self.getEnergyListFortran
-            except ImportError:
-                print "using slow python LJ implementation"
+#        self.getEnergy = self.getEnergySlow
+#        self.getEnergyGradient = self.getEnergyGradientSlow
+#        if not self.periodic:
+#            try: 
+#                import fortran.lj as ljf
+#                print "using fast fortran LJ implementation"
+#                self.ljf = ljf
+#                self.getEnergy = self.getEnergyFortran
+#                self.getEnergyGradient = self.getEnergyGradientFortran
+#                self.getEnergyGradientList = self.getEnergyGradientListFortran
+#                self.getEnergyList = self.getEnergyListFortran
+#            except ImportError:
+#                try: 
+#                    import cpp.ljcpp_ as ljc
+#                    print "using fast cpp LJ implementation"
+#                    self.ljc = ljc
+#                    self.getEnergy = self.getEnergyFast
+#                    self.getEnergyGradient = self.getEnergyGradientFast
+#                except ImportError:
+#                    print "using slow python LJ implementation"
+#            #js850> getEnergyGradientList only implemented for fortran
+#            try:
+#                import fortran.lj as ljf
+#                self.ljf = ljf
+#                self.getEnergyGradientList = self.getEnergyGradientListFortran
+#            except ImportError:
+#                pass
+#        else:
+#            try: 
+#                import fortran.lj as ljf
+#                print "using fast fortran LJ implementation"
+#                self.ljf = ljf
+#                self.getEnergy = self.getEnergyFortran
+#                self.getEnergyGradient = self.getEnergyGradientFortran
+#                self.getEnergyGradientList = self.getEnergyGradientListFortran
+#                self.getEnergyList = self.getEnergyListFortran
+#            except ImportError:
+#                print "using slow python LJ implementation"
 
             
 
-    def getSep_periodic(self, vec1, vec2 ):
-        assert len(vec1) == 3, "get_sep: vec length not 3"
-        dr = (vec2-vec1)
-        dr = apply_periodic( dr, self.boxl)
-        R = sqrt((dr**2).sum())
-        #print R, boxl
-        if R > 2./sqrt(3.)*self.boxl:
-            print "warning get_sep may not be working right: R = ", R, "boxl=", self.boxl
-            print "           ", vec1
-            print "           ", vec2
-        return R, dr
+#    def getSep_periodic(self, vec1, vec2 ):
+#        assert len(vec1) == 3, "get_sep: vec length not 3"
+#        dr = (vec2-vec1)
+#        dr = apply_periodic( dr, self.boxl)
+#        R = sqrt((dr**2).sum())
+#        #print R, boxl
+#        if R > 2./sqrt(3.)*self.boxl:
+#            print "warning get_sep may not be working right: R = ", R, "boxl=", self.boxl
+#            print "           ", vec1
+#            print "           ", vec2
+#        return R, dr
+#
+#    def getSep_abs(self, vec1, vec2 ):
+#        assert len(vec1) == 3, "get_sep: vec length not 3"
+#        dr = (vec2-vec1)
+#        R = sqrt((dr**2).sum())
+#        return R, dr
+#
+#    def vij(self, r):
+#        return 4.*self.eps * ( (self.sig/r)**12 - (self.sig/r)**6 )
+#
+#    def dvij(self, r):
+#        return 4.*self.eps * ( -12./self.sig*(self.sig/r)**13 + 6./self.sig*(self.sig/r)**7 )
+#
+#    def getEnergySlow(self, coords):
+#        natoms = coords.size/3
+#        V = np.zeros([natoms*3], np.float64) 
+#        energy=0.
+#        for i in xrange(natoms):
+#            for j in xrange(i+1,natoms):
+#                v1 = coords[i*3:i*3+3]
+#                v2 = coords[j*3:j*3+3]
+#                r,dr = self.getSep(v1,v2)
+#                energy += self.vij(r)
+#        return energy
+#
+#    def getEnergyGradientSlow(self, coords):
+#        natoms = coords.size/3
+#        V = np.zeros([natoms*3], np.float64) 
+#        energy=0.
+#        for i in xrange(natoms):
+#            for j in xrange(i+1,natoms):
+#                v1 = coords[i*3:i*3+3]
+#                v2 = coords[j*3:j*3+3]
+#                r,dr = self.getSep(v1,v2)
+#                energy += self.vij(r)
+#                g = self.dvij(r)
+#                V[i*3:i*3+3] += -g * dr/r
+#                V[j*3:j*3+3] += g * dr/r
+#        return energy,V
+#    
+#    def getEnergyFast(self, coords):
+#        E = self.ljc.energy(coords, self.eps, self.sig)
+#        return E
+#
+#    def getEnergyGradientFast(self, coords):
+#        grad=np.zeros(coords.shape[0], np.float64)
+#        E = self.ljc.gradient(coords, grad, self.eps, self.sig)
+#        return E, grad 
 
-    def getSep_abs(self, vec1, vec2 ):
-        assert len(vec1) == 3, "get_sep: vec length not 3"
-        dr = (vec2-vec1)
-        R = sqrt((dr**2).sum())
-        return R, dr
-
-    def vij(self, r):
-        return 4.*self.eps * ( (self.sig/r)**12 - (self.sig/r)**6 )
-
-    def dvij(self, r):
-        return 4.*self.eps * ( -12./self.sig*(self.sig/r)**13 + 6./self.sig*(self.sig/r)**7 )
-
-    def getEnergySlow(self, coords):
-        natoms = coords.size/3
-        V = np.zeros([natoms*3], np.float64) 
-        energy=0.
-        for i in xrange(natoms):
-            for j in xrange(i+1,natoms):
-                v1 = coords[i*3:i*3+3]
-                v2 = coords[j*3:j*3+3]
-                r,dr = self.getSep(v1,v2)
-                energy += self.vij(r)
-        return energy
-
-    def getEnergyGradientSlow(self, coords):
-        natoms = coords.size/3
-        V = np.zeros([natoms*3], np.float64) 
-        energy=0.
-        for i in xrange(natoms):
-            for j in xrange(i+1,natoms):
-                v1 = coords[i*3:i*3+3]
-                v2 = coords[j*3:j*3+3]
-                r,dr = self.getSep(v1,v2)
-                energy += self.vij(r)
-                g = self.dvij(r)
-                V[i*3:i*3+3] += -g * dr/r
-                V[j*3:j*3+3] += g * dr/r
-        return energy,V
-    
-    def getEnergyFast(self, coords):
-        E = self.ljc.energy(coords, self.eps, self.sig)
-        return E
-
-    def getEnergyGradientFast(self, coords):
-        grad=np.zeros(coords.shape[0], np.float64)
-        E = self.ljc.gradient(coords, grad, self.eps, self.sig)
-        return E, grad 
-
-    def getEnergyFortran(self, coords):
+    def getEnergy(self, coords):
         natoms = len(coords) / 3
-        E = self.ljf.ljenergy(
+        E = ljf.ljenergy(
                 coords, self.eps, self.sig, self.periodic, self.boxl, [natoms])
         return E
 
-    def getEnergyGradientFortran(self, coords):
+    def getEnergyGradient(self, coords):
         natoms = len(coords) / 3
-        E, grad = self.ljf.ljenergy_gradient(
+        E, grad = ljf.ljenergy_gradient(
                 coords, self.eps, self.sig, self.periodic, self.boxl, [natoms])
         return E, grad 
     
-    def getEnergyListFortran(self, coords, ilist):
+    def getEnergyList(self, coords, ilist):
         #ilist = ilist_i.getNPilist()
         #ilist += 1 #fortran indexing
         nlist = len(ilist)
         natoms = len(coords) / 3
-        E = self.ljf.energy_ilist(
+        E = ljf.energy_ilist(
                 coords, self.eps, self.sig, ilist.reshape(-1), self.periodic, 
                 self.boxl, [natoms, nlist])
         #ilist -= 1
         return E
     
-    def getEnergyGradientListFortran(self, coords, ilist):
+    def getEnergyGradientList(self, coords, ilist):
         #ilist = ilist_i.getNPilist()
         #ilist += 1 #fortran indexing
         nlist = len(ilist)
         natoms = len(coords) / 3
-        E, grad = self.ljf.energy_gradient_ilist(
+        E, grad = ljf.energy_gradient_ilist(
                 coords, self.eps, self.sig, ilist.reshape(-1), self.periodic, 
                 self.boxl, [natoms, nlist])
         #ilist -= 1
