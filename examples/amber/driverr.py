baddbbbc4e9e3172017d5143df9f181d576d274c
Sandeep Somani <ss2029@cam.ac.uk>
2013-01-14 13:52:28 -0500

    Amber potential class wrapping over GMIN

--- examples/amber/driverr.py
+++ examples/amber/driverr.py
@@ -1,145 +1,59 @@
-"""
-BH and connections using OpenMM Amber Potential       
 
-26-dec-2012 -- all working here 
- 
-todo: integrate with GUI in guisystem.py  
-"""
-import openMMamberPot as amb  
-import pygmin.utils.amber as readAmb
+from pygmin.systems.amberSystem import AMBERSystem_GMIN, AMBERSystem_OpenMM 
 
-from amberSystem import AMBERsystem  
+# create new amber system
+print '----------------------------------'
+print 'GMIN POTENTIAL' 
+sys   = AMBERSystem_GMIN('coords.prmtop', 'coords.inpcrd')        
+sys.test_potential('coords.pdb')
 
-from pygmin.systems import BaseSystem
-from pygmin.mindist import minPermDistStochastic, MinDistWrapper, ExactMatchCluster
-from pygmin.transition_states import orthogopt
+#print 'OPENmm POTENTIAL' 
+#sysOpenMM  = AMBERSystem_OpenMM('coords.prmtop', 'coords.inpcrd')
+#sysOpenMM.test_potential('coords.pdb')
 
-from simtk.unit import  angstrom 
-    
-'''  ------------------------------------------------------------------- '''
-import numpy as np
-def run_basinhopping(x0):
-    
-    natoms = len(coords)/3 
-#    sysAmb = MySystem(natoms)
-    sysAmb = AMBERsystem(natoms)
+# load existing database 
+from pygmin.storage import Database
+dbcurr = Database(db="aladipep.db")
+
+print "---------id, minener"
+
+for minimum in dbcurr.minima():
+    print minimum._id, minimum.energy    
+
+print "---------id, m1_id, m2_id, tsener"
+for ts in dbcurr.transition_states() :
+    print ts._id, ts._minimum1_id, ts._minimum2_id,  ts.energy      
+            
+# create new database  
+# dbcurr = sys.create_database(db=dbcurr)    
+
+# connect to existing db 
+#    sysOpenMM.create_database(db=dbcurr)    
         
-#    database = sysAmb.create_database()
+# ------- TEST gui 
+from pygmin.gui import run as gr    
+gr.run_gui(sys, db="aladipep.db")
+
+# ------ Test potential 
+sys.test_potential('coords.pdb')
     
-    import pygmin.gui.run as gr
-    gr.run_gui(sysAmb)
+# ------ BH 
+sys.test_BH(dbcurr)
 
+# ------- Connect runs 
+sys.test_connect(dbcurr)  
     
-    from pygmin.takestep import RandomDisplacement, AdaptiveStepsizeTemperature
-    takeStepRnd   = RandomDisplacement( stepsize=2 )
-    tsAdaptive = AdaptiveStepsizeTemperature(takeStepRnd, interval=10, verbose=True)
-
-    sysAmb.params.database.accuracy = 1e-3
-    sysAmb.params.basinhopping["temperature"] = 10.0        
-#    sysAmb.params.basinhopping["insert_rejected"] = True      
-    # todo - how do you save N lowest?    
-
-#    bh = sysAmb.get_basinhopping(database=database, takestep = tsAdaptive, coords=x0)    
-    bh = sysAmb.get_basinhopping(database=database, takestep = takeStepRnd, coords=x0)    
-          
-    # todo -- how to set run parameters like tolerances; 
-    #         have hardcoded insert_rejected in basinhopping.py 
+# ------- Disconn graph  
+sys.test_disconn_graph(dbcurr)  
     
-    print 'Running BH .. '
-    bh.run(5)
-    print "Number of minima found = ", len(database.minima())
-    min0 = database.minima()[0]
-    print "lowest minimum found has energy = ", min0.energy
-    return sysAmb, database
-
-
-
-'''  ------------------------------------------------------------------- '''
-
-def run_double_ended_connect(sys, database):
-    #connect the all minima to the lowest minimum
-    minima = database.minima()
-    min1 = minima[0]
+# ------- Test mindist  
+sys.test_mindist(dbcurr)
     
-#    for min2 in minima[1:]:
-    for min2 in minima[1:]:
-        connect = sys.get_double_ended_connect(min1, min2, database)
-        connect.connect()
 
-'''  ------------------------------------------------------------------- '''
-from pygmin.utils.disconnectivity_graph import DisconnectivityGraph
-from pygmin.landscape import Graph
-import matplotlib.pyplot as plt
 
-def make_disconnectivity_graph(database):
-    graph = Graph(database).graph
-    dg = DisconnectivityGraph(graph, nlevels=3, center_gmin=True)
-    dg.calculate()
-    dg.plot()
-    plt.show()
 
-'''
-  -------------------------------------------------------------------
-'''
-def test_potential(coords):
-    pot = amb.amberPot()       
-    e = pot.getEnergy(coords)
-    print 'Energy (kJ/mol) = '
-    print e
-    
-    e, g = pot.getEnergyGradient(coords)
-    gnum = pot.NumericalDerivative(coords, eps=1e-6)
 
-    print 'Energy (kJ/mol) = '
-    print e 
-    print 'Analytic Gradient = '
-    print g[60:65] 
-    print 'Numerical Gradient = '
-    print gnum[60:65] 
 
-    print 'Num vs Analytic Gradient =' 
-    print np.max(np.abs(gnum-g)), np.max(np.abs(gnum))
-    print np.max(np.abs(gnum-g)) / np.max(np.abs(gnum))
 
-'''
-  ==============================================================
-'''
-if __name__ == "__main__":
-    
-    from pygmin.gui import run as gr    
 
-    # create new amber system 
-    sysAmb   = AMBERsystem('coords.prmtop', 'coords.inpcrd')        
-    # create new database  
-    database = sysAmb.create_database()
-    # run gui 
-    gr.run_gui(sysAmb)
-    
-    exit()
 
-    # read one conformation from pdb file 
-    from simtk.openmm.app import pdbfile as openmmpdb
-    pdb = openmmpdb.PDBFile('coords.pdb')
-    
-    openmmpdb.PDBFile.writeFile(   topology, positions, file, modelIndex)
-    
-    coords = pdb.getPositions() / angstrom   
-    coords = np.reshape(np.transpose(coords), 3*len(coords), 1)
-
-    test_potential(coords)
-        
-    # Basin Hopping     
-    mysys, database = run_basinhopping(coords)
-    
-    # Connect run
-    # run_double_ended_connect(mysys, database)
-    
-    # Disconn 
-    # make_disconnectivity_graph(database)
-
-#    import pygmin.gui.run as gr
-#    gr.run_gui(MySystem)
-    
-    
-    
-    
--- examples/amber/tmp/test.py
+++
@@ -1,8 +0,0 @@
-import pylab as pl
-
-pl.ion()
-pl.plot([1, 2, 3])
-pl.show()
-
-while True:
-    pass
