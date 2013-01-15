4eeacb958d183a8aa06196087e1a70a92877b37f
Victor Ruehle <ruehle@votca.org>
2013-01-15 13:50:20 +0000

    fixed bug with default value in config

--- pygmin/config.py
+++ pygmin/config.py
@@ -3,7 +3,7 @@ from os.path import expanduser
 
 config = ConfigParser.ConfigParser()
 config.add_section("gui")
-config.set("gui", "use_pymol", value=False)
+config.set("gui", "use_pymol", value="False")
 
 print "Open option file",expanduser("~")+'/.pygmin/pygmin.ini'
 config.read(expanduser("~")+'/.pygmin/pygmin.ini')
