0a40f50bd5bee5030fd8d2a450d7cd4c690ed947
Victor Ruehle <ruehle@votca.org>
2013-01-31 17:31:53 +0000

    added some tools for oxdna

---
+++ playground/oxdna/ox_neb.py
@@ -0,0 +1,17 @@
+import sys
+from OpenGL.GLUT import glutInit
+from oxgui import OXDNASystem
+from PyQt4.QtGui import QApplication
+import pickle
+
+glutInit()
+app = QApplication(sys.argv)
+from pygmin.gui.neb_explorer import NEBExplorer
+system = OXDNASystem()
+
+wnd = NEBExplorer(None, system, app)
+wnd.show()
+path = pickle.load(open("interpolate.pickle"))
+wnd.new_neb(path[0], path[-1], path=path, run=False)
+
+sys.exit(app.exec_())
--- playground/oxdna/oxgui.py
+++ playground/oxdna/oxgui.py
@@ -62,5 +62,21 @@ class OXDNASystem(RBSystem):
         
 
 if __name__ == "__main__":
+    import sys
+    from OpenGL.GLUT import glutInit
     
-    gr.run_gui(OXDNASystem, db="oxdna.sqlite")
+    from PyQt4.QtGui import QApplication
+    import pickle
+    
+    glutInit()
+    app = QApplication(sys.argv)
+    from pygmin.gui.neb_explorer import NEBExplorer
+    system = OXDNASystem()
+    
+    wnd = NEBExplorer(None, system, app)
+    wnd.show()
+    path = pickle.load(open("interpolate.pickle"))
+    wnd.new_neb(path[0], path[-1], path=path, run=False)
+    
+    sys.exit(app.exec_())
+    #gr.run_gui(OXDNASystem, db="oxdna.sqlite")
