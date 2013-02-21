4fd817e4e8982832dc9944cf0904dcc80cfb01e9
Victor Ruehle <ruehle@votca.org>
2013-02-21 17:09:30 +0000

    worked on water free energy graphs

--- playground/gmin_tip4p/tip4p_freq.py
+++ playground/gmin_tip4p/tip4p_freq.py
@@ -1,8 +1,10 @@
-from pygmin.thermodynamics import normalmode_frequencies
+from pygmin.thermodynamics import normalmode_frequencies, logproduct_freq2
 from tip4p_system import TIP4PSystem
 from pygmin.angleaxis.aamindist import TransformAngleAxisCluster
 from pygmin.utils import rotations
 import numpy as np
+from pygmin.mindist import PointGroupOrderCluster
+from pygmin.angleaxis import ExactMatchAACluster
 
 system = TIP4PSystem()
 db = system.create_database(db="tip4p_8.sqlite")
@@ -14,69 +16,40 @@ energy = pot.getEnergy(coords)
 #coords = np.loadtxt("2.txt").flatten()
 print energy
 
-site = system.aasystem.sites[0]
-xi = system.aasystem.sites[0].atom_positions
-mi = system.aasystem.sites[0].atom_masses
-M = np.sum(mi)
-metric = np.identity(coords.size)
-
-ca = system.aasystem.coords_adapter(coords)
-S0 = site.Sm.copy()
-
-gn = np.zeros([3,3])
-for xx, mm in zip(xi, mi):
-    y = xx # np.dot(R, xx)
-    gn += mm*(np.dot(y,y)*np.identity(3) - np.outer(y,y))
-
-# construct instantaneous metric tensor
-for i, x, p in zip(xrange(coords.size/6), ca.posRigid, ca.rotRigid):
-    j1 = 3*i
-    j2 = 3*i + ca.nrigid*3
-    metric[j1:j1+3,j1:j1+3] = np.identity(3)*M
-    R = rotations.aa2mx(p)
-    gx, gp = site.metric_tensor(np.zeros(3))
-    #site.Sm = np.dot(R, np.dot(gp, R.transpose()))
-    gp = np.dot(R, np.dot(gp, R.transpose()))
-    gn = np.zeros([3,3])
-    for xx, mm in zip(xi, mi):
-        y = np.dot(R, xx)
-        gn += mm*(np.dot(y,y)*np.identity(3) - np.outer(y,y))
-        
-    metric[j2:j2+3,j2:j2+3] = gn
-
-hess = np.loadtxt('/home/vr274/tip4p_frequencies/test2inst').reshape([coords.size, coords.size])
-
-g_inv = np.linalg.inv(metric)
-A = np.dot(g_inv, hess)
-print np.real(np.linalg.eigvals(A)) #[0:10] 
-#print np.real(np.linalg.norm(np.dot(g_inv, metric) - np.identity(2*n)))
-A = hess  #np.dot(g_inv, hess)
-print "norm", np.linalg.norm(A - A.transpose())
-#exit()
+match = ExactMatchAACluster(system.aasystem)
+get_pgorder = PointGroupOrderCluster(match)
 
 coords = db.minima()[0].coords
-
-hess_read = np.loadtxt('/home/vr274/tip4p_frequencies/test1lab').reshape([coords.size, coords.size])
 hess = pot.NumericalHessian(coords, eps=1e-4)
-
-#hess = hess_read
-
 metric = system.aasystem.metric_tensor(coords)
-frq1 = normalmode_frequencies(hess, metric)
 
-coords = np.loadtxt("2.txt").flatten()
-hess_read = np.loadtxt('/home/vr274/tip4p_frequencies/test2lab').reshape([coords.size, coords.size])
+print get_pgorder(coords)
+frq = normalmode_frequencies(hess, metric)
+print frq
+fvib = logproduct_freq2(frq, 6)[1]
+print fvib
 
-hess = pot.NumericalHessian(coords, eps=1e-4)
-#hess = hess_read
-metric = system.aasystem.metric_tensor(coords)
-frq2 = normalmode_frequencies(hess, metric)
-energy = pot.getEnergy(coords)
-print energy
+beta = 1./2.479
+for m in db.minima():
+    hess = pot.NumericalHessian(m.coords, eps=1e-5)
+    metric = system.aasystem.metric_tensor(m.coords)
 
-print frq1 - frq2
-A = np.dot(np.linalg.inv(metric), hess_read)
-print "-------------------------"
-print np.sort(np.real(frq1))
-print np.sort(np.real(frq2))
-#print np.real(np.linalg.eigvals(A)) #[0:10]
+    pgorder =  get_pgorder(m.coords)
+    frq = normalmode_frequencies(hess, metric, eps=1e-3)
+    fvib = logproduct_freq2(frq, 6, eps=1e-3)[1]
+    print fvib, 0.5*fvib /beta 
+    m.pgorder = pgorder
+    m.fvib = fvib 
+
+for ts in db.transition_states():
+    hess = pot.NumericalHessian(ts.coords, eps=1e-5)
+    metric = system.aasystem.metric_tensor(ts.coords)
+
+    pgorder = get_pgorder(ts.coords)
+    frq = normalmode_frequencies(hess, metric, eps=1e-3)
+    fvib = logproduct_freq2(frq, 6, nnegative=1, eps=1e-3)[1]
+    print fvib, 0.5*fvib /beta
+    ts.pgorder = pgorder
+    ts.fvib = fvib 
+
+db.session.commit()
