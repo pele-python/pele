89800270d2c0ff45ab29acc3b97b29e0c6e1bcf9
Jacob Stevenson <js850@cam.ac.uk>
2013-02-22 13:45:06 +0000

    updates to OTP system in playground

--- playground/otp/otp.py
+++ playground/otp/otp.py
@@ -1,18 +1,16 @@
+import tempfile
+from copy import deepcopy
+
 import numpy as np
 from numpy import cos, sin, pi
-from pygmin.potentials import GMINPotential
+
 import gmin_ as GMIN
-from copy import deepcopy
+
+from pygmin.potentials import GMINPotential
 from pygmin.potentials import LJ
 from pygmin.angleaxis import RBTopology, RBSystem, RigidFragment
+from pygmin.utils.xyz import write_xyz
 
-def make_otp():
-    otp = RigidFragment()
-    otp.add_atom("O", np.array([0.0, -2./3 * np.sin( 7.*pi/24.), 0.0]), 1.)
-    otp.add_atom("O", np.array([cos( 7.*pi/24.),  1./3. * sin( 7.* pi/24.), 0.0]), 1.)
-    otp.add_atom("O", np.array([-cos( 7.* pi/24.),  1./3. * sin( 7.*pi/24), 0.0]), 1.)
-    otp.finalize_setup()
-    return otp
 
 class OTPPotential(LJ):
     def __init__(self, aatopology):
@@ -31,6 +29,7 @@ class OTPPotential(LJ):
 
 class OTPSystem(RBSystem):
     def __init__(self, nmol, boxl=None):
+#        super(OTPSystem, self)
         self.nmol = nmol
         if boxl is None:
             self.periodic = False
@@ -38,7 +37,43 @@ class OTPSystem(RBSystem):
         else:
             self.periodic = True
             self.boxl = boxl
-        RBSystem.__init__(self)
+        super(OTPSystem, self).__init__()
+        
+        self.setup_params(self.params)
+
+    def get_rigid_fragment(self):
+        return RigidFragment()
+
+    def make_otp(self):
+        otp = self.get_rigid_fragment()
+        otp.add_atom("O", np.array([0.0, -2./3 * np.sin( 7.*pi/24.), 0.0]), 1.)
+        otp.add_atom("O", np.array([cos( 7.*pi/24.),  1./3. * sin( 7.* pi/24.), 0.0]), 1.)
+        otp.add_atom("O", np.array([-cos( 7.* pi/24.),  1./3. * sin( 7.*pi/24), 0.0]), 1.)
+        otp.finalize_setup()
+        return otp
+
+    
+    def setup_params(self, params):
+        params.gui["basinhopping_nsteps"] = 1000
+        
+        nebparams = params.double_ended_connect.local_connect_params.NEBparams
+        nebparams.max_images = 50
+        nebparams.image_density = 5
+        nebparams.iter_density = 10.
+        nebparams.k = 5.
+        nebparams.reinterpolate = 50
+        
+        nebparams.NEBquenchParams["iprint"] = 10
+        
+        
+        tssearch = params.double_ended_connect.local_connect_params.tsSearchParams
+        tssearch.nsteps_tangent1 = 10
+        tssearch.nsteps_tangent2 = 30
+        tssearch.lowestEigenvectorQuenchParams["nsteps"] = 50
+        tssearch.iprint=1
+        tssearch.nfail_max = 100
+        
+        
     
     def get_random_coordinates(self):
         coords = np.zeros([self.nmol*2, 3])
@@ -70,7 +105,7 @@ class OTPSystem(RBSystem):
         self.nrigid = coords.size/6
         assert(self.nrigid == self.nmol)
         #self.nrigid = self.nmol
-        otp = make_otp()
+        otp = self.make_otp()
         topology = RBTopology()
         topology.add_sites([deepcopy(otp) for i in xrange(self.nrigid)])
         
@@ -92,13 +127,82 @@ class OTPSystem(RBSystem):
     def __call__(self):
         return self
 
+    def load_coords_pymol(self, coordslist, oname, index=1):
+        """load the coords into pymol
+        
+        the new object must be named oname so we can manipulate it later
+                        
+        Parameters
+        ----------
+        coordslist : list of arrays
+        oname : str
+            the new pymol object must be named oname so it can be manipulated
+            later
+        index : int
+            we can have more than one molecule on the screen at one time.  index tells
+            which one to draw.  They are viewed at the same time, so should be
+            visually distinct, e.g. different colors.  accepted values are 1 or 2
+        
+        Notes
+        -----
+        the implementation here is a bit hacky.  we create a temporary xyz file from coords
+        and load the molecule in pymol from this file.  
+        """
+        # pymol is imported here so you can do, e.g. basinhopping without installing pymol
+        import pymol 
+
+        # create the temporary file
+        suffix = ".xyz"
+        f = tempfile.NamedTemporaryFile(mode="w", suffix=suffix)
+        fname = f.name
+                
+        # write the coords into the xyz file
+        from pygmin.mindist import CoMToOrigin
+        for coords in coordslist:
+            if hasattr(self, "atom_types"):
+                atom_types = self.atom_types
+            else:
+                atom_types = ["O"]
+            atom_coords = self.aasystem.to_atomistic(coords)
+#            atom_coords = CoMToOrigin(coords.copy())
+            write_xyz(f, atom_coords, title=oname, atomtypes=atom_types)#["C", "N", "N"])
+        f.flush()
+                
+        # load the molecule from the temporary file
+        pymol.cmd.load(fname)
+        
+        # get name of the object just create and change it to oname
+        objects = pymol.cmd.get_object_list()
+        objectname = objects[-1]
+        pymol.cmd.set_name(objectname, oname)
+        
+        #set the representation
+        pymol.cmd.hide("everything", oname)
+        pymol.cmd.show("spheres", oname)
+        
+        
+        pymol.cmd.unbond(oname, oname)
+        
+        if hasattr(self, "draw_bonds"):
+            for i1, i2 in self.draw_bonds:
+                pymol.cmd.bond("id "+str(i1+1), "id "+str(i2+1))
+            pymol.cmd.show("lines", oname)
+
+        # set the color according to index
+        if index == 1:
+            pymol.cmd.color("red", oname)
+        else:
+            pymol.cmd.color("gray", oname)
+
+        pymol.cmd.set("sphere_scale", value=0.2, selection=oname)
+
 def rungui(system, db=None):
     import pygmin.gui.run as gr
     from pygmin.storage import Database
     gr.run_gui(system, db=db)
     
 if __name__ == "__main__":
-    nmol = 6
+    nmol = 7
     periodic = False
     if periodic:
         boxl = 10.
