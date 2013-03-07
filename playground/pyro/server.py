eb9b0806de425419256d38077ee9717e6dceabb3
Victor Ruehle <ruehle@votca.org>
2013-03-06 20:16:54 -0500

    generalized client/server code and added concurrent module

--- playground/pyro/server.py
+++ playground/pyro/server.py
@@ -1,51 +1,11 @@
 from random import choice
-import Pyro4
 from pygmin.systems import LJCluster
 from pygmin.storage import Minimum
+from pygmin.concurrent import RandomConnectServer
 
-manager_name = "ljconnect_example"
+server_name = "ljconnect_example"
 hostname="localhost"
 port=11567
-
-# we need to run pyros in multiplex mode, otherwise we run into problems with 
-# SQLAlchemy. This is due to the fact that a session can only be used from one
-# thread. We really should fix this issue and allow for multiple sessions!
-Pyro4.config.SERVERTYPE = "multiplex"
-
-
-class ConnectManager(object):
-    ''' Manager which decides which connect jobs to run '''
-    
-    def __init__(self, system, database):
-        self.system = system
-        self.db = database
-        
-    def get_connect_job(self):
-        ''' get a new connect job '''
-        print "new connect job assigned to worker"
-        minima = self.db.minima()
-        min1 = choice(minima)
-        min2 = choice(minima)
-        
-        return min1._id, min1.coords, min2._id, min2.coords
-
-    def get_system(self):
-        ''' get the system class '''
-        return self.system
-    
-    def add_minimum(self, E, coords):
-        ''' called by worker if a new minimum is found '''
-        print "a client found a minimum", E
-        return self.db.addMinimum(E, coords)._id
-    
-    def add_ts(self, id1, id2, E, coords):
-        ''' called by worker if a new transition state is found '''
-        
-        print "a client found a transition state", E
-        min1 = self.db.session.query(Minimum).filter(Minimum._id == id1).one()
-        min2 = self.db.session.query(Minimum).filter(Minimum._id == id2).one()
-        
-        return self.db.addTransitionState(E, coords, min1, min2)._id
 
 print "setting up LJ38 with database lj38.sqlite"
 system = LJCluster(38)
@@ -61,14 +21,7 @@ print "Working on %d minima"%db.session.query(Minimum).count()
 print "Running basinhopping to generate initial database"
         
 print "Creating new connect manager"
-connect_manager=ConnectManager(system, db)
+connect_manager=RandomConnectServer(system, db, server_name=server_name,
+                                     host=hostname, port=port)
 
-print "Starting Pyros daemon"
-daemon=Pyro4.Daemon(host=hostname, port=port)
-
-# make the connect_manager available to Pyros childs
-uri=daemon.register(connect_manager, objectId=manager_name)
-print "The connect manager can be accessed by the following uri: ", uri 
-
-print "Ready to accept connections"
-daemon.requestLoop() 
+connect_manager.run()
--- playground/pyro/worker.py
+++ playground/pyro/worker.py
@@ -1,8 +1,6 @@
-from pygmin.storage import Minimum, TransitionState
-import Pyro4
+from pygmin.concurrent import RandomConnectWorker
 
 nruns = 5
-
 manager_name = "ljconnect_example"
 hostname = "localhost"
 port = 11567
@@ -10,49 +8,6 @@ port = 11567
 print "I will do %d connect runs before exiting"%nruns
 
 uri="PYRO:%s@%s:%d"%(manager_name,hostname,port)
-print "connecting to",uri
-connect_manager=Pyro4.Proxy(uri)
 
-system = connect_manager.get_system()
-pot = system.get_potential()
-
-# global minimum id
-gid = {}
-
-# forwarding new minima to server
-def minimum_added(minimum):
-    minid = connect_manager.add_minimum(minimum.energy, minimum.coords)
-    # store the id of minimum on serverside for quick access later on
-    gid[minimum] = minid
-    print "added minimum %d as %d"%(minimum._id, minid)
-    
-# forwarding new ts to server 
-def ts_added(ts):
-    id1 = gid[ts.minimum1]
-    id2 = gid[ts.minimum2]
-    
-    tsid = connect_manager.add_ts(id1, id2, ts.energy, ts.coords)
-
-# create a fake database in memory
-db = system.create_database(db=":memory:")
-
-# connect to events and forward them to server
-db.on_minimum_added.connect(minimum_added)
-db.on_ts_added.connect(ts_added)
-    
-for i in xrange(nruns):
-    print "Obtain a new job"
-    id1, coords1, id2, coords2 = connect_manager.get_connect_job()
-    
-    print "add minima to local database"
-    min1 = db.addMinimum(pot.getEnergy(coords1), coords1)
-    min2 = db.addMinimum(pot.getEnergy(coords2), coords2)
-    
-    print "run double ended connect"
-    connect = system.get_double_ended_connect(min1, min2, db, fresh_connect=True)
-    connect.connect()
-
-print "finished sucessfully!"
-print "Data collected during run:"
-print db.session.query(Minimum).count(), "minima"
-print db.session.query(TransitionState).count(), "transition states"
+worker = RandomConnectWorker(uri)
+worker.run()
