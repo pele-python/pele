2c1ffe93f5e615475b8cf7c77b4f628362399105
Victor Ruehle <ruehle@votca.org>
2013-04-05 08:16:33 -0400

    removed dmacrys example

--- examples/dmacrys/tetrol_app.py
+++
@@ -1,7 +0,0 @@
-from pygmin.utils.dmagmin import AppDMAGMINBH
-
-app = AppDMAGMINBH()
-#app.quenchParameters["iprint"] = 1
-#app.overlap_cutoff = 1.1
-app.execute()
-#Qu   0 E= -107.867110776 quench_steps= 143 RMS= 5.50874938278e-05 Markov E= 114.506858602 accepted= True
