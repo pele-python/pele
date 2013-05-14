d665167afdec9be258ef918a7a65fe319a049271
Jacob Stevenson <js850@cam.ac.uk>
2013-05-14 14:54:39 +0100

    redo optim_converter reading the binary data directly

--- scripts/optim_converter.py
+++ scripts/optim_converter.py
@@ -1,109 +1,127 @@
-import structure_read
 import numpy as np
-from pygmin.storage import Database, Minimum, TransitionState
-# import sys
-# import getopt
+from pygmin.storage import Database
 import argparse
 
-def file_len(fname):
-    with open(fname) as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
+def read_points_min_ts(fname, natoms=None, endianness="="):
+    """
+    the files were written with fortran WRITE options 
+    
+    ACCESS='DIRECT',FORM='UNFORMATTED',STATUS='UNKNOWN',RECL=NDUMMY
+    
+    This means the data is stored without any header information.  
+    It is just a long list of double precision floating point numbers.
+    
+    Note that some fortran compilers use different endiness for the data.  If
+    the coordinates comes out garbage this is probably the problem.  The solution
+    is to pass a different data type
+    
+    dtype=np.dtype("<d")  # for little-endian double precision
+    dtype=np.dtype(">d")  # for big-endian double precision
+    
+    Parameters
+    ----------
+    fname : str
+        filenname to read from
+    natoms : int, optional
+        for testing to make sure the number of floats read is a multiple of natoms
+    endianness : str
+        define the endianness of the data. can be "=", "<", ">" 
+     
+    """
+    with open(fname, "rb") as fin:
+        coords = np.fromfile(fin, dtype=np.dtype(endianness+"d"))
+    if natoms is not None:
+        if len(coords) % natoms != 0:
+                raise Exception("number of double precision variables read from %s is not divisible by natoms (%d)" % 
+                                (fname, natoms) )
+    print coords
+    return coords.flatten()
 
 class Convert(object):
     '''
     Converts old OPTIM to pygmin database
     '''
-    def __init__(self,db_name=None):
-        if not db_name: db_name = "Database.db"
-        self.db_name = db_name
-        self.db = None
-        self.natoms = None
-        self.mindata = None
-        self.tsdata = None
-        self.pointsmin = None
-        self.pointsts = None
-                
-    def setNatoms(self,natoms):
+    def __init__(self, natoms, db_name="Database.db", mindata="min.data", 
+                  tsdata="ts.data", pointsmin="points.min", pointsts="points.ts",
+                  endianness="="):
         self.natoms = natoms
-        
-    def setMindata(self,mindata=None):
-        if not mindata: mindata = 'min.data'
+        self.db_name = db_name
         self.mindata = mindata
-        
-    def setTsdata(self,tsdata=None):
-        if not tsdata: tsdata = 'ts.data'
         self.tsdata = tsdata
-        
-    def setPointsmin(self,pointsmin=None):
-        if not pointsmin: pointsmin = 'points.min'
         self.pointsmin = pointsmin
-        
-    def setPointsts(self,pointsts=None):
-        if not pointsts: pointsts = 'points.ts'
         self.pointsts = pointsts
-        
-    def initDatabase(self):
+        self.endianness = endianness
+
         self.db = Database(self.db_name)
-        
+                
     def setAccuracy(self,accuracy = 0.000001):
         self.db.accuracy = accuracy
         
     def ReadMindata(self):
-        indx = 1
-        f_len = file_len(self.mindata)
+        print "reading from", self.mindata
+        indx = 0
+#        f_len = file_len(self.mindata)
+        self.index2min = dict()
         for line in open(self.mindata,'r'):
             sline = line.split()
-            coords = structure_read.structure_read(self.pointsmin,
-                                                   indx,
-                                                   self.natoms)
-#             coords = np.random.random(3*self.natoms)
-            e, f = map(float,sline[:2])
-            pg = int(sline[2])
             
-            min1 = Minimum(e, coords)
+            # get the coordinates corresponding to this minimum
+            coords = self.pointsmin_data[indx,:]
             
-            min1.fvib = f
+            # read data from the min.data line            
+            e, fvib = map(float,sline[:2]) # energy and vibrational free energy
+            pg = int(sline[2]) # point group order
+            
+            # create the minimum object and attach the data
+            min1 = self.db.addMinimum(e, coords, commit=False) 
+            min1.fvib = fvib
             min1.pgorder = pg
             
-            min1.op = 1.0
-            
-            self.db.session.add(min1)
+            self.index2min[indx] = min1
 
             indx += 1
 
     def ReadTSdata(self):
-        indx = 1
-        f_len = file_len(self.tsdata)
+        print "reading from", self.tsdata
+
+        indx = 0
         for line in open(self.tsdata,'r'):
             sline = line.split()
 
-            coords = structure_read.structure_read(self.pointsts,
-                                                   indx,          
-                                                   self.natoms)
-#             coords = np.random.random(3*self.natoms)
-            e, f = map(float,sline[:2])
-            pg = int(sline[2])
-            m1, m2 = map(int,sline[3:5])
-            
-            min1 = self.db.getMinimum(m1)
-            min2 = self.db.getMinimum(m2)
-            trans = TransitionState(e, coords, min1, min2)
-            
-            
-            trans.fvib = f
-            trans.pgorder = pg
-            
-            self.db.session.add(trans)
+            # get the coordinates corresponding to this minimum
+            coords = self.pointsts_data[indx,:]
 
+            # read data from the min.ts line            
+            e, fvib = map(float, sline[:2]) # get energy and fvib
+            pg = int(sline[2]) # point group order
+            m1indx, m2indx = map(int, sline[3:5]) 
+            
+            min1 = self.index2min[m1indx - 1] # minus 1 for fortran indexing
+            min2 = self.index2min[m2indx - 1] # minus 1 for fortran indexing
+
+            trans = self.db.addTransitionState(e, coords, min1, min2, commit=False)
+            
+            trans.fvib = fvib
+            trans.pgorder = pg
             
             indx += 1
         
+    def read_points_min(self):
+        print "reading from", self.pointsmin
+        coords = read_points_min_ts(self.pointsmin, self.natoms, endianness=self.endianness)
+        self.pointsmin_data = coords.reshape([-1, self.natoms])
+
+    def read_points_ts(self):
+        print "reading from", self.pointsts
+        coords = read_points_min_ts(self.pointsts, self.natoms, endianness=self.endianness)
+        self.pointsts_data = coords.reshape([-1, self.natoms])
+                  
         
     def Convert(self):
-        
+        self.read_points_min()
         self.ReadMindata()
+        
+        self.read_points_ts()
         self.ReadTSdata()
         
     def getDatabase(self):
@@ -113,31 +131,38 @@ class Convert(object):
 
     
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+def main():
+    parser = argparse.ArgumentParser(description="""
+convert an OPTIM database to a pygmin sqlite database.  Four files are needed.  Normally they are called:
+
+    points.min : the coordinates of the minima in binary format
+    min.data   : additional information about the minima (like the energy)
+    points.ts  : the coordinates of the transition states
+    min.ts     : additional information about transition states (like which minima they connect)
+
+Other file names can optionally be passed.
+    """, formatter_class=argparse.RawDescriptionHelpFormatter)
+    
     parser.add_argument('natoms', help='Number of atoms in system', type = int)
-    parser.add_argument('--Database','-d', help = 'Name of database to write into', type = str)
-    parser.add_argument('--Mindata','-m', help = 'Name of min.data file', type = str)
-    parser.add_argument('--Tsdata','-t', help = 'Name of ts.data file', type = str)
-    parser.add_argument('--Pointsmin','-p', help = 'Name of points.min file', type = str)
-    parser.add_argument('--Pointsts','-q', help = 'Name of points.ts file', type = str)
+    parser.add_argument('--Database','-d', help = 'Name of database to write into', type = str, default="optimdb.sqlite")
+    parser.add_argument('--Mindata','-m', help = 'Name of min.data file', type = str, default="min.data")
+    parser.add_argument('--Tsdata','-t', help = 'Name of ts.data file', type = str, default="ts.data")
+    parser.add_argument('--Pointsmin','-p', help = 'Name of points.min file', type = str, default="points.min")
+    parser.add_argument('--Pointsts','-q', help = 'Name of points.ts file', type = str, default="points.ts")
+    parser.add_argument('--endianness', help = 'set the endianness of the binary data.  Can be "<" for little or ">" for big-endian', type = str, default="=")
     args = parser.parse_args()
     
-    cv = Convert(args.Database)
+    cv = Convert(args.natoms, db_name=args.Database, mindata=args.Mindata, 
+                 tsdata=args.Tsdata, pointsmin=args.Pointsmin, pointsts=args.Pointsts,
+                 endianness=args.endianness)
 
-    cv.initDatabase()
     cv.setAccuracy()
-    
-    cv.setNatoms(args.natoms)
-     
-    cv.setMindata(args.Mindata)
-    cv.setTsdata(args.Tsdata)
-    cv.setPointsts(args.Pointsmin)
-    cv.setPointsmin(args.Pointsts)
      
     cv.Convert()
     cv.db.session.commit()
 
 
     
+if __name__ == "__main__":
+    main()
 
