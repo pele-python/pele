15d43f9824627a9c044edaa1d73c3688cb7e2d5d
Kyle Sutherland-Cash <khs26@cam.ac.uk>
2013-07-23 16:14:51 +0100

    Committed AMBER changes.

--- examples/amber/driverr.py
+++ examples/amber/driverr.py
@@ -39,7 +39,27 @@ sys.test_potential('coords.pdb')
 start = time.clock()
 sys.test_BH(dbcurr,100)
 elapsed = (time.clock() - start)
-print "time taken by BH = ", elapsed 
+#print "time taken by BH = ", elapsed 
+
+start = time.clock()
+group_rotation_parameters = {}
+group_rotation_parameters["NME"] = {}
+group_rotation_parameters["NME"]["bond_atom_1"] = 8
+group_rotation_parameters["NME"]["bond_atom_2"] = 14
+group_rotation_parameters["NME"]["group_atoms"] = 15, 16, 17, 18, 19, 20, 21
+group_rotation_parameters["NME"]["max_angle_magnitude"] = 0.3
+group_rotation_parameters["NME"]["selection_probability"] = 1.0
+group_rotation_parameters["ACE"] = {}
+group_rotation_parameters["ACE"]["bond_atom_1"] = 8
+group_rotation_parameters["ACE"]["bond_atom_2"] = 6
+group_rotation_parameters["ACE"]["group_atoms"] = 0, 1, 2, 3, 4, 5, 7 
+group_rotation_parameters["ACE"]["max_angle_magnitude"] = 0.3
+group_rotation_parameters["ACE"]["selection_probability"] = 1.0
+sys.test_BH_group_rotation(dbcurr,100, group_rotation_parameters)
+elapsed2 = (time.clock() - start)
+print "time taken by BH = ", elapsed
+print "time taken by BH with group rotation = ", elapsed2
+
 
 exit() 
 # ------- Connect runs 
