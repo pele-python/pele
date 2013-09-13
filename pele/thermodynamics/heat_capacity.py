fix mean energy calculation in HSA heat capacity
also fix <U^2>

The heat capacity is unchanged

pele/thermodynamics/heat_capacity.py
@@ -49,14 +49,14 @@ def dos_to_cv(energies, ldos, T, K=1.):
 
     return lZ, U, U2, Cv
 
-def minima_to_cv(minima, T, k):
+def minima_to_cv(minima, kT, k):
     """compute the heat capacity and other thermodynamic quantities from a list of minima using the harmonic approximation
     
     Parameters
     ----------
     minima : list of Minimum objects
         mimima from which to compute the thermodynamic computations
-    T : array
+    kT : array
         Temperatures at which to do the calculations.  Should be normalized by the 
         Bolzmann constant (you should pass k_B*T)
     k : int
@@ -67,13 +67,13 @@ def minima_to_cv(minima, T, k):
     See DJW Energy Landscapes book page 371
     Za = P * exp(-beta Ea) / (beta * h * nu_bar)**k / O_a
     """
-    beta = np.array(1./T)
+    beta = np.array(1./kT)
     k = float(k)
     energies = np.array([m.energy for m in minima])
     
     # compute the log of the terms in the partition function
     try:
-        lZterms = np.array([-beta * m.energy - m.fvib/2. - np.log(m.pgorder) for m in minima])
+        lZterms = np.array([-beta * m.energy - 0.5 * m.fvib - np.log(m.pgorder) for m in minima])
     except TypeError or AttributeError:
         print "Error reading thermodynamic data from minima.  Have you computed the normal mode"\
               " frequencies and point group order for all the minima?  See pele.thermodynamics "\
@@ -89,14 +89,21 @@ def minima_to_cv(minima, T, k):
     # compute Z, <E> and <E**2>
     Zpref = np.exp(lZterms)
     Z  = np.sum(Zpref, axis=1 )
-    U  = np.sum(Zpref * energies[np.newaxis,:], axis=1 )
-    U2 = np.sum(Zpref * energies[np.newaxis,:]**2, axis=1 )
-    U /= Z
-    U2 /= Z
     
+    # compute the mean energy of the occupied minimum as a function of temperature
+    V  = np.sum(Zpref * energies[np.newaxis,:], axis=1 )
+    V2 = np.sum(Zpref * energies[np.newaxis,:]**2, axis=1 )
+    V /= Z
+    V2 /= Z
     
-    # compute the heat capacity  
-    Cv = k + (U2 - U**2) * beta**2
+    # compute the heat capacity
+    Cv = 0.5 * k + (V2 - V**2) * beta**2
+    # add in the contribution from the momentum degrees of freedom
+    Cv += 0.5 * k
+
+    # compute the mean potential energy 
+    U2 = V2 + V * k / beta + (0.5 * k + 0.25 * k**2) / beta**2
+    U = V + 0.5 * k / beta
 
     lZ = np.log(Z) + lZmax
     