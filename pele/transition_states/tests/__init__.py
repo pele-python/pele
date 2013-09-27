cf0dd1f02bce09374a70b8b69c4f6c40c6b847d7
Jacob Stevenson <js850@cam.ac.uk>
2013-09-27 15:20:16 +0100

    add a test for LowestEigPot

---
+++ pele/transition_states/tests/__init__.py
@@ -0,0 +1,4 @@
+from _test_eig_pot import *
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ pele/transition_states/tests/_test_eig_pot.py
@@ -0,0 +1,28 @@
+import unittest
+import numpy as np
+
+from pele.systems import LJCluster, LJClusterFrozen, ljcluster_frozen
+from pele.transition_states.find_lowest_eig import LowestEigPot
+
+class TestEigPot(unittest.TestCase):
+    def setUp(self):
+        natoms = 18
+#        s = LJCluster(natoms)
+#        nfrozen = 6
+#        reference_coords = s.get_random_configuration()
+#        self.system = LJClusterFrozen(13, range(nfrozen), reference_coords)
+        self.system = LJCluster(natoms)
+        self.x = self.system.get_random_configuration()
+        self.pot = self.system.get_potential()
+        self.eigpot = LowestEigPot(self.x, self.pot, orthogZeroEigs=self.system.get_orthogonalize_to_zero_eigenvectors())
+    
+    def test_gradient(self):
+        e1 = self.eigpot.getEnergy(self.x)
+        e, g = self.eigpot.getEnergyGradient(self.x)
+        gnum = self.eigpot.NumericalDerivative(self.x)
+        
+        self.assertLess(np.max(np.abs(g-gnum)) / np.max(np.abs(g)), 1e-6)
+        self.assertAlmostEqual(e, e1, 6)
+        
+if __name__ == "__main__":
+    unittest.main()
