8013920c1272a56c587a2a4f539e4380f464f3c5
Victor Ruehle <ruehle@votca.org>
2013-10-21 16:34:22 +0100

    added missing file

---
+++ playground/native_code/potentialfunction.h
@@ -0,0 +1,25 @@
+#include "include/array.h"
+#include "include/potential.h"
+
+namespace pele {
+
+class PotentialFunction : public Potential
+{
+public:
+	typedef double EnergyCallback(Array, void *);
+	typedef double EnergyGradientCallback(Array, Array, void *);
+
+	PotentialFunction(EnergyCallback *get_energy, EnergyGradientCallback *get_energy_gradient, void *userdata)
+		:	_get_energy(get_energy), _get_energy_gradient(get_energy_gradient), _userdata(userdata) {}
+
+	virtual double get_energy(Array x) { return (*_get_energy)(x, _userdata); } ;
+	virtual double get_energy_gradient(Array x, Array grad) {  return (*_get_energy_gradient)(x, grad, _userdata); }
+
+private:
+	EnergyCallback *_get_energy;
+	EnergyGradientCallback *_get_energy_gradient;
+	void *_userdata;
+};
+
+
+}
