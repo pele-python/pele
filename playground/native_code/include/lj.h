088dfd058763ff559f4c5decdfb86ba1711f4479
Jacob Stevenson <js850@cam.ac.uk>
2013-10-26 12:35:09 +0100

    change tabs to 4 spaces in c++ code

--- playground/native_code/include/lj.h
+++ playground/native_code/include/lj.h
@@ -18,86 +18,86 @@
 #include "distance.h"
 namespace pele {
 
-	/**
+    /**
    * Pairwise interaction for lennard jones 
    */
-	struct lj_interaction {
-		double const _C6, _C12;
-		double const _6C6, _12C12;
-		lj_interaction(double C6, double C12) : 
-      _C6(C6), _C12(C12),
-      _6C6(6.*_C6), _12C12(12.*_C12) 
-    {}
+    struct lj_interaction {
+        double const _C6, _C12;
+        double const _6C6, _12C12;
+        lj_interaction(double C6, double C12) : 
+            _C6(C6), _C12(C12),
+            _6C6(6.*_C6), _12C12(12.*_C12) 
+        {}
 
-		/* calculate energy from distance squared */
-		double energy(double r2) const {
-			double ir2 = 1.0/r2;
-			double ir6 = ir2*ir2*ir2;
-			double ir12 = ir6*ir6;
+        /* calculate energy from distance squared */
+        double energy(double r2) const {
+            double ir2 = 1.0/r2;
+            double ir6 = ir2*ir2*ir2;
+            double ir12 = ir6*ir6;
 
-			return -_C6*ir6 + _C12*ir12;
-		}
+            return -_C6*ir6 + _C12*ir12;
+        }
 
-		/* calculate energy and gradient from distance squared, gradient is in g/|rij| */
-		double energy_gradient(double r2, double *gij) const {
-			double ir2 = 1.0/r2;
-			double ir6 = ir2*ir2*ir2;
-			double ir12 = ir6*ir6;
+        /* calculate energy and gradient from distance squared, gradient is in g/|rij| */
+        double energy_gradient(double r2, double *gij) const {
+            double ir2 = 1.0/r2;
+            double ir6 = ir2*ir2*ir2;
+            double ir12 = ir6*ir6;
 
-			*gij = (_12C12 * ir12 - _6C6 * ir6) * ir2;
-			return -_C6*ir6 + _C12*ir12;
-		}
-	};
+            *gij = (_12C12 * ir12 - _6C6 * ir6) * ir2;
+            return -_C6*ir6 + _C12*ir12;
+        }
+    };
 
-	/**
+    /**
    * Define a pairwise interaction for lennard jones with a cutoff.  The
    * potential goes smoothly to zero using a second order
    * polynomial.
    */
-	struct lj_interaction_cut_smooth {
-		double const _C6, _C12;
-		double const _6C6, _12C12;
-    double const _rcut2;
-    double const _A0;
-    double const _A2;
-    double const _2A2;
-		lj_interaction_cut_smooth(double C6, double C12, double rcut) 
-      : 
-        _C6(C6), _C12(C12), 
-        _6C6(6.*_C6), _12C12(12.*_C12),
-        _rcut2(rcut*rcut),
-        //A0 = 4.0*(sig**6/rcut**6) - 7.0*(sig**12/rcut**12)
-        _A0( 4.*_C6 / (_rcut2*_rcut2*_rcut2) - 7.*_C12/(_rcut2*_rcut2*_rcut2*_rcut2*_rcut2*_rcut2)),
-        //A2 = (-3.0*(sig6/rcut**8) + 6.0*(sig12/rcut**14))
-        _A2( -3.*_C6 / (_rcut2*_rcut2*_rcut2*_rcut2) + 6.*_C12/(_rcut2*_rcut2*_rcut2*_rcut2*_rcut2*_rcut2*_rcut2)),
-        _2A2(2.*_A2)
-    {}
+    struct lj_interaction_cut_smooth {
+        double const _C6, _C12;
+        double const _6C6, _12C12;
+        double const _rcut2;
+        double const _A0;
+        double const _A2;
+        double const _2A2;
+        lj_interaction_cut_smooth(double C6, double C12, double rcut) 
+            : 
+                _C6(C6), _C12(C12), 
+                _6C6(6.*_C6), _12C12(12.*_C12),
+                _rcut2(rcut*rcut),
+                //A0 = 4.0*(sig**6/rcut**6) - 7.0*(sig**12/rcut**12)
+                _A0( 4.*_C6 / (_rcut2*_rcut2*_rcut2) - 7.*_C12/(_rcut2*_rcut2*_rcut2*_rcut2*_rcut2*_rcut2)),
+                //A2 = (-3.0*(sig6/rcut**8) + 6.0*(sig12/rcut**14))
+                _A2( -3.*_C6 / (_rcut2*_rcut2*_rcut2*_rcut2) + 6.*_C12/(_rcut2*_rcut2*_rcut2*_rcut2*_rcut2*_rcut2*_rcut2)),
+                _2A2(2.*_A2)
+        {}
 
-		/* calculate energy from distance squared */
-		double energy(double r2) const {
-      if (r2 >= _rcut2) {
-        return 0.;
-      }
-			double ir2 = 1.0/r2;
-			double ir6 = ir2*ir2*ir2;
-			double ir12 = ir6*ir6;
+        /* calculate energy from distance squared */
+        double energy(double r2) const {
+            if (r2 >= _rcut2) {
+                return 0.;
+            }
+            double ir2 = 1.0/r2;
+            double ir6 = ir2*ir2*ir2;
+            double ir12 = ir6*ir6;
 
-			return -_C6*ir6 + _C12*ir12 + _A0 + _A2*r2;
-		}
+            return -_C6*ir6 + _C12*ir12 + _A0 + _A2*r2;
+        }
 
-		/* calculate energy and gradient from distance squared, gradient is in g/|rij| */
-		double energy_gradient(double r2, double *gij) const {
-      if (r2 >= _rcut2) {
-        *gij = 0.;
-        return 0.;
-      }
-			double ir2 = 1.0/r2;
-			double ir6 = ir2*ir2*ir2;
-			double ir12 = ir6*ir6;
+        /* calculate energy and gradient from distance squared, gradient is in g/|rij| */
+        double energy_gradient(double r2, double *gij) const {
+            if (r2 >= _rcut2) {
+                *gij = 0.;
+                return 0.;
+            }
+            double ir2 = 1.0/r2;
+            double ir6 = ir2*ir2*ir2;
+            double ir12 = ir6*ir6;
 
-			*gij = (_12C12 * ir12 - _6C6 * ir6) * ir2 - _2A2;
-			return -_C6*ir6 + _C12*ir12 + _A0 + _A2*r2;
-		}
+            *gij = (_12C12 * ir12 - _6C6 * ir6) * ir2 - _2A2;
+            return -_C6*ir6 + _C12*ir12 + _A0 + _A2*r2;
+        }
   };
 
   //
@@ -105,56 +105,58 @@ namespace pele {
   // defined classes
   //
 
-	/**
+    /**
    * Pairwise Lennard-Jones potential
    */
-	class LJ : public SimplePairwisePotential< lj_interaction > {
-	public:
-		LJ(double C6, double C12)
-			: SimplePairwisePotential< lj_interaction > ( new lj_interaction(C6, C12) ) {}
-	};
+    class LJ : public SimplePairwisePotential< lj_interaction > {
+        public:
+            LJ(double C6, double C12)
+                : SimplePairwisePotential< lj_interaction > ( new lj_interaction(C6, C12) ) {}
+    };
 
-	/**
+    /**
    * Pairwise Lennard-Jones potential in a rectangular box
    */
-	class LJPeriodic : public SimplePairwisePotential< lj_interaction, periodic_distance > {
-	public:
-		LJPeriodic(double C6, double C12, double const *boxvec)
-			: SimplePairwisePotential< lj_interaction, periodic_distance> ( 
-          new lj_interaction(C6, C12), 
-          new periodic_distance(boxvec[0], boxvec[1], boxvec[2])
-          ) {}
-	};
+    class LJPeriodic : public SimplePairwisePotential< lj_interaction, periodic_distance > {
+        public:
+            LJPeriodic(double C6, double C12, double const *boxvec)
+                : SimplePairwisePotential< lj_interaction, periodic_distance> ( 
+                        new lj_interaction(C6, C12), 
+                        new periodic_distance(boxvec[0], boxvec[1], boxvec[2])
+                        ) 
+            {}
+    };
 
-	/**
+    /**
    * Pairwise Lennard-Jones potential with smooth cutoff
    */
-	class LJCut : public SimplePairwisePotential< lj_interaction_cut_smooth > {
-	public:
-		LJCut(double C6, double C12, double rcut)
-			: SimplePairwisePotential< lj_interaction_cut_smooth > ( new  lj_interaction_cut_smooth(C6, C12, rcut) ) {}
-	};
+    class LJCut : public SimplePairwisePotential< lj_interaction_cut_smooth > {
+        public:
+            LJCut(double C6, double C12, double rcut)
+                : SimplePairwisePotential< lj_interaction_cut_smooth > ( new  lj_interaction_cut_smooth(C6, C12, rcut) ) {}
+    };
 
-	/**
+    /**
    * Pairwise Lennard-Jones potential with smooth cutoff in a rectangular box
    */
-	class LJCutPeriodic : public SimplePairwisePotential< lj_interaction_cut_smooth, periodic_distance > {
-	public:
-		LJCutPeriodic(double C6, double C12, double rcut, double const *boxvec)
-			: SimplePairwisePotential< lj_interaction_cut_smooth, periodic_distance> ( 
-          new lj_interaction_cut_smooth(C6, C12, rcut), 
-          new periodic_distance(boxvec[0], boxvec[1], boxvec[2])
-          ) {}
-	};
+    class LJCutPeriodic : public SimplePairwisePotential< lj_interaction_cut_smooth, periodic_distance > {
+        public:
+            LJCutPeriodic(double C6, double C12, double rcut, double const *boxvec)
+                : SimplePairwisePotential< lj_interaction_cut_smooth, periodic_distance> ( 
+                        new lj_interaction_cut_smooth(C6, C12, rcut), 
+                        new periodic_distance(boxvec[0], boxvec[1], boxvec[2])
+                        ) 
+            {}
+    };
 
 
-	/**
+    /**
    * Pairwise Lennard-Jones potential with interaction lists
    */
-	class LJ_interaction_list : public SimplePairwiseInteractionList< lj_interaction > {
-	public:
-		LJ_interaction_list(Array<long int> & ilist, double C6, double C12)
-			:  SimplePairwiseInteractionList< lj_interaction > ( new lj_interaction(C6, C12), ilist) {}
-	};
+    class LJ_interaction_list : public SimplePairwiseInteractionList< lj_interaction > {
+        public:
+            LJ_interaction_list(Array<long int> & ilist, double C6, double C12)
+                :  SimplePairwiseInteractionList< lj_interaction > ( new lj_interaction(C6, C12), ilist) {}
+    };
 }
 #endif
--- playground/native_code/include/potentialfunction.h
+++ playground/native_code/include/potentialfunction.h
@@ -1,32 +1,35 @@
+#ifndef _PELE_POTENTIAL_FUNCTION_H
+#define _PELE_POTENTIAL_FUNCTION_H
 #include "array.h"
 #include "base_potential.h"
 
 namespace pele {
 
-/**
- * This class wraps a get_energy function and a get_energy_gradient function in
- * a class that derives from BasePotential.  This is necessary to be able to use
- * the functions in the pele c++ interface.  This is a backup method, the
- * preferred method is to define a class separately for each potential, which
- * eliminates the need to pass around the void * userdata parameter.
- */
-class PotentialFunction : public BasePotential
-{
-public:
-	typedef double EnergyCallback(Array<double>, void *);
-	typedef double EnergyGradientCallback(Array<double>, Array<double>, void *);
+    /**
+     * This class wraps a get_energy function and a get_energy_gradient function in
+     * a class that derives from BasePotential.  This is necessary to be able to use
+     * the functions in the pele c++ interface.  This is a backup method, the
+     * preferred method is to define a class separately for each potential, which
+     * eliminates the need to pass around the void * userdata parameter.
+     */
+    class PotentialFunction : public BasePotential
+    {
+        public:
+            typedef double EnergyCallback(Array<double>, void *);
+            typedef double EnergyGradientCallback(Array<double>, Array<double>, void *);
 
-	PotentialFunction(EnergyCallback *get_energy, EnergyGradientCallback *get_energy_gradient, void *userdata)
-		:	_get_energy(get_energy), _get_energy_gradient(get_energy_gradient), _userdata(userdata) {}
+            PotentialFunction(EnergyCallback *get_energy, EnergyGradientCallback *get_energy_gradient, void *userdata)
+                :    _get_energy(get_energy), _get_energy_gradient(get_energy_gradient), _userdata(userdata) {}
 
-	virtual double get_energy(Array<double> x) { return (*_get_energy)(x, _userdata); } ;
-	virtual double get_energy_gradient(Array<double> x, Array<double> grad) {  return (*_get_energy_gradient)(x, grad, _userdata); }
+            virtual double get_energy(Array<double> x) { return (*_get_energy)(x, _userdata); } ;
+            virtual double get_energy_gradient(Array<double> x, Array<double> grad) {  return (*_get_energy_gradient)(x, grad, _userdata); }
 
-private:
-	EnergyCallback *_get_energy;
-	EnergyGradientCallback *_get_energy_gradient;
-	void *_userdata;
-};
+        private:
+            EnergyCallback *_get_energy;
+            EnergyGradientCallback *_get_energy_gradient;
+            void *_userdata;
+    };
 
 
 }
+#endif
