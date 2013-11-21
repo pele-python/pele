be42d28d475b5fa67b1d7ce46bd96f041ab87826
Stefano Martiniani <stefano.martiniani@gmail.com>
2013-11-21 14:17:20 +0000

    bug fix in gradient of hs_wca potential

--- source/hs_wca.h
+++ source/hs_wca.h
@@ -1,7 +1,7 @@
 #ifndef _PELE_HS_WCA_H
 #define _PELE_HS_WCA_H
 
-#include "pairwise_potential.h"
+#include "simple_pairwise_potential.h"
 #include "atomlist_potential.h"
 #include "distance.h"
 
@@ -28,10 +28,10 @@ namespace pele {
     	{}
 
         /* calculate energy from distance squared, r0 is the hard core distance, r is the distance between the centres */
-        double energy(double r2, size_t i, size_t j) const {
+        double energy(double r2, size_t atomi, size_t atomj) const {
             double E;
         	double r = sqrt(r2);
-        	double r0 = _radii[i] + _radii[j]; //sum of the hard core radii
+        	double r0 = _radii[atomi] + _radii[atomj]; //sum of the hard core radii
         	double dr = r - r0;
             double ir2 = 1.0/(dr*dr);
             double ir6 = ir2*ir2*ir2;
@@ -43,7 +43,7 @@ namespace pele {
             if (r <= r0)
             {
             	E = _infty;
-            	std::cout<<"WARNING: distance between particles "<<i<<" and "<<j<<" is less than their hard core radii"<<std::endl;
+            	std::cout<<"WARNING: distance between atoms "<<atomi<<" and "<<atomj<<" is less than their hard core radii"<<std::endl;
             }
             else if(r < coff )
             	E = _eps*(-C6*ir6 + C12*ir12 + 1.0/4);
@@ -54,10 +54,10 @@ namespace pele {
         }
 
         /* calculate energy and gradient from distance squared, gradient is in g/|rij|, r0 is the hard core distance, r is the distance between the centres */
-        double energy_gradient(double r2, double *gij, size_t i, size_t j) const {
+        double energy_gradient(double r2, double *gij, size_t atomi, size_t atomj) const {
         	double E;
 			double r = sqrt(r2);
-			double r0 = _radii[i] + _radii[j]; //sum of the hard core radii
+			double r0 = _radii[atomi] + _radii[atomj]; //sum of the hard core radii
 			double dr = r - r0;
 			double ir2 = 1.0/(dr*dr);
 			double ir6 = ir2*ir2*ir2;
@@ -71,12 +71,12 @@ namespace pele {
 			{
 				E = _infty;
 				*gij = _infty;
-				std::cout<<"WARNING: distance between particles "<<i<<" and "<<j<<" is less than their hard core radii"<<std::endl;
+				std::cout<<"WARNING: distance between atoms "<<atomi<<" and "<<atomj<<" is less than their hard core radii"<<std::endl;
 			}
 			else if(r < coff)
 			{
 				E = _eps*(- C6 * ir6 + C12 * ir12 + 1.0/4);
-				*gij = _eps*(12 * C12 * ir12 - 6 * C6 * ir6) * ir2;
+				*gij = _eps*(- 6 * C6 * ir6 + 12 * C12 * ir12) / (dr*r); // this is -g|gij| (for consistency with the loop in pairwise potential)
 			}
 			else
 			{
@@ -97,20 +97,20 @@ namespace pele {
     /**
      * Pairwise HS_WCA potential
      */
-    class HS_WCA : public PairwisePotential< HS_WCA_interaction >
+    class HS_WCA : public SimplePairwisePotential< HS_WCA_interaction >
     {
         public:
             HS_WCA(double eps, double sca, Array<double> radii)
-                : PairwisePotential< HS_WCA_interaction > ( new HS_WCA_interaction(eps, sca, radii) ) {}
+                : SimplePairwisePotential< HS_WCA_interaction > ( new HS_WCA_interaction(eps, sca, radii) ) {}
     };
 
     /**
      * Pairwise HS_WCA potential in a rectangular box
      */
-    class HS_WCAPeriodic : public PairwisePotential< HS_WCA_interaction, periodic_distance > {
+    class HS_WCAPeriodic : public SimplePairwisePotential< HS_WCA_interaction, periodic_distance > {
         public:
             HS_WCAPeriodic(double eps, double sca, Array<double> radii, double const *boxvec)
-                : PairwisePotential< HS_WCA_interaction, periodic_distance> (
+                : SimplePairwisePotential< HS_WCA_interaction, periodic_distance> (
                         new HS_WCA_interaction(eps, sca, radii),
                         new periodic_distance(boxvec[0], boxvec[1], boxvec[2])
                         )
