9cd4bc2c460d35e47cc9fde63781525ca22d14c2
Stefano Martiniani <stefano.martiniani@gmail.com>
2014-03-18 09:25:04 +0000

    added unit test for metropolis monte carlo

---
+++ playground/monte_carlo/test/__init__.py
@@ -0,0 +1,4 @@
+from test_metropolis_mcrunner import *
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ playground/monte_carlo/test/test_metropolis_mcrunner.py
@@ -0,0 +1,42 @@
+from __future__ import division
+import numpy as np
+from pele.utils.rotations import vector_random_uniform_hypersphere
+from pele.potentials import Harmonic
+from playground.monte_carlo import Metropolis_MCrunner
+import unittest
+import logging
+
+
+class MetropolisTest(unittest.TestCase):
+    
+    def test_heat_capacity(self):
+        self.ndim = 3
+        self.k=1
+        self.origin = np.zeros(self.ndim)
+        self.potential = Harmonic(self.origin,self.k)
+        self.Emax = 5 #this choice is fundamentally arbitrary, it's only used to generate the initial configuration
+        self.start_coords = vector_random_uniform_hypersphere(self.ndim) * np.sqrt(2*self.Emax) #coordinates sampled from Pow(ndim)
+               
+        temperatures = [0.2,0.27,0.362,0.487,0.65,0.88,1.18,1.6]
+                
+        for T in temperatures:
+            print T
+            mcrunner = Metropolis_MCrunner(self.potential, self.start_coords,  temperature=T, niter=1e6, stepsize=1, 
+                                       adjustf = 0.9, adjustf_niter = 10000, radius=1000000)
+            #MCMC 
+            mcrunner.run()
+            #collect the results
+            
+            binenergy, hist = mcrunner.get_histogram()
+            
+            average = np.average(binenergy,weights=hist)
+                
+            average2 = np.average(np.square(binenergy),weights=hist)
+                
+            cv =  (average2 - average**2)/(T**2) + self.ndim/2
+            
+            self.assertLess(cv-self.ndim,0.1)
+
+if __name__ == "__main__":
+    logging.basicConfig(filename='Metropolis_mcrunner.log',level=logging.DEBUG)
+    unittest.main()
