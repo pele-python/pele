d8404aa2021a8b2d0f26ae6a985e3fb673dd508f
ssomani <ssomani@gmail.com>
2014-03-24 14:32:45 -0400

    after fixing minimization error with Cpp lbfgs

--- examples/amber/testOpenMM/testOpenMM_in_PELE.py
+++ examples/amber/testOpenMM/testOpenMM_in_PELE.py
@@ -1,6 +1,7 @@
-from pele.amber.openmm_potential import OpenMMAmberPotential 
 from simtk.unit import   kilocalories_per_mole, kilojoules_per_mole, nanometer, angstrom, picosecond 
 import numpy 
+# --- Test  OpenMMAmberPotential 
+from pele.amber.openmm_potential import OpenMMAmberPotential 
 
 # create potential for the molecule in coords.pdb
 prmtopFname = '../aladipep/coords.prmtop' 
@@ -33,4 +34,42 @@ import numpy as np
 print 'Num vs Analytic Gradient =' 
 print np.max(np.abs(gnum-g)), np.max(np.abs(gnum))
 print np.max(np.abs(gnum-g)) / np.max(np.abs(gnum))
+
+
+# --- Test  AMBERSystem class 
+from pele.amber.amberSystem import AMBERSystem 
+
+# create new amber system    
+sysAmb  = AMBERSystem('../aladipep/coords.prmtop', '../aladipep/coords.inpcrd')
+    
+# load existing database 
+from pele.storage import Database
+dbcurr = Database(db="../aladipep/aladipep.db")
+                            
+# ------ Test potential 
+print 'testing potential in ambSystem' 
+sysAmb.test_potential("../aladipep/coords.pdb")
+    
+# ------ BH 
+print 'testing BH' 
+nsteps = 10
+sysAmb.test_BH(dbcurr, nsteps)
+
+# ------- TEST gui 
+#from pele.gui import run as gr    
+#gr.run_gui(sysAmb)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
