056dd15c225848f2337eb268645a7d3139714fe6
Stefano Martiniani <stefano.martiniani@gmail.com>
2014-03-28 22:27:12 +0000

    added __init__.py for monte_carlo

---
+++ playground/monte_carlo/__init__.py
@@ -0,0 +1,7 @@
+from _accept_test_cpp import *
+from _conf_test_cpp import *
+from _takestep_cpp import *
+from _monte_carlo_cpp import *
+from _action_cpp import *
+from _base_mcrunner import _base_MCrunner
+from mcrunner import Metropolis_MCrunner
