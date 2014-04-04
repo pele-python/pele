f1940783933242b0dede09ff04b8898040d066ff
Jacob Stevenson <js850@cam.ac.uk>
2014-04-04 13:59:01 +0100

    use gtest test Fixtures

--- cpp_tests/source/test_lj.hpp
+++ cpp_tests/source/test_lj.hpp
@@ -28,35 +28,36 @@ TEST(LJInteractionTest, EnergyGradient_Works){
     ASSERT_NEAR(g, 9.2454671845389917, 1e-10);
 }
 
-TEST(LJTest, Energy_Works){
-    double c6 = 1.2;
-    double c12 = 2.3;
+class LJTest :  public ::testing::Test
+{
+public:
+    double c6, c12, etrue;
+    Array<double> x;
+    void SetUp(){
+        c6 = 1.2;
+        c12 = 2.3;
+        x = Array<double>(6);
+        x[0]  = 0.1;
+        x[1]  = 0.2;
+        x[2]  = 0.3;
+        x[3]  = 0.44;
+        x[4]  = 0.55;
+        x[5]  = 1.66;
+        etrue = -0.10410023149146598;
+    }
+};
+
+TEST_F(LJTest, Energy_Works){
     pele::LJ lj(c6, c12);
-    Array<double> x(6);
-    x[0]  = 0.1;
-    x[1]  = 0.2;
-    x[2]  = 0.3;
-    x[3]  = 0.44;
-    x[4]  = 0.55;
-    x[5]  = 1.66;
     double e = lj.get_energy(x);
-    ASSERT_NEAR(e, -0.10410023149146598, 1e-10);
+    ASSERT_NEAR(e, etrue, 1e-10);
 }
 
-TEST(LJTest, EnergyGradient_Works){
-    double c6 = 1.2;
-    double c12 = 2.3;
+TEST_F(LJTest, EnergyGradient_Works){
     pele::LJ lj(c6, c12);
-    Array<double> x(6);
-    x[0]  = 0.1;
-    x[1]  = 0.2;
-    x[2]  = 0.3;
-    x[3]  = 0.44;
-    x[4]  = 0.55;
-    x[5]  = 1.66;
     Array<double> g(6);
     double e = lj.get_energy_gradient(x, g);
-    ASSERT_NEAR(e, -0.10410023149146598, 1e-10);
+    ASSERT_NEAR(e, etrue, 1e-10);
     ASSERT_NEAR(g[0], -0.07457726800542995, 1e-10);
     ASSERT_NEAR(g[1], -0.076770717064413199, 1e-10);
     ASSERT_NEAR(g[2], -0.2983090720217198, 1e-10);
