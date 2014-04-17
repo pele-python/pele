8b1bf0c6c569e24f19dd684f4607a8f57b14a103
Julian Schrenk <kjs73@cam.ac.uk>
2014-04-17 15:34:19 +0100

    add test of frozen property

--- cpp_tests/source/test_frozen_hs_wca.hpp
+++ cpp_tests/source/test_frozen_hs_wca.hpp
@@ -1,6 +1,7 @@
 #include "pele/array.h"
 #include "pele/frozen_atoms.h"
 #include "pele/hs_wca.h"
+#include "pele/modified_fire.h"
 
 #include <iostream>
 #include <stdexcept>
@@ -14,6 +15,7 @@ using pele::HS_WCAPeriodic;
 
 class FrozenHS_WCATest: public ::testing::Test{
 public:
+	typedef pele::MODIFIED_FIRE opt_t;
 	double eps, sca, etrue;
 	Array<double> radii, radii_small, radii_large;
 	Array<double> x, y;
@@ -156,5 +158,33 @@ TEST_F(FrozenHS_WCATest, TestNumericalGradient_Correct_Periodic){
 	auto gtrue_red = pot._coords_converter.get_reduced_coords(gtrue);
 	for (int i=0; i<gred.size(); ++i){
 		EXPECT_NEAR(gtrue_red[i], gred[i], 1e-10);
+	}
+}
+
+TEST_F(FrozenHS_WCATest, TestMinimizationFreezing_Correct){
+	// minimization for non-frozen HS_WCA
+	HS_WCA pot_nofreeze(eps,sca,radii);
+	const auto e_notfreeze_before = pot_nofreeze.get_energy(x);
+	opt_t minimizer(&pot_nofreeze, x, 1e-4, 1.0, 0.5);
+	minimizer.run();
+	const auto x_after = minimizer.get_x();
+	const auto e_notfreeze_after = pot_nofreeze.get_energy(x_after);
+	EXPECT_TRUE( e_notfreeze_after <= e_notfreeze_before );
+	EXPECT_NEAR( e_notfreeze_after, 0, 1e-10 );
+	// minimization for frozen HS_WCA
+	HS_WCAFrozen pot(eps, sca, radii, x, frozen_dof);
+	auto xred = pot._coords_converter.get_reduced_coords(x);
+	const auto e_before = pot.get_energy(xred);
+	opt_t minimizer_red(&pot, xred, 1e-4, 1.0, 0.5);
+	minimizer_red.run();
+	const auto xred_after = minimizer_red.get_x();
+	const auto e_after = pot.get_energy(xred_after);
+	EXPECT_TRUE( e_after <= e_before );
+	EXPECT_TRUE( e_notfreeze_after <= e_after );
+	// verify that frozen dof are in fact frozen
+	const auto xred_after_inflated = pot._coords_converter.get_full_coords(xred_after);
+	for (unsigned int i(0); i < frozen_dof.size(); ++i){
+		const auto idx = frozen_dof[i];
+		EXPECT_NEAR( x[idx], xred_after_inflated[idx], 1e-10 );
 	}
 }
