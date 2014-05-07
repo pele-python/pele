fix bug in atomic cluster pgorder calculation
During check_match() the rotational alignment should not be
re-optimized.  This was giving false matches, making the point group
order larger than it should be.

pgorder for the LJ6 and LJ75 global minimum are now computed correctly.

I've not yet fully checked that this does not cause any other problems

pele/mindist/_pointgrouporder.py
@@ -37,7 +37,38 @@ class PointGroupOrderCluster(object):
         pgorder = 0
         for rot, invert in self.exact_match.standard_alignments(x1, x1):
             if invert: continue
-            if self.exact_match.check_match(x1, x1, rot, invert):
+            if self.exact_match.check_match(x1, x1, rot, invert, reoptimize_rotation=False):
                 pgorder += 1
+#                print "pgorder", pgorder
         
-        return inversion_multiplier*pgorder
+        return inversion_multiplier * pgorder
+
+def testlj75():
+    import numpy as np
+    coords = np.genfromtxt("tests/coords.lj75.gmin")
+    from pele.mindist import ExactMatchAtomicCluster
+    
+    permlist = [range(75)]
+    match = ExactMatchAtomicCluster(permlist=permlist, can_invert=True)
+    calculator = PointGroupOrderCluster(match)
+    pgorder = calculator(coords)
+    print pgorder
+
+def testlj6():
+    from pele.systems import LJCluster
+    from pele.thermodynamics import get_thermodynamic_information
+    system = LJCluster(6)
+    db = system.create_database()
+    bh = system.get_basinhopping(db)
+    bh.setPrinting(ostream=None)
+    bh.run(10)
+    
+    get_thermodynamic_information(system, db)
+
+    for m in db.minima():
+        print m.energy, m.pgorder
+        
+    
+
+if __name__ == "__main__":
+    testlj6()

pele/mindist/exact_match.py
@@ -11,6 +11,8 @@ class StandardClusterAlignment(object):
     '''
     class to iterate over standard alignments for atomic clusters
 
+    Notes
+    -----
     Quickly determines alignments of clusters which are possible exact matches.
     It uses atoms which are far away from the center to determine possible
     rotations. The algorithm does the following:
@@ -255,7 +257,7 @@ class ExactMatchCluster(object):
                 return self.exact_transformation
         return None
 
-    def check_match(self, x1, x2, rot, invert):
+    def check_match(self, x1, x2, rot, invert, reoptimize_rotation=True):
         '''Give a rotation and inversion, make a more detailed comparison if the 2 structures match
 
         Parameters
@@ -279,17 +281,25 @@ class ExactMatchCluster(object):
 
         # apply the rotation to x2_trial
         self.transform.rotate(x2_trial, rot)
+        
 
         # get the best permutation
         dist, perm = self.measure.find_permutation(x1, x2_trial)
         x2_trial = self.transform.permute(x2_trial, perm)
 
-        # now find best rotational alignment, this is more reliable than just
-        # aligning the 2 reference atoms
-        dist, rot2 = self.measure.find_rotation(x1, x2_trial)
-        # further rotate x2_trial by rot2
-        self.transform.rotate(x2_trial, rot2)
-        # use the maximum distance, not rms as cutoff criterion
+#        print "after rotation and permutation: dist = ",  self.measure.get_dist(x1, x2_trial)
+
+        if reoptimize_rotation:
+            # now find best rotational alignment, this is more reliable than just
+            # aligning the 2 reference atoms
+            dist, rot2 = self.measure.find_rotation(x1, x2_trial)
+            # further rotate x2_trial by rot2
+            self.transform.rotate(x2_trial, rot2)
+            # use the maximum distance, not rms as cutoff criterion
+        else:
+            rot2 = np.eye(3)
+
+#        print "    after further improving the rotation: dist = ",  self.measure.get_dist(x1, x2_trial)
 
         self._last_checked_rotation = np.dot(rot2, rot)
         if  self.measure.get_dist(x1, x2_trial) < self.tol:
@@ -345,6 +355,8 @@ def test():
         #dist, x1n, x2n = findBestPermutation(xx1.flatten(), xx2.flatten())
         #print dist
         print i,ExactMatchCluster()(xx1.flatten(), xx2.flatten())
+
+
 
 
 if __name__ == '__main__':