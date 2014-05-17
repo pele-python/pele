c7ca398ac6f108f88e5991ace11984f60e39a6c8
Jacob Stevenson <js850@cam.ac.uk>
2014-05-17 18:08:04 +0100

    lbfgs now derives from the cdef gradient optimizer

--- pele/optimize/_lbfgs_cpp.pyx
+++ pele/optimize/_lbfgs_cpp.pyx
@@ -4,6 +4,7 @@
 import numpy as np
 cimport numpy as np
 from pele.potentials import _pele, _pythonpotential
+cimport pele.optimize._pele_opt as _pele_opt
 from pele.potentials cimport _pele
 from pele.optimize import Result
 cimport cython
@@ -42,12 +43,13 @@ cdef extern from "pele/lbfgs.h" namespace "pele":
         int success() except +
 
 
-cdef class _Cdef_LBFGS_CPP(object):
+cdef class _Cdef_LBFGS_CPP(_pele_opt.GradientOptimizer):
     """This class is the python interface for the c++ LBFGS implementation
     """
-    cdef cppLBFGS *thisptr
+#     cdef cppLBFGS *thisptr
     cdef _pele.BasePotential pot # this is stored so that the memory is not freed
-    cpdef events
+#     cpdef events
+    cdef cppLBFGS *lbfgs_ptr
     
     def __cinit__(self, x0, potential, double tol=1e-5, int M=4, double maxstep=0.1, 
                   double maxErise=1e-4, double H0=0.1, int iprint=-1,
@@ -58,19 +60,18 @@ cdef class _Cdef_LBFGS_CPP(object):
                 print "LBFGS_CPP: potential is not subclass of BasePotential; wrapping it.", potential
 #                print "           Wrapping the potential like this is dangerous.  All python exceptions will be ignored"
             potential = _pythonpotential.CppPotentialWrapper(potential)
-        cdef _pele.BasePotential pot = potential
+        self.pot = potential # so that the memory is not freed
         cdef np.ndarray[double, ndim=1] x0c = np.array(x0, dtype=float)
-        self.thisptr = <cppLBFGS*>new cppLBFGS(pot.thisptr, 
+        self.thisptr = <_pele_opt.cGradientOptimizer*>new cppLBFGS(self.pot.thisptr, 
                _pele.Array[double](<double*> x0c.data, x0c.size),
                tol, M)
-        opt = self.thisptr
-        opt.set_H0(H0)
-        opt.set_maxstep(maxstep)
-        opt.set_max_f_rise(maxErise)
-        opt.set_max_iter(nsteps)
-        opt.set_verbosity(verbosity)
-        opt.set_iprint(iprint)
-        self.pot = pot # so that the memory is not freed
+        self.lbfgs_ptr = <cppLBFGS*> self.thisptr
+        self.lbfgs_ptr.set_H0(H0)
+        self.lbfgs_ptr.set_maxstep(maxstep)
+        self.lbfgs_ptr.set_max_f_rise(maxErise)
+        self.lbfgs_ptr.set_max_iter(nsteps)
+        self.lbfgs_ptr.set_verbosity(verbosity)
+        self.lbfgs_ptr.set_iprint(iprint)
         
         cdef np.ndarray[double, ndim=1] g_  
         if energy is not None and gradient is not None:
@@ -81,87 +82,7 @@ cdef class _Cdef_LBFGS_CPP(object):
         if self.events is None: 
             self.events = []
         
-    def __dealloc__(self):
-        if self.thisptr != NULL:
-            del self.thisptr
-            self.thisptr = NULL
-        
-    def run(self, niter=None):
-        if not self.events:
-            # if we don't need to call python events then we can
-            # go just let the c++ optimizer do it's thing
-            if niter is None:
-                self.thisptr.run()
-            else:
-                self.thisptr.run(niter)
-        else:
-            # we need to call python events after each iteration.
-            if niter is None:
-                niter = self.get_maxiter() - self.get_niter()
-    
-            for i in xrange(niter):
-                if self.stop_criterion_satisfied():
-                    break
-                self.one_iteration()
-            
-        return self.get_result()
-            
-
-    @cython.boundscheck(False)
-    def get_result(self):
-        """return a results object"""
-        res = Result()
-        
-        cdef _pele.Array[double] xi = self.thisptr.get_x()
-        cdef _pele.Array[double] gi = self.thisptr.get_g()
-        cdef double *xdata = xi.data()
-        cdef double *gdata = gi.data()
-        cdef np.ndarray[double, ndim=1, mode="c"] x = np.zeros(xi.size())
-        cdef np.ndarray[double, ndim=1, mode="c"] g = np.zeros(xi.size())
-        cdef size_t i
-        for i in xrange(xi.size()):
-            x[i] = xdata[i]
-            g[i] = gdata[i]
-
-        #jake> it's anoying having to copy the data manually like this.
-        # We can possibly use np.frombuffer(), thought I haven't gotten it to work.
-        # We can also maybe use the c function PyArray_SimpleNewFromData.
-        # In the meantime the loop won't be too slow if we use cython properly to speed it up.
-        #cdef np.ndarray[double, ndim=1, mode="c"] g2
-        #g2 = np.frombuffer(xi.data(), dtype=np.float64, count=xi.size())
-        
-        res.energy = self.thisptr.get_f()
-        res.coords = x
-        res.grad = g
-        
-        res.rms = self.thisptr.get_rms()        
-        res.nsteps = self.thisptr.get_niter()
-        res.nfev = self.thisptr.get_nfev()
-        res.H0 = self.thisptr.get_H0()
-        res.success = bool(self.thisptr.success())
-        return res
-    
-    def one_iteration(self):
-        self.thisptr.one_iteration()
-        res = self.get_result()
-        for event in self.events:
-            event(coords=res.coords, energy=res.energy, rms=res.rms)
-    
-    def stop_criterion_satisfied(self):
-        return bool(self.thisptr.stop_criterion_satisfied())
-
-    def get_maxiter(self):
-        return self.thisptr.get_maxiter()
-
-    def get_niter(self):
-        return self.thisptr.get_niter()
 
 class LBFGS_CPP(_Cdef_LBFGS_CPP):
     """This class is the python interface for the c++ LBFGS implementation
     """
-
-    
-#     def one_iteration(self):
-#         """do one iteration"""
-#         _Cdef_LBFGS_CPP.one_iteration(self)
-    
