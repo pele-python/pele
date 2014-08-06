547a81f8ad06dda2983ed1834595297b22093c80
Stefano Martiniani <stefano.martiniani@gmail.com>
2014-08-06 12:17:14 +0100

    cell list reset function added

--- source/pele/neighbor_iterator.h
+++ source/pele/neighbor_iterator.h
@@ -8,6 +8,7 @@
 #include "distance.h"
 #include <iostream>
 #include <memory>
+#include <exception>
 
 using std::cout;
 
@@ -47,7 +48,7 @@ public:
 
 /*cell list currently only work with box of equal side lengths*/
 
-template<typename distance_policy=cartesian_distance<3> >
+template<typename distance_policy=periodic_distance<3> >
 class CellIter : public NeighborIter<distance_policy>
 {
 protected:
@@ -70,7 +71,7 @@ protected:
     //this function assumes that particles have been already put in box
     size_t _atom2icell(size_t i)
     {
-        size_t index = 0;
+        size_t icell = 0;
 
         for(size_t j =0;j<_ndim;++j)
         {
@@ -78,25 +79,42 @@ protected:
             if (_coords[j1] < 0){
                 _coords[j1] += _boxl;
             }
-            index += floor(x / _rcell) * std::pow(_ncellx,j);
+            icell += floor(x / _rcell) * std::pow(_ncellx,j);
         }
-        return index;
+        return icell;
     }
 
 public:
     virtual ~NeighborIter() {}
 
-    /*build linked lists*/
+    /*re-build linked lists
+     * Algorithm 37 page 552 Understanding Molecular Simulation 2nd ed.
+     * start by setting head of chain (hoc of size ncells) to -1 (meaning end of chain)
+     * then update linked list so that atom i points to the next atom in the chain,
+     * obviously this starts from -1 if it is the only element in the chain. If the next
+     * atom i is the same cell, then the hoc for that cell is set to be the i
+     * and the linked list at position i will point to the index of the previous atom.
+     * This is done iteratively for all atoms.
+     */
     virtual void reset(pele::Array<double> coords)
     {
         _coords.assign(coords);
-        _dist.put_in_box(_coords);
-        _hoc.assign(-1); //set head of chains to -1 (empty state)
-        for(size_t i=0;i<_natoms;++i)
-        {
 
+        try{
+            _dist.put_in_box(_coords);
+        }
+        catch (exception& e){
+            cout << "put in box not implemented for this type of distance. Exception: " << e.what() << '\n';
         }
 
+        _hoc.assign(-1); //set head of chains to -1 (empty state)
+
+        for(size_t i=0;i<_natoms;++i)
+        {
+            size_t icell = this->_atom2icell(i);
+            _ll[i] = _hoc[icell];
+            _hoc[icell] = i;
+        }
     }
 
 
