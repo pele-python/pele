89560729cac5cae03f6c71805dd2283219722dc5
Stefano Martiniani <stefano.martiniani@gmail.com>
2014-08-08 10:18:54 +0100

    fixed python tests for inverse potential

--- pele/potentials/tests/test_inversepower_cpp.py
+++ pele/potentials/tests/test_inversepower_cpp.py
@@ -5,7 +5,7 @@ import os
 import logging
 
 from pele.potentials import _inversepower_cpp
-from pele.optimize._quench import lbfgs_cpp
+from pele.optimize._quench import lbfgs_cpp, modifiedfire_cpp
 import _base_test
 
 def read_xyzdr(fname, bdim=3):
@@ -33,15 +33,16 @@ class TestInversePower_CPP(_base_test._BaseTest):
         current_dir = os.path.dirname(__file__)
         xyz, hs_radii, rattlers = read_xyzdr(current_dir + "/_hswca20_min2.xyzdr")
         sca = 0.205071132088
-        radii = hs_radii * (1+sca)
-        boxv = [6.26533756282, 6.26533756282, 6.26533756282]
-        pow = 5/2
+        radii = hs_radii * (1.0+sca)
+        boxv = np.array([6.26533756282, 6.26533756282, 6.26533756282])
+        pow = 4
         eps = 1
         self.pot = _inversepower_cpp.InversePower(pow, eps, radii, boxvec = boxv)
         self.natoms = 20
         result = minimize(xyz, self.pot)
         self.xmin = result[0] #xyz
         self.Emin = result[1] #self.pot.getEnergy(self.xmin)
+        print self.Emin
         self.xrandom = np.random.uniform(-1,1,len(xyz))*1e-2
 
 if __name__ == "__main__":
