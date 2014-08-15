3278cc96e562f3a0f3e64248b0b28a9b5d68c333
Julian Schrenk <kjs73@cam.ac.uk>
2014-08-15 18:39:09 +0100

    add meta pow function for integer and half integer exponents

---
+++ source/pele/meta_pow.h
@@ -0,0 +1,66 @@
+#ifndef _PELE_META_POW_H
+#define _PELE_META_POW_H
+
+namespace pele{
+
+#include <cmath> //for sqrt in half powers
+
+template<class T, int N>
+struct meta_pow{
+    static T f(const T x)
+    {
+        return meta_pow<T, N - 1>::f(x) * x;
+    }
+};
+
+template<class T>
+struct meta_pow<T, 0>{
+    static T f(const T x)
+    {
+        return T(1);
+    }
+};
+
+/**
+ * pow(x, N) where N >= 0, integer
+ * usage: pos_int_pow<N>(x)
+ */
+template<int N, class T>
+inline T pos_int_pow(const T x)
+{
+    return meta_pow<T, N>::f(x);
+}
+
+/**
+ * pow(x, - N) where N >= 0, integer
+ * usage: neg_int_pow<-N>(x)
+ */
+template<int N, class T>
+inline T neg_int_pow(const T x)
+{
+    return T(1) / meta_pow<T, -N>::f(x);
+}
+
+/**
+ * pow(x, N / 2) where N >= 0, integer
+ * usage: pos_half_int_pow<N>(x)
+ */
+template<int N, class T>
+inline T pos_half_int_pow(const T x)
+{
+    return meta_pow<T, N>::f(std::sqrt(x));
+}
+
+/**
+ * pow(x, - N / 2) where N >= 0, integer
+ * usage: neg_half_int_pow<-N>(x)
+ */
+template<int N, class T>
+inline T neg_half_int_pow(const T x)
+{
+    return T(1) / meta_pow<T, -N>::f(std::sqrt(x));
+}
+
+}//namespace pele
+
+#endif //#ifndef _PELE_META_POW_H
