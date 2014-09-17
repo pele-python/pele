7ec315535f55a1c84f829af77839f83a2c7d4bcc
Jacob Stevenson <js850@cam.ac.uk>
2014-09-17 12:15:02 +0100

    move colfusion.py to playground
    It wasn't documented or tested and it didn't seem like it was used.

--- playground/possibly_obsolete/coldfusioncheck.py
+++ playground/possibly_obsolete/coldfusioncheck.py
@@ -3,6 +3,11 @@ from potential import potential
 __all__ = ["ColdFusionCheck", "addColdFusionCheck"]
 
 class ColdFusionCheck(potential):
+    """fixes for potentials that have cold fusion
+    
+    some potentials experience cold fusion, is our term for when the energy
+    becomes un-physically small.  This is usually when two atoms sit on top of each other. 
+    """
     def __init__(self, potential, coldfusionlimit=-1000, coldfusionenergy=1000000):
         self.potential = potential
         self.coldfusionlimit = coldfusionlimit
@@ -10,17 +15,19 @@ class ColdFusionCheck(potential):
         
     def getEnergy(self, coords):
         E = self.potential.getEnergy(coords)
-        if(E<self.coldfusionlimit):
+        if E < self.coldfusionlimit:
             return self.coldfusionenergy
         return E
     
     def getEnergyGradient(self, coords):
         E, grad = self.potential.getEnergyGradient(coords)
-        if(E<self.coldfusionlimit):
-            grad[:]=0.
+        if E < self.coldfusionlimit:
+            E = self.coldfusionenergy
+            grad[:] = 0.
         return E, grad
     
     def __call__(self, E, conf, **kwargs):
+        """test whether this energy satisfies the cold fusion criterion"""
         if E < self.coldfusionlimit:
             print "Cold fusion detected, energy was " + str(E)
             #kwargs['driver'].trial_energy = self.coldfusionenergy
