ef63f14412490db3100c82ac77f97ff9bbb19d9f
Julian Schrenk <kjs73@cam.ac.uk>
2014-09-17 12:18:34 +0100

    add log prob function

--- pele/potentials/ml.py
+++ pele/potentials/ml.py
@@ -8,24 +8,32 @@ class MLCost(BasePotential):
     
     Parameters
     ----------
+    data : array of floats
+        The observed data.
+    log_probf : callable `log_probf(data, parameters)`
+        Log of probability function which takes an array of data and an array
+        of parameters and returns a float value
     probf : callable `probf(data, parameters)`
         Probability function which takes an array of data and an array
         of parameters and returns a float value
-    data : array of floats
-        The observed data.
     
     Examples
     --------
     To get maximum likelihood estimates of the parameters, do e.g.:
     
-        pot = MLCost(gauss, observed)
+        pot = MLCost(log_gauss, observed)
         optimizer = LBFGS_CPP(parameters, pot)
         result = optimizer.run()
         opt_parameters = result.coords
     """
-    def __init__(self, probf, data):
+    def __init__(self, data, log_probf=None, probf=None):
+        self.log_probf = log_probf
         self.probf = probf
-        self.data = np.array(data)
+        if self.log_probf == None and self.probf == None:
+            raise Exception("provide either log_probf or probf")
+        self.data = np.asarray(data)
 
     def getEnergy(self, parameters):
-        return -np.sum(np.log(self.probf(self.data, np.asarray(parameters))))
+        if self.probf != None:
+            return -np.sum(np.log(self.probf(self.data, np.asarray(parameters))))
+        return -np.sum(self.log_probf(self.data, np.asarray(parameters)))
--- pele/potentials/tests/test_ml.py
+++ pele/potentials/tests/test_ml.py
@@ -10,6 +10,11 @@ def gauss(x, pars):
     ss = pars[1]
     return np.exp(-(x - mu)**2 / (2 * ss)) / np.sqrt(2 * np.pi * ss)
 
+def log_gauss(x, pars):
+    mu = pars[0]
+    ss = pars[1]
+    return (-(x - mu)**2 / (2 * ss)) - 0.5 * np.log(2 * np.pi * ss)
+
 class MLTest(unittest.TestCase):
     def setUp(self):
         self.nr_points = 1000
@@ -18,17 +23,23 @@ class MLTest(unittest.TestCase):
         self.ss = 12
     def test_normal_estimates_OK(self):
         observed = np.sqrt(self.ss) * randn(self.nr_points) + self.mu
-        pot = MLCost(gauss, observed)
+        pot = MLCost(observed, probf=gauss)
+        potl = MLCost(observed, log_probf=log_gauss)
         parameters = [self.mu + 1, self.ss - 2]
         optimizer = LBFGS_CPP(parameters, pot)
+        optimizerl = LBFGS_CPP(parameters, potl)
         result = optimizer.run()
+        resultl = optimizerl.run()
         opt_parameters = result.coords
+        opt_parametersl = resultl.coords
         opt_mu = opt_parameters[0]
-        print "opt_mu", opt_mu
+        opt_mul = opt_parametersl[0]
         opt_ss = opt_parameters[1]
-        print "opt_ss", opt_ss
+        opt_ssl = opt_parametersl[1]
         self.assertAlmostEqual(opt_mu, self.mu, delta=2*self.mu/np.sqrt(self.nr_points))
         self.assertAlmostEqual(opt_ss, self.ss, delta=2*self.ss/np.sqrt(self.nr_points))
+        self.assertAlmostEqual(opt_mu, opt_mul, delta = 1e-4)
+        self.assertAlmostEqual(opt_ss, opt_ssl, delta = 1e-4)
 
 if __name__ == "__main__":
     unittest.main()
