eec9517f8bdf413a4840c1ed975b41d856f9b8a5
Jacob Stevenson <jstevenson131@gmail.com>
2014-09-17 11:42:55 +0100

    update the documentation of MLCost

--- pele/potentials/ml.py
+++ pele/potentials/ml.py
@@ -4,19 +4,29 @@ from pele.potentials import BasePotential
 
 class MLCost(BasePotential):
     """
-    Cost function to be used for ML optimization.
-    Takes probability function,
-    its parameters,
-    and observed data.
+    Cost function to be used for maximum likelihood optimization.
+    
+    Parameters
+    ----------
+    probf : callable `probf(data, parameters)`
+        Probability function which takes an array of data and an array
+        of parameters and returns a float value
+    data : array of floats
+        The observed data.
+    
+    Examples
+    --------
     To get maximum likelihood estimates of the parameters, do e.g.:
-    pot = MLCost(gauss, observed)
-    optimizer = LBFGS_CPP(parameters, pot)
-    result = optimizer.run()
-    opt_parameters = result.coords
+    
+        pot = MLCost(gauss, observed)
+        optimizer = LBFGS_CPP(parameters, pot)
+        result = optimizer.run()
+        opt_parameters = result.coords
     """
     def __init__(self, probf, data):
         self.probf = probf
         self.data = np.array(data)
-    def getEnergy(self, coords):
-        coords = np.array(coords)
-        return -np.sum(np.log(self.probf(self.data, coords)))
+
+    def getEnergy(self, parameters):
+        params = np.array(parameters)
+        return -np.sum(np.log(self.probf(self.data, parameters)))
