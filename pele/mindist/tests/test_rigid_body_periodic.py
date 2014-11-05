ec1b2dced7a9cb501c5b84e3d52fca4ba486bb48
Samuel Niblett <sn402@cam.ac.uk>
2014-11-05 13:44:57 +0000

    More updated tests

--- pele/mindist/tests/test_rigid_body_periodic.py
+++ pele/mindist/tests/test_rigid_body_periodic.py
@@ -3,7 +3,7 @@ import copy
 import numpy as np
 #from math import pi, cos, sin
 
-from pele.mindist.periodic_exact_match import MeasurePeriodicRigid, ExactMatchPeriodic, TransformPeriodicRigid
+from pele.mindist.periodic_exact_match import MeasurePeriodicRigid, ExactMatchRigidPeriodic, TransformPeriodicRigid
 from pele.angleaxis.rigidbody import RBTopologyBulk, RigidFragmentBulk
 from pele.mindist.minpermdist_stochastic import MinPermDistBulk
 
@@ -24,17 +24,22 @@ class TestExactMatchPeriodicRigid(unittest.TestCase):
         #self.permlist = self.get_permlist()      
         self.measure = MeasurePeriodicRigid(boxl, self.topology)
         self.transform = TransformPeriodicRigid()
-        self.exact_match = ExactMatchPeriodic(self.measure, accuracy=1e-5)
+        self.exact_match = ExactMatchRigidPeriodic(self.measure, accuracy=1e-5)
         self.mindist = MinPermDistBulk(boxl, self.measure)
                 
         self.x1 = self.get_random_configuration(boxl)
         self.x2diff = self.get_random_configuration(boxl)
-        self.x2same = self.x1
+        self.x2same = self.x1.copy()
 #         self.x2same = self.randomly_permute(self.x1.copy())
-        self.x2trans = self.x1
-        self.transform.translate(self.x2trans, np.random.random(3)*boxl)
+        self.x2trans = self.x1.copy()
+        trans = np.random.random(3)*boxl
+#         print "trans", trans
+        self.transform.translate(self.x2trans, trans)
         dist = self.measure.get_dist(self.x1, self.x2diff)
-        print "dist", dist
+#         print "x1", self.x1
+#         print "x2diff", self.x2diff
+#         print "x2trans", self.x2trans                
+#         print "dist", dist
                  
 #     def get_permlist(self):
 #         return [range(self.natoms)]
@@ -70,7 +75,8 @@ class TestExactMatchPeriodicRigid(unittest.TestCase):
         return x.flatten()        
     
     def test_exact_match(self):
-        self.assertTrue(self.exact_match(self.x1, self.x2same))
+        print "test_exact_match", self.x1, self.x2trans
+        self.assertTrue(self.exact_match(self.x1, self.x2trans))
         
     def test_no_exact_match(self):
         self.assertFalse(self.exact_match(self.x1, self.x2diff))
@@ -79,10 +85,6 @@ class TestExactMatchPeriodicRigid(unittest.TestCase):
         self.x2same[:3] += self.measure.boxlengths  
         self.assertTrue(self.exact_match(self.x1, self.x2same))
         
-    def test_translation(self):
-        dist, x1, x2 = self.mindist(self.x1, self.x2trans)
-        self.assertTrue(self.exact_match(x1, x2), dist)
-
 
 #class TestExactMatchPeriodicBLJ(TestExactMatchPeriodicLJ):        
 #    def get_permlist(self):
