55b08ccbf710e1eeee0e18bde3d170a13c2ce67f
cjforman <cjf41@cam.ac.uk>
2014-11-05 13:05:25 +0000

    A Base class for a simple molecular system representing a molecule as a networkx graph.  Understands PDB and XYZ files using the pymol interface, but could easily be modified to add other files that pymol understands.  tested and passed its own test class also.

--- playground/molecule/molecule.py
+++ playground/molecule/molecule.py
@@ -1,190 +1,240 @@
-from utilities import read_text_file, write_text_file
+import pele.utils.elements as elem
 
 import networkx as nx
-import numpy as np
 import pymol_wrapper
-import sys as sys
 import os as os
 
-class Molecule():
+
+class Molecule(object):
     """
     A representation of a molecule consisting of the following items:
-    
-    1) A graph whose nodes are atom objects and whose edges have bonds attributes.
-        This defines the topology of the molecule.
-    2) A list of coords of the atomic positions defining the current state of the molecule.
-    3) A collection of functions for performing operations on and calculations about the molecule
-    
-    
-    self.topology              Network X Graph. The nodes are atoms and the edges are bonds.
-    self.coords                Spatial coords of the atoms. The unique index number, i, of each node in 
-                               the network corresponds to the ith entry in the coords array.  
-    
+
+    1) A graph whose nodes are atom objects and whose edges have bonds
+       attributes. This defines the topology of the molecule.
+    2) A list of coords of the atomic positions defining the current
+       state of the molecule.
+    3) A collection of functions for performing operations on and
+       calculations about the molecule
+
+    self.topology     Network X Graph. Nodes are atoms and edges are bonds.
+    self.coords       Spatial coords of the atoms. The unique index number, i,
+                      of each node in the network corresponds to the ith
+                      entry in the coords array.
     """
 
-    def __init__(self, l_input_filename):
+    def __init__(self, l_input_filename, args):
         '''
         Initialises an instance of a molecular class from a file of data.
-        
+
         Takes advantage of the the pymol file interface to parse a range of
         files such as PDBs and XYZ files
-        
+
         Constructs a graph of the molecule.
-        and maintains a list of atomic coords            
+        and maintains a list of atomic coords.
         '''
-        #Set up Pymol
-        pymol=pymol_wrapper.init('-qc')
-            
-        #load the data into pymol    
-        self.load_data_pymol(pymol,l_input_filename)
+        # Set up Pymol
+        pymol = pymol_wrapper.init(args)
 
-        #Extract the pymol object defining the molecule
-        l_pymol_data=self.extract_data_pymol(pymol)
-        
-        #initialise member variables of Molecule object
-        self.coords=[]
-        self.topology=nx.Graph()
-        
-        #get the atomic information from the pymol data.
-        #This populates the self.coords array and 
-        #constructs the nodes of self.topology but with no edge information.
-        l_pymol_index_map=self.extract_atoms(l_pymol_data)
-        
-        #construct the edges of the graph from the pymol data and the index map.
-        #Populates self.topology 
-        self.extract_topology(l_pymol_data,l_pymol_index_map)
-        
+        # load the data into pymol
+        self.load_data_pymol(pymol, l_input_filename)
 
-    def load_data_pymol(self,pymol,l_input_filename):
+        # Extract the pymol object defining the molecule
+        l_pymol_data = self.extract_data_pymol(pymol)
+
+        # Initialise member variables of Molecule object
+        self.coords = []
+        self.topology = nx.Graph()
+
+        # Gget the atomic information from the pymol data.
+        # This populates the self.coords array and
+        # constructs the nodes of self.topology but with no edge information.
+        l_pymol_index_map = self.extract_atoms(l_pymol_data)
+
+        # Construct edges of the graph from the pymol data and the index map.
+        # Populates self.topology.
+        self.extract_topology(l_pymol_data, l_pymol_index_map)
+
+    def load_data_pymol(self, pymol, l_input_filename):
         '''Loads a file into pymol object to take advantage of it's file handling
         and molecular construction capability.'''
+
         try:
-            #determine file type from file header
-            l_fileType=l_input_filename[-3:]
-            
-            #process pdb file
-            if l_fileType=='pdb':
-                #attempt to load the data
-                pymol.cmd.load(l_input_filename)
+            with open(l_input_filename, "r") as f_in:
+                f_in.close()
+        except IOError:
+            raise Exception("Unable to open file: " + l_input_filename)
 
-            if l_fileType=='xyz':
-                #pymol loads all the frames in an xyz file,
-                #This function loads only the first frame.
-                l_raw_data=read_text_file(l_input_filename)
-                l_num_atoms=int(l_raw_data[0])
-                write_text_file('temporaryFile.xyz',l_raw_data[0:l_num_atoms+2-1])
-                pymol.cmd.load('temporaryFile.xyz')
-                os.remove('temporaryFile.xyz')
-
-        except: 
-            #unable to load file
-            print "Unable to load filename: ",l_input_filename
-            sys.exit()
-    
-    def extract_data_pymol(self,pymol):
-        '''Function extracts all the data about a single model from pymol''' 
         try:
-            #Get the name of the last object that was loaded into the local pymol instance
-            #and return all information about that pymol object
-            return(pymol.cmd.get_model(pymol.cmd.get_object_list()[-1]))  
-        except:
-            print "Unable to extract molecular information from Pymol"
-            sys.exit()
-    
-    def extract_atoms(self,l_pymol_object_data):
-        '''Extract the coordinates of each atom from pymol 
-           Constructs an atom object for each atom and adds it as a node to the graph.
-           returns a dictionary which maps the pymol index to the pele index.'''
-        
+            # Determine file type from file header
+            l_fileType = l_input_filename[-3:]
+        except IndexError:
+            raise Exception("Cannot find file header: " + l_input_filename)
+
+        # Process pdb file
+        if l_fileType == 'pdb':
+            # Attempt to load the data
+            pymol.cmd.load(l_input_filename)
+        elif l_fileType == 'xyz':
+            # Pymol loads all the frames in an xyz file,
+            # This function loads only the first frame.
+            try:
+                with open(l_input_filename, "r") as f_in:
+                    l_raw_data = f_in.readlines()
+            except IOError:
+                raise Exception("Cannot open input file:" + l_input_filename)
+
+            # Extract the number of atoms
+            l_num_atoms = int(l_raw_data[0])
+
+            try:
+                with open('temporaryFile.xyz', "w") as f_out:
+                    f_out.writelines(l_raw_data[0:l_num_atoms + 2])
+            except IOError:
+                raise Exception("Unable to write xyz frame for output")
+
+            pymol.cmd.load('temporaryFile.xyz')
+            os.remove('temporaryFile.xyz')
+        else:
+            raise Exception("Unrecognised file type: " + l_input_filename)
+
+    def extract_data_pymol(self, pymol):
+        '''Function extracts all the data about a single model from pymol'''
+        # Get the name of the last object loaded into pymol instance
+        # and return all information about that pymol object
         try:
-            #index keeping track of position in coords array
-            l_atom_id=0 
-            l_pymol_index_map={}
-            #loop through the atoms in the pymol object and convert into 
-            #the pele molecular representation.
-            for l_atom_pymol in l_pymol_object_data.atom:
-                    #populate the coords array coords
-                    self.coords.append(l_atom_pymol.coord[0])
-                    self.coords.append(l_atom_pymol.coord[1])
-                    self.coords.append(l_atom_pymol.coord[2])
-                    
-                    #create a pele atom object
-                    l_atom=Atom(l_atom_pymol.name, l_atom_id)
-                    
-                    #Add the current atom as a node
-                    self.topology.add_node(l_atom)
-                    
-                    #make a note of the reverse mapping between the the pymol index and the pele atom.
-                    l_pymol_index_map[l_atom_pymol.id]=l_atom
-                    
-                    #increment atomic ID.
-                    l_atom_id+=1
-                    
-            return l_pymol_index_map
-        except:
-            print "Unable to process atomic information from pymol"
-            sys.exit()
+            pymol_object_name = pymol.cmd.get_object_list()[-1]
+        except IndexError:
+            raise Exception("No objects in list obtained from pymol")
+        return pymol.cmd.get_model(pymol_object_name)
 
-    def extract_topology(self,l_pymol_object_data, l_pymol_index_map):
-        '''This function loops through the bond information from pymol, identifies 
-           the pele atoms referred to in the bond and adds the edge to the graph.
-           Creates a pele bond object and associates it with the edge.
-        '''
-        
-        #loop through the bond data
-        try:
-            l_bond_id=0
-            for l_bond in l_pymol_object_data.bond:
-                    #find the pele atom referred to in the pymol bond info 
-                    l_atom1=l_pymol_index_map.get(l_bond.index[0],None)
-                    l_atom2=l_pymol_index_map.get(l_bond.index[1],None)
+    def extract_atoms(self, l_pymol_object_data):
+        '''Extract the coordinates of each atom from pymol
+           Constructs an atom object for each atom and adds it as a node
+           to the graph. Returns a dictionary which maps the pymol index to the
+           pele index.'''
 
-                    if not None in [l_atom1,l_atom2]: #for some reason pymol reports a bond between non-existent atoms...??
-                        #create a pele bond object
-                        bond=Bond(l_bond_id,l_atom1.atom_id,l_atom2.atom_id,l_bond.order)
-                        
-                        #add the edge to the network and associate the bond object to the edge
-                        self.topology.add_edge(l_atom1,l_atom2,object=bond)
+        # Initialise index to keep track of position in coords array
+        l_atom_id = 0
 
-                        #increase count
-                        l_bond_id+=1
-                    
-        except:
-            print "Unable to process bond information from pymol"
-            sys.exit()
+        # Initialise a dictionary mapping the pymol id to the pele atoms
+        l_pymol_index_map = {}
 
-        print "topology done"
+        # Loop through the atoms in the pymol object and convert into
+        # The pele molecular representation.
+        for l_atom_pymol in l_pymol_object_data.atom:
+            try:
+                # Populate the coords array coords
+                self.coords.append(l_atom_pymol.coord[0])
+                self.coords.append(l_atom_pymol.coord[1])
+                self.coords.append(l_atom_pymol.coord[2])
+            except LookupError:
+                raise Exception("Error extracting data from Pymol Object")
+
+            # Create a pele atom object
+            l_atom = Atom(l_atom_id, l_atom_pymol.name)
+
+            # Add the current atom as a node
+            self.topology.add_node(l_atom)
+
+            try:
+                # Make a note of the reverse mapping between the the pymol
+                # index and the pele atom.
+                l_pymol_index_map[l_pymol_object_data.index_atom(l_atom_pymol)] = l_atom
+            except LookupError:
+                raise Exception("Invalid key for atom dictionary")
+
+            # Increment atomic ID.
+            l_atom_id += 1
+
+        return l_pymol_index_map
+
+    def extract_topology(self, l_pymol_object_data, l_pymol_index_map):
+        '''This function loops through the bond information from pymol,
+           identifies the pele atoms referred to in the pymol bond and adds
+           the edge to the graph. A pele bond object is associate with the
+           edge. '''
+        # Initialise index to keep track of bond id
+        l_bond_id = 0
+
+        # Loop through the pymol bond data
+        for l_bond in l_pymol_object_data.bond:
+            try:
+                # Find the pele atom referred to in the pymol bond info
+                l_atom1 = l_pymol_index_map.get(l_bond.index[0], None)
+                l_atom2 = l_pymol_index_map.get(l_bond.index[1], None)
+            except LookupError:
+                raise Exception("Unable to find Pele atom with Pymol index.")
+
+            # Make sure that both atoms were found
+            if None not in [l_atom1, l_atom2]:
+                # Create a pele bond object
+                bond = Bond(l_bond_id,
+                            l_atom1.id,
+                            l_atom2.id,
+                            l_bond.order)
+
+                # Add the edge to the network.
+                # Associate the bond object to the edge.
+                self.topology.add_edge(l_atom1, l_atom2, object=bond)
+
+                # Increase count
+                l_bond_id += 1
+
 
 class Atom(object):
     '''
     A minimal representation of an atom:
-        Mass, atomic number, name, symbol
-         
+        id is unique index in the molecule.
+        symbol identifies the type of atom.
+
+        Conventions for atomic symbols that are understood are the PDB
+        atom naming system and the normal element naming system.
     '''
-    def __init__(self, atom_name, atom_id):
-        self.atom_name=atom_name
-        self.atom_id=atom_id
-        #self.atom_symbol=atom_symbol
-        #self.atom_mass=elem[self.atom_symbol]['mass']
-        #self.atom_number=elem[self.atom_symbol]['atomicNumber']
+    def __init__(self, id, symbol):
+        # populate the member variables of the Atom class.
+        self.id = id
+
+        # A variety of atomic naming conventions within molecules.
+        # exist. E.g. H could be 1H, 2H, OH etc in a pdb file.
+        # Keep a note of the given defining symbol.
+        self.alt_symbol = symbol
+
+        # lookup the standard symbol in the alternative names dictionary.
+        try:
+            self.symbol = elem.alternate_names[self.alt_symbol]
+        except KeyError:
+            # if there is no entry in the alt_names list for the identifier,
+            # assume the identifier is the alternative_symbol.
+            self.symbol = self.alt_symbol
+
+        # look up the remaining element details from the elements dictionary
+        try:
+            self.name = elem.elements[self.symbol]['name']
+        except KeyError:
+            # Raise an exception pointing out that the given symbol is unknown.
+            raise Exception("Unknown element: " + symbol)
+
+        # given symbol has been identified. Acquire remaining details
+        self.mass = elem.elements[self.symbol]['mass']
+        self.radius = elem.elements[self.symbol]['radius']
+        self.color = elem.elements[self.symbol]['color']
+
 
 class Bond(object):
-    ''' 
-    defines a bond between two atoms within a molecule. Atom1 and atom2 are the unique ids 
-    of two atoms within a molecule and bond_id is the unique id of a given bond
     '''
-    def __init__(self,bond_id,atom1_id,atom2_id,bond_order):
-        self.bond_id=bond_id
-        self.atom1_id=atom1_id
-        self.atom2_id=atom2_id
-        self.order=bond_order
+    defines a bond between two atoms within a molecule. Atom1 and atom2
+    are the unique ids of two atoms within a molecule and bond_id is the
+    unique id of a given bond.
+    '''
+    def __init__(self, id, atom1_id, atom2_id, order):
+        self.id = id
+        self.atom1_id = atom1_id
+        self.atom2_id = atom2_id
+        self.order = order
 
-        
 if __name__ == '__main__':
-    
-    HETS=Molecule('2RNM HETS.pdb')
-    PHG12=Molecule('lowest.xyz')
+
+    HETS = Molecule('2RNM HETS.pdb', '-qc')
+    PHG12 = Molecule('lowest.xyz', '-qc')
 
     print "Done"
-    
-            
--- playground/molecule/molecule_test.py
+++ playground/molecule/molecule_test.py
@@ -1,7 +1,7 @@
 from molecule import Molecule, Atom, Bond
-from utilities import write_text_file,read_text_file
 
-
+import networkx as nx
+import unittest
 import sys
 import os
 import urllib
@@ -11,112 +11,142 @@ class TestMolecule(unittest.TestCase):
     """
     a base class for molecule unit tests
     """
-    def runtest(self, X1, X2, mindist):
-        X1i = np.copy(X1)
-        X2i = np.copy(X2)
-        
-        (distreturned, X1, X2) = mindist(X1, X2)
 
-        distinit = np.linalg.norm(self.X1 - X2i)
-        distfinal = np.linalg.norm(X1 - X2)
-        self.assertAlmostEqual( distfinal, distreturned, 5, "returned distance is wrong: %g != %g, difference %g" % (distfinal, distreturned, distfinal-distreturned) )
-        self.assertLessEqual(distfinal, distinit)
-        
-        #test if the energies have changed
-        Ei = self.pot.getEnergy(X1i)        
-        Ef = self.pot.getEnergy(X1)
-        self.assertAlmostEqual( Ei, Ef, 10, "Energy of X1 changed: %g - %g = %g" % (Ei, Ef, Ei - Ef) )
-        Ei = self.pot.getEnergy(X2i)        
-        Ef = self.pot.getEnergy(X2)
-        self.assertAlmostEqual( Ei, Ef, 10, "Energy of X2 changed: %g - %g = %g" % (Ei, Ef, Ei - Ef) )
+    def test_molecule_xyz(self):
+        # Generate test data
+        pdb_data = []
+        pdb_data.append("    4\n")
+        pdb_data.append("    Energy Comment\n")
+        pdb_data.append("N   4.954  -0.924  -5.684\n")
+        pdb_data.append("C   5.427   0.193  -4.880\n")
+        pdb_data.append("C   5.873  -0.251  -3.503\n")
+        pdb_data.append("O   5.756  -1.417  -3.054\n")
 
-        return distreturned, X1, X2
+        # Save test data as a file
+        try:
+            with open('test.xyz', "w") as f_out:
+                f_out.writelines(pdb_data)
+        except IOError:
+            raise Exception("Unable to write xyz file for output")
 
-def test_read_write():
-    lines=["The lazy brown dog jumped over the sly red fox\n","The lazy brown dog jumped over the sly red fox\n"]
-    write_text_file(lines,'testfile.txt')
-    newlines=read_text_file('testfile.txt')
-    os.remove('testfile.txt')
-    
-    return not False in [ False for newline,line in zip(newlines,lines) if newline!=line]
+        self.check_Molecule_Init('test.xyz', '-qc')
 
-def test_pymol():
-        #Set up Pymol
-        pymol=pymol_wrapper.init('-qc')
-            
-        #load the data into pymol    
-        self.load_data_pymol(pymol,l_input_filename)
+        # delete the test file
+        os.remove('test.xyz')
 
-        #Extract the pymol object defining the molecule
-        l_pymol_data=self.extract_data_pymol(pymol)
+    def test_molecule_pdb(self):
+        # Generate test data
+        pdb_data = []
+        pdb_data.append("ATOM      1  N   GLY A   1       4.954  -0.924  -5.684  1.00  4.90           N\n")
+        pdb_data.append("ATOM      2  CA  GLY A   1       5.427   0.193  -4.880  1.00  4.21           C\n")
+        pdb_data.append("ATOM      3  C   GLY A   1       5.873  -0.251  -3.503  1.00  4.62           C\n")
+        pdb_data.append("ATOM      4  O   GLY A   1       5.756  -1.417  -3.054  1.00  5.04           O\n")
+
+        # Save test data as a file
+        try:
+            with open('test.pdb', "w") as f_out:
+                f_out.writelines(pdb_data)
+        except IOError:
+            raise Exception("Unable to write pdb file for output")
+
+        self.check_Molecule_Init('test.pdb', '-qc')
+
+        # delete the test file
+        os.remove('test.pdb')
+
+    def check_Molecule_Init(self, filename, args):
+
+        # Create the Molecule class for testing
+        test_molecule = Molecule(filename, args)
+
+        # Generate comparison coords list
+        coords = [4.954, -0.924, -5.684,
+                  5.427, 0.193, -4.880,
+                  5.873, -0.251, -3.503,
+                  5.756, -1.417, -3.054]
+
+        # Generate the Atom objects - zero based index ordered as in file.
+        N = Atom(0, 'N')
+        CA = Atom(1, 'CA')
+        C = Atom(2, 'C')
+        O = Atom(3, 'O')
+
+        # Create the bond objects using atoms ids.
+        bond_N_CA = Bond(1, 0, 1, 1)
+        bond_CA_C = Bond(2, 1, 2, 1)
+        bond_C_O = Bond(3, 2, 3, 2)
+
+        # Create the graph
+        mol_graph = nx.Graph()
+
+        # add the nodes in the order of appearance in the file.
+        mol_graph.add_node(N)
+        mol_graph.add_node(CA)
+        mol_graph.add_node(C)
+        mol_graph.add_node(O)
+
+        # add the edges. don't care so much about the order here.
+        mol_graph.add_edge(N, CA, object=bond_N_CA)
+        mol_graph.add_edge(CA, C, object=bond_CA_C)
+        mol_graph.add_edge(C, O, object=bond_C_O)
+
+        # confirm that test molecule representation is identical to
+        # the molecule created locally.
+
+        # Check the coords are the same - same as the order in file.
+        for coord, test_coord in zip(coords, test_molecule.coords):
+            self.assertAlmostEqual(coord, test_coord, delta=0.0001)
+
+        # ensure that both networks are isomorphic - same number of
+        # nodes and same edge connection pattern.
+        l_isomorphic = nx.is_isomorphic(mol_graph, test_molecule.topology)
+        self.assertTrue(l_isomorphic)
+
+        # if the network is isomorphic check that the nodes
+        # are correctly labelled
+        if l_isomorphic:
+            for l_atom, l_test_atom in zip(mol_graph.nodes(),
+                                           test_molecule.topology.nodes()):
+                # check the atom types and ids are the same in both graphs
+                self.assertEqual(l_test_atom.symbol,
+                                 ['N', 'C', 'C', 'O'][l_test_atom.id])
+                self.assertEqual(l_atom.symbol,
+                                 ['N', 'C', 'C', 'O'][l_atom.id])
+
+            # ensure the edges in the test molecule connect the same atoms
+            # as the edges in the constructed molecule
+            bond_labels = {tuple(sorted([bond[0].id, bond[1].id])) for bond in mol_graph.edges_iter()}
+            test_bond_labels = {tuple(sorted([test_bond[0].id, test_bond[1].id])) for test_bond in test_molecule.topology.edges_iter()}
+
+            # check that the sets are equivalent (a permutation of each other)
+            self.assertTrue(test_bond_labels == bond_labels)
+
+    def test_atom(self):
+        atom = Atom(1, '1H')
+        self.assertEqual(1, atom.id)
+        self.assertEqual('1H', atom.alt_symbol)
+        self.assertEqual('hydrogen', atom.name)
+        self.assertEqual('H', atom.symbol)
+        self.assertEqual([1.000, 1.000, 1.000], atom.color)
+        self.assertEqual(1.00794000, atom.mass)
+        self.assertEqual(1.2000, atom.radius)
+
+        atom2 = Atom(2, 'Na')
+        self.assertEqual(2, atom2.id)
+        self.assertEqual('Na', atom2.alt_symbol)
+        self.assertEqual('sodium', atom2.name)
+        self.assertEqual('Na', atom2.symbol)
+        self.assertEqual([0.671, 0.361, 0.949], atom2.color)
+        self.assertEqual(22.98976928, atom2.mass)
+        self.assertEqual(2.2700, atom2.radius)
+
+    def test_bond(self):
+        bond = Bond(1, 2, 3, 4)
+        self.assertEqual(1, bond.id)
+        self.assertEqual(2, bond.atom1_id)
+        self.assertEqual(3, bond.atom2_id)
+        self.assertEqual(4, bond.order)
 
 
-def test_extract_atoms():
-    
-def test_extract_data():
-    
-def test_extract_topology():
-    
-
-def test_molecule:
-
-
-def test_atom():
-    atom=Atom(1,'H')
-    result=False
-    if atom.atom_id==1and atom.atom_name=='H':
-        result=True
-    return result
-    
-def test_bond():
-    bond=Bond(1,1,2,1)
-    result=False
-    if bond.bond_id==1 and bond.atom1_id=='H' and bond.atom2_id==2 and bond.order==1:
-        result=True
-    return result
-
-def fetch_pdb(id):
-  url = 'http://www.rcsb.org/pdb/files/%s.pdb' % id
-  return urllib.urlopen(url).read()
-
-
-if __name__=='__main__':
-
-    testResults=[]
-    testResults.append(test_atom())
-    testResults.append(test_bond())
-
-    #create a "molecule"
-    test_data_coords=[0.0 0.0 0.0 
-                      0.0 1.0 0.0 
-                      0.0 1.0 -1.0]
-    
-    atom1=Atom(1,'H')
-    atom1=Atom(2,'O')
-    atom1=Atom(3,'H')
-    
-    
-
-    
-    
-    testResults.append(test_read_write())
-    
-    
-    
-    testStructure = write_text_file(fetch_pdb('2RNM'),'2RNM.pdb')
-    
-    
-    HETS=Molecule('2RNM.pdb')
-    
-    HETS.coords
-    HETS.extract_atoms(l_pymol_object_data)
-    HETS.extract_data_pymol(pymol)
-    HETS.extract_topology(l_pymol_object_data, l_pymol_index_map)
-    HETS.load_data_pymol(pymol, l_input_filename)
-    HETS.topology
-    HETS.__init__(l_input_filename)
-    
-    
-    
-    
-    
+if __name__ == '__main__':
+    unittest.main()
