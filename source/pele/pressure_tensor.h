4920d4deb71ef95751ee0c361fe4dbbf6a5de9f6
Julian Schrenk <kjs73@cam.ac.uk>
2015-05-13 11:33:58 +0100

    fix typos, fix includes

--- source/pele/pressure_tensor.h
+++ source/pele/pressure_tensor.h
@@ -1,9 +1,11 @@
 #ifndef _PELE_PRESSURE_TENSOR_H
 #define _PELE_PRESSURE_TENSOR_H
 
+#include "pele/base_potential.h"
+
 namespace pele {
 
-double pressure_tensor(std::shared_prt<pele::BasePotential> pot,
+double pressure_tensor(std::shared_ptr<pele::BasePotential> pot,
                        const pele::Array<double>& x,
                        pele::Array<double>& ptensor,
                        const double volume);
--- source/pressure_tensor.cpp
+++ source/pressure_tensor.cpp
@@ -2,12 +2,12 @@
 
 namespace pele {
 
-double pressure_tensor(std::shared_prt<pele::BasePotential> pot,
+double pressure_tensor(std::shared_ptr<pele::BasePotential> pot,
                        const pele::Array<double>& x,
                        pele::Array<double>& ptensor,
                        const double volume)
 {
-    
+        
 }
     
 } // namespace pele
