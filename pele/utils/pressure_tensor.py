9847204668c59b3779e01b18919dd6cad8c2e1e5
Julian Schrenk <kjs73@cam.ac.uk>
2015-05-13 08:54:47 +0100

    paste documentation

---
+++ pele/utils/pressure_tensor.py
@@ -0,0 +1,6 @@
+"""
+    Computes the pressure tensor and returns the pressure.
+    The negative of the pressure tensor is often called the stress
+    tensor, see Allen, Tidsley, "Computer Simulation of Liquids"
+    pp. 60-61.
+"""
