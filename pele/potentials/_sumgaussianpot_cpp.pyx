941727ea3b7429e05d070dcfed79d541f46c8ce0
Julian Schrenk <kjs73@cam.ac.uk>
2015-08-11 18:05:29 +0100

    add documentation for gaussian potential

--- pele/potentials/_sumgaussianpot_cpp.pyx
+++ pele/potentials/_sumgaussianpot_cpp.pyx
@@ -18,17 +18,28 @@ cdef extern from "pele/sumgaussianpot.h" namespace "pele":
 
 cdef class GaussianPot(_pele.BasePotential):
     """python interface to c++ GaussianPot
+    
+    Gaussian-shaped potential function. For given arrays mean and cov, the energy of a configuration with coordinates $x$ and $N$ degrees of freedom is given by:
+    $\text{energy}(\text{mean}; \text{cov}, x) = -\exp \left[ -1/2 \sum_{i=1}^N (x_i - \text{mean}_i)^2 / \text{cov}_i \right]$.
+    
+    Parameters
+    ----------
+    mean : array
+        Mean of the gaussian, needs to be of lenght bdim.
+    cov : array
+        Diagonal of the covariance matrix of the gaussian, needs to be
+        of length bdim.
     """
-    def __cinit__(self, means, cov):
-        if (means.shape != cov.shape):
-            print("means.shape", means.shape)
+    def __cinit__(self, mean, cov):
+        if (mean.shape != cov.shape):
+            print("mean.shape", mean.shape)
             print("cov.shape", cov.shape)
             raise Exception("GaussianPot: illegal input")
-        means = means.flatten()
+        mean = mean.flatten()
         cov = cov.flatten()
-        cdef _pele.Array[double] cmeans = array_wrap_np(means)
+        cdef _pele.Array[double] cmean = array_wrap_np(mean)
         cdef _pele.Array[double] ccov = array_wrap_np(cov)
-        self.thisptr = shared_ptr[_pele.cBasePotential](<_pele.cBasePotential*> new cppGaussianPot(cmeans, ccov))
+        self.thisptr = shared_ptr[_pele.cBasePotential](<_pele.cBasePotential*> new cppGaussianPot(cmean, ccov))
 
 cdef class SumGaussianPot(_pele.BasePotential):
     """Sum of GaussianPot
