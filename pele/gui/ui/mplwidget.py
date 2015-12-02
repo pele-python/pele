5016cc5d868139a47667209e793dffeff7f8eec8
Stefano Martiniani <stefano.martiniani@gmail.com>
2015-12-02 10:52:15 +0000

    fixed import issue with gui

--- pele/gui/ui/mplwidget.py
+++ pele/gui/ui/mplwidget.py
@@ -1,6 +1,6 @@
 from PyQt4 import QtGui, QtCore
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
 from PyQt4.QtGui import QWidget, QVBoxLayout
 
 from matplotlib.figure import Figure
