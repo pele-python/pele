637d7ad4544b71f65986b450132bdce9ee950de0
smcantab <stefano.martiniani@gmail.com>
2016-01-11 11:57:57 +0000

    implemented gradient with backprop

--- playground/pspin/pspin_potential_tf.py
+++ playground/pspin/pspin_potential_tf.py
@@ -2,16 +2,25 @@ import numpy as np
 import tensorflow as tf
 from pele.potentials import BasePotential
 
+class DoubleGDOptimizer(tf.train.GradientDescentOptimizer):
+    """
+    this wrapper adds support for float64 for tf.train.GradientDescentOptimizer
+    """
+    def _valid_dtypes(self):
+        return set([tf.float32, tf.float64])
+
 class MeanFieldPSpinSphericalTF(BasePotential):
     def __init__(self, nspins, interaction, p=3):
         assert p==3
         assert interaction.shape == (nspins, nspins, nspins)
         self.nspins = nspins
-        self.interaction = tf.constant(interaction)
-        self.x_ = tf.placeholder(dtype='float32', shape=[nspins])
-        #self.x_ = tf.Variable(tf.zeros([self.nspins], dtype='float32'))
-        init = tf.initialize_all_variables()
+        self.interaction = tf.constant(interaction, tf.float64)
+        # self.x_ = tf.placeholder(dtype='float64', shape=[nspins])
+        self.xi = tf.Variable(tf.zeros([self.nspins], dtype='float64'))
+        self.xj = tf.Variable(tf.zeros([self.nspins], dtype='float64'))
+        self.xk = tf.Variable(tf.zeros([self.nspins], dtype='float64'))
         self.session = tf.Session()
+        init = tf.initialize_all_variables()
         self.session.run(init)
 
     def __exit__(self, exc_type, exc_value, traceback):
@@ -20,12 +29,12 @@ class MeanFieldPSpinSphericalTF(BasePotential):
     @property
     def lossTensorPartial(self):
         """this gives a 2x2 matrix generator"""
-        return tf.mul(tf.reshape(self.x_, [self.nspins]), tf.reshape(self.x_, [self.nspins,1]))
+        return tf.mul(tf.reshape(self.xi, [self.nspins]), tf.reshape(self.xj, [self.nspins,1]))
 
     @property
     def lossTensor(self):
         """this gives the full loss tensor generator"""
-        return tf.mul(self.lossTensorPartial, tf.reshape(self.x_, [self.nspins,1,1]))
+        return tf.mul(self.lossTensorPartial, tf.reshape(self.xk, [self.nspins,1,1]))
 
     @property
     def loss(self):
@@ -33,25 +42,25 @@ class MeanFieldPSpinSphericalTF(BasePotential):
         return tf.reduce_sum(tf.mul(self.interaction, self.lossTensor))
 
     def getEnergy(self, coords):
-        e = self.session.run(self.loss, feed_dict={self.x_:coords})
+        self.session.run(tf.group(self.xi.assign(coords),
+                                  self.xj.assign(coords),
+                                  self.xk.assign(coords)))
+        e = self.session.run(self.loss)
         return -e
 
-    # @property
-    # def loss_grad(self):
-    #     return tf.gradients(self.lossTensor, self.x_)
+    def getEnergyGradient(self, coords):
+        self.session.run(tf.group(self.xi.assign(coords),
+                                  self.xj.assign(coords),
+                                  self.xk.assign(coords)))
+        e = self.session.run(self.loss)
+        print tf.gradients(self.loss, self.xk)[0].eval(session=self.session)
+        print self.xk.eval(session=self.session)
+        return -e
 
-    # def getEnergyGradient(self, coords):
-    #   e = self.session.run(self.loss, feed_dict={self.x_:coords})
-    #   g = self.session.run(self.loss_grad, feed_dict={self.x_:coords})
-    #   grads_and_vars = self.opt.compute_gradients(self.loss)
-    #   self.opt = tf.train.GradientDescentOptimizer(learning_rate=0.1)
-    #   g, v = self.session.run(grads_and_vars)
-    #   for g, v in grads_and_vars:
-    #       print v
-    #       print g
 
 if __name__ == "__main__":
-    pot = MeanFieldPSpinSphericalTF(5, np.ones([5,5,5], dtype='float32'))
-    e = pot.getEnergy(np.ones(5, dtype='float32'))
-    print e
-    assert e == -125.
+    n = 5
+    pot = MeanFieldPSpinSphericalTF(n, np.ones([n,n,n], dtype='float64'))
+    coords = np.ones(n, dtype='float64')
+    print pot.getEnergy(coords)
+    print pot.getEnergyGradient(coords)
