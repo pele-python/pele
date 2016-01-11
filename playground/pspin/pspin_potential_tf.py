5c4e6194d4f197f80f11017c7c8d843f10d05e27
smcantab <stefano.martiniani@gmail.com>
2016-01-11 15:38:39 +0000

    implemented functioning pspin potential based on tensorflow

--- playground/pspin/pspin_potential_tf.py
+++ playground/pspin/pspin_potential_tf.py
@@ -1,6 +1,11 @@
 import numpy as np
 import tensorflow as tf
 from pele.potentials import BasePotential
+from itertools import permutations, combinations
+from pele.transition_states._zeroev import orthogonalize
+from scipy.special import factorial
+from pele.potentials import MeanFieldPSpinSpherical
+from numba import jit
 
 class DoubleGDOptimizer(tf.train.GradientDescentOptimizer):
     """
@@ -10,12 +15,14 @@ class DoubleGDOptimizer(tf.train.GradientDescentOptimizer):
         return set([tf.float32, tf.float64])
 
 class MeanFieldPSpinSphericalTF(BasePotential):
-    def __init__(self, nspins, interaction, p=3):
+    def __init__(self, interactions, nspins, p=3):
         assert p==3
-        assert interaction.shape == (nspins, nspins, nspins)
+        assert interactions.shape == (nspins, nspins, nspins)
+        self.p = p
         self.nspins = nspins
-        self.interaction = tf.constant(interaction, tf.float64)
-        # self.x_ = tf.placeholder(dtype='float64', shape=[nspins])
+        self.prf = factorial(self.p) * np.power(np.sqrt(self.nspins), self.p-1) if self.p > 2 else 1.
+        interactions = self._adaptInteractions(interactions)
+        self.interactions = tf.constant(interactions, tf.float64)
         self.xi = tf.Variable(tf.zeros([self.nspins], dtype='float64'))
         self.xj = tf.Variable(tf.zeros([self.nspins], dtype='float64'))
         self.xk = tf.Variable(tf.zeros([self.nspins], dtype='float64'))
@@ -25,6 +32,19 @@ class MeanFieldPSpinSphericalTF(BasePotential):
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.session.close()
+
+    def _adaptInteractions(self, interactions):
+        """
+        this function guarantees that the repeated indexes terms are zero
+        :param nspins:
+        :param p:
+        :return:
+        """
+        new_interactions = np.zeros(interactions.shape)
+        for comb in combinations(range(self.nspins), self.p):
+                for perm in permutations(comb):
+                    new_interactions[perm] = interactions[perm]
+        return new_interactions
 
     @property
     def lossTensorPartial(self):
@@ -39,28 +59,41 @@ class MeanFieldPSpinSphericalTF(BasePotential):
     @property
     def loss(self):
         """this mutliplies times the interaction and reduces sum reduces the tensor"""
-        return tf.reduce_sum(tf.mul(self.interaction, self.lossTensor))
+        return tf.reduce_sum(tf.mul(self.interactions, self.lossTensor))
 
+    def _setSpins(self, coords):
+        self.session.run(tf.group(self.xi.assign(coords),
+                                  self.xj.assign(coords),
+                                  self.xk.assign(coords)))
+    @jit
+    def _orthog_to_zero(self, v, zerov):
+        return orthogonalize(v, zerov)
+
+    @jit
     def getEnergy(self, coords):
-        self.session.run(tf.group(self.xi.assign(coords),
-                                  self.xj.assign(coords),
-                                  self.xk.assign(coords)))
-        e = self.session.run(self.loss)
-        return -e
+        # coords /= np.linalg.norm(coords)
+        self._setSpins(coords)
+        e = -self.session.run(self.loss)
+        return e/self.prf
 
+    @jit
     def getEnergyGradient(self, coords):
-        self.session.run(tf.group(self.xi.assign(coords),
-                                  self.xj.assign(coords),
-                                  self.xk.assign(coords)))
-        e = self.session.run(self.loss)
-        print tf.gradients(self.loss, self.xk)[0].eval(session=self.session)
-        print self.xk.eval(session=self.session)
-        return -e
-
+        # coords /= np.linalg.norm(coords)
+        self._setSpins(coords)
+        e = -self.session.run(self.loss)
+        grad = -tf.gradients(self.loss, self.xk)[0].eval(session=self.session)
+        grad = self._orthog_to_zero(grad/self.prf, coords)
+        return e/self.prf, grad
 
 if __name__ == "__main__":
-    n = 5
-    pot = MeanFieldPSpinSphericalTF(n, np.ones([n,n,n], dtype='float64'))
+    n = 150
+    # interactions = np.random.random((n,n,n))
+    interactions = np.ones((n,n,n))
+
+    potTF = MeanFieldPSpinSphericalTF(interactions, n)
+    potPL = MeanFieldPSpinSpherical(interactions.flatten(), n, 3)
     coords = np.ones(n, dtype='float64')
-    print pot.getEnergy(coords)
-    print pot.getEnergyGradient(coords)
+    # coords /= np.linalg.norm(coords)
+    import timeit
+    print timeit.timeit('e, grad = potPL.getEnergyGradient(coords)', "from __main__ import potPL, coords", number=100)
+    print timeit.timeit('e, grad = potTF.getEnergyGradient(coords)', "from __main__ import potTF, coords", number=100)
