d34df2b2046734403216db33c82aef89683a4c63
Julian Schrenk <kjs73@cam.ac.uk>
2016-03-23 17:29:13 +0000

    add rectanglular cell lists tests, minimisation

--- cpp_tests/source/test_cell_lists.cpp
+++ cpp_tests/source/test_cell_lists.cpp
@@ -1,9 +1,10 @@
 #include "pele/array.h"
-#include "pele/inversepower.h"
-#include "pele/hs_wca.h"
 #include "pele/cell_lists.h"
-#include "pele/modified_fire.h"
 #include "pele/distance.h"
+#include "pele/hs_wca.h"
+#include "pele/inversepower.h"
+#include "pele/lj_cut.h"
+#include "pele/modified_fire.h"
 
 #include <iostream>
 #include <stdexcept>
@@ -14,6 +15,9 @@
 using pele::Array;
 using pele::InversePowerPeriodic;
 using pele::InversePower_interaction;
+
+static double const EPS = std::numeric_limits<double>::min();
+#define EXPECT_NEAR_RELATIVE(A, B, T)  EXPECT_NEAR(A/(fabs(A)+fabs(B) + EPS), B/(fabs(A)+fabs(B) + EPS), T)
 
 
 // this is used only for tests.  It should be moved to the tests folder
@@ -63,7 +67,7 @@ size_t get_direct_nr_unique_pairs(std::shared_ptr<distance_policy> dist,
 class CellListsTest : public ::testing::Test {
 public:
     double pow, eps, etrue, etrue_r, rcut, sca;
-    Array<double> x, g, gnum, radii, boxvec, boxvec_r;
+    Array<double> x, g, gnum, radii, boxvec;
     void SetUp(){
     	pow = 2.5;
     	eps = 1;
@@ -79,7 +83,6 @@ public:
         x[8] = 2.49;
         radii = Array<double>(3);
         boxvec = Array<double>(3, 5);
-        boxvec_r = {4.5, 5, 5.6};
         for (size_t j = 0; j < 3; ++j) {
             double center = 0;
             for (size_t k = 0; k < 3; ++k) {
@@ -104,18 +107,15 @@ public:
 //test number of distinguishable pairs
 TEST_F(CellListsTest, Number_of_neighbors){
     pele::CellLists<> cell(std::make_shared<pele::periodic_distance<3> >(boxvec), boxvec, boxvec[0]);
-    pele::CellLists<pele::periodic_distance<3> > cell_r(std::make_shared<pele::periodic_distance<3> >(boxvec_r), boxvec_r, 4.5);
     pele::CellLists<> cell2(std::make_shared<pele::periodic_distance<3> >(boxvec), boxvec, boxvec[0], 1);
     pele::CellLists<> cell3(std::make_shared<pele::periodic_distance<3> >(boxvec), boxvec, boxvec[0], 4.2);
     pele::CellLists<> cell4(std::make_shared<pele::periodic_distance<3> >(boxvec), boxvec, boxvec[0], 5);
     cell.reset(x);
-    cell_r.reset(x);
     cell2.reset(x);
     cell3.reset(x);
     cell4.reset(x);
     size_t count = 3u;
     ASSERT_EQ(count, get_nr_unique_pairs(cell));
-    ASSERT_EQ(count, get_nr_unique_pairs(cell_r));
     ASSERT_EQ(count, get_nr_unique_pairs(cell2));
     ASSERT_EQ(count, get_nr_unique_pairs(cell3));
     ASSERT_EQ(count, get_nr_unique_pairs(cell4));
@@ -123,18 +123,15 @@ TEST_F(CellListsTest, Number_of_neighbors){
 
 TEST_F(CellListsTest, Number_of_neighbors_Cartesian){
     pele::CellLists<pele::cartesian_distance<3> > cell(std::make_shared<pele::cartesian_distance<3> >(), boxvec, boxvec[0]);
-    pele::CellLists<pele::cartesian_distance<3> > cell_r(std::make_shared<pele::cartesian_distance<3> >(), boxvec_r, 4.5);
     pele::CellLists<pele::cartesian_distance<3> > cell2(std::make_shared<pele::cartesian_distance<3> >(), boxvec, boxvec[0], 1);
     pele::CellLists<pele::cartesian_distance<3> > cell3(std::make_shared<pele::cartesian_distance<3> >(), boxvec, boxvec[0], 4.2);
     pele::CellLists<pele::cartesian_distance<3> > cell4(std::make_shared<pele::cartesian_distance<3> >(), boxvec, boxvec[0], 5);
     cell.reset(x);
-    cell_r.reset(x);
     cell2.reset(x);
     cell3.reset(x);
     cell4.reset(x);
     size_t count = 3u;
     ASSERT_EQ(count, get_nr_unique_pairs(cell));
-    ASSERT_EQ(count, get_nr_unique_pairs(cell_r));
     ASSERT_EQ(count, get_nr_unique_pairs(cell2));
     ASSERT_EQ(count, get_nr_unique_pairs(cell3));
     ASSERT_EQ(count, get_nr_unique_pairs(cell4));
@@ -163,65 +160,38 @@ TEST_F(CellListsTest, NumberNeighborsDifferentRcut_Works){
 TEST_F(CellListsTest, NumberNeighborsDifferentRcut_WorksCartesian){
     auto dist = std::make_shared<pele::cartesian_distance<3> >();
     pele::CellLists<pele::cartesian_distance<3> > cell(dist, boxvec, boxvec[0]);
-    pele::CellLists<pele::cartesian_distance<3> > cell_r(dist, boxvec_r, boxvec_r[0]);
     pele::CellLists<pele::cartesian_distance<3> > cell2(dist, boxvec, boxvec[0], 1);
-    pele::CellLists<pele::cartesian_distance<3> > cell2_r(dist, boxvec_r, boxvec_r[0], 1);
     pele::CellLists<pele::cartesian_distance<3> > cell3(dist, boxvec, boxvec[0], 4.2);
-    pele::CellLists<pele::cartesian_distance<3> > cell3_r(dist, boxvec_r, boxvec_r[0], 4.2);
     pele::CellLists<pele::cartesian_distance<3> > cell4(dist, boxvec, boxvec[0], 5);
-    pele::CellLists<pele::cartesian_distance<3> > cell4_r(dist, boxvec_r, boxvec_r[0], 5);
     cell.reset(x);
-    cell_r.reset(x);
     cell2.reset(x);
-    cell2_r.reset(x);
     cell3.reset(x);
-    cell3_r.reset(x);
     cell4.reset(x);
-    cell4_r.reset(x);
     size_t count = get_direct_nr_unique_pairs(dist, boxvec[0], x);
     size_t count2 = get_nr_unique_pairs(cell2);
-    size_t count2_r = get_nr_unique_pairs(cell2_r);
     size_t count3 = get_nr_unique_pairs(cell3);
-    size_t count3_r = get_nr_unique_pairs(cell3_r);
     size_t count4 = get_nr_unique_pairs(cell4);
-    size_t count4_r = get_nr_unique_pairs(cell4_r);
     ASSERT_EQ(3u, count);
     ASSERT_EQ(count, count2);
-    ASSERT_EQ(count, count2_r);
     ASSERT_EQ(count, count3);
-    ASSERT_EQ(count, count3_r);
     ASSERT_EQ(count, count4);
-    ASSERT_EQ(count, count4_r);
 }
 
 TEST_F(CellListsTest, Energy_Works){
     pele::InversePowerPeriodicCellLists<3> pot_cell(pow, eps, radii, boxvec, 1.0);
-    pele::InversePowerPeriodicCellLists<3> pot_cell_r(pow, eps, radii, boxvec_r, 1.0);
     pele::InversePowerPeriodicCellLists<3> pot_cell2(pow, eps, radii, boxvec, 2.0);
-    pele::InversePowerPeriodicCellLists<3> pot_cell2_r(pow, eps, radii, boxvec_r, 2.0);
     pele::InversePowerPeriodicCellLists<3> pot_cell3(pow, eps, radii, boxvec, 3.0);
-    pele::InversePowerPeriodicCellLists<3> pot_cell3_r(pow, eps, radii, boxvec_r, 3.0);
     pele::InversePowerPeriodicCellLists<3> pot_cell4(pow, eps, radii, boxvec, 4.0);
-    pele::InversePowerPeriodicCellLists<3> pot_cell4_r(pow, eps, radii, boxvec_r, 4.0);
     pele::InversePowerPeriodic<3> pot(pow, eps, radii, boxvec);
-    pele::InversePowerPeriodic<3> pot_r(pow, eps, radii, boxvec_r);
     const double ecell = pot_cell.get_energy(x);
-    const double ecell_r = pot_cell_r.get_energy(x);
     const double ecell2 = pot_cell2.get_energy(x);
-    const double ecell2_r = pot_cell2_r.get_energy(x);
     const double ecell3 = pot_cell3.get_energy(x);
-    const double ecell3_r = pot_cell3_r.get_energy(x);
     const double ecell4 = pot_cell4.get_energy(x);
-    const double ecell4_r = pot_cell4_r.get_energy(x);
     const double etrue = pot.get_energy(x);
     ASSERT_NEAR(ecell, etrue, 1e-10);
-    ASSERT_NEAR(ecell_r, etrue_r, 1e-10);
     ASSERT_NEAR(ecell2, etrue, 1e-10);
-    ASSERT_NEAR(ecell2_r, etrue_r, 1e-10);
     ASSERT_NEAR(ecell3, etrue, 1e-10);
-    ASSERT_NEAR(ecell3_r, etrue_r, 1e-10);
     ASSERT_NEAR(ecell4, etrue, 1e-10);
-    ASSERT_NEAR(ecell4_r, etrue_r, 1e-10);
 }
 
 TEST_F(CellListsTest, ChangeCoords_Works){
@@ -245,8 +215,6 @@ TEST_F(CellListsTest, ChangeCoords_Works){
     ecell = pot_cell.get_energy(x);
     etrue = pot.get_energy(x);
     ASSERT_NEAR(ecell, etrue, 1e-10);
-
-
 }
 
 
@@ -1200,4 +1168,71 @@ TEST(LatticeNeighborsTest, NonPeriodic_Works2)
 
     neibs = lattice.find_all_neighbors(2);
     ASSERT_EQ(neibs.size(), size_t(2*3*2));
+}
+
+using pele::SimplePairwisePotential;
+using pele::lj_interaction_cut_smooth;
+using pele::periodic_distance;
+
+template<size_t NDIM>
+class LJCutPeriodicN : public SimplePairwisePotential<lj_interaction_cut_smooth, periodic_distance<NDIM> > {
+public:
+    LJCutPeriodicN(double C6, double C12, double rcut, Array<double> const boxvec)
+        : SimplePairwisePotential< lj_interaction_cut_smooth, periodic_distance<NDIM> > (
+                std::make_shared<lj_interaction_cut_smooth>(C6, C12, rcut),
+                std::make_shared<periodic_distance<NDIM> >(boxvec)
+                ) 
+    {}
+};
+
+template<size_t ndim>
+void test_rectangular_cell_lists()
+{
+    double rcut = 2.5;
+    Array<double> boxvec(ndim);
+    for (size_t i = 0; i < ndim; ++i) {
+        boxvec[i] = std::pow((2 * rcut + 1), i + 1) + 0.8 * i;
+    }
+    const size_t N = 25;
+    pele::Array<double> x(ndim * N);
+    std::mt19937_64 gen(42);
+    for (size_t i = 0; i < N; ++i) {
+        x[i * ndim] = std::uniform_real_distribution<double>{0, boxvec[0]}(gen);
+        x[i * ndim + 1] = std::uniform_real_distribution<double>{0, boxvec[1]}(gen);
+    }
+    std::shared_ptr<pele::BasePotential> pot_c = std::make_shared<pele::LJCutPeriodicCellLists<ndim> >(4., 4., rcut, boxvec, 1);
+    std::shared_ptr<pele::BasePotential> pot_p = std::make_shared<LJCutPeriodicN<ndim> >(4., 4., rcut, boxvec);
+    EXPECT_DOUBLE_EQ(pot_c->get_energy(x), pot_p->get_energy(x));
+    Array<double> g_c(x.size());
+    auto g_p = g_c.copy();
+    Array<double> h_c(x.size() * x.size());
+    auto h_p = h_c.copy();
+    pot_p->get_energy_gradient(x, g_p);
+    pot_c->get_energy_gradient(x, g_c);
+    for (size_t i = 0; i < x.size(); ++i) {
+        EXPECT_NEAR_RELATIVE(g_p[i], g_c[i], 1e-10);
+    }
+    pot_p->get_energy_gradient_hessian(x, g_p, h_p);
+    pot_c->get_energy_gradient_hessian(x, g_c, h_c);
+    for (size_t i = 0; i < g_p.size(); ++i) {
+        EXPECT_NEAR_RELATIVE(g_p[i], g_c[i], 1e-10);
+    }
+    for (size_t i = 0; i < h_p.size(); ++i) {
+        EXPECT_NEAR_RELATIVE(h_p[i], h_c[i], 1e-10);
+    }
+    std::shared_ptr<pele::GradientOptimizer> optimizer_c = std::make_shared<pele::MODIFIED_FIRE>(pot_c, x, 1, 1, 1);
+    std::shared_ptr<pele::GradientOptimizer> optimizer_p = std::make_shared<pele::MODIFIED_FIRE>(pot_p, x, 1, 1, 1);
+    optimizer_c->run();
+    optimizer_p->run();
+    auto xo_p = optimizer_p->get_x();
+    auto xo_c = optimizer_c->get_x();
+    for (size_t i = 0; i < xo_p.size(); ++i) {
+        EXPECT_NEAR_RELATIVE(xo_p[i], xo_c[i], 1e-10);
+    }
+}
+
+TEST(LatticeNeighborsTest, RectangleWorks)
+{
+    test_rectangular_cell_lists<2>();
+    test_rectangular_cell_lists<3>();
 }
