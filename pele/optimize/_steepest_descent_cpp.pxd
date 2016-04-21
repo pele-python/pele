d5580bf8ee3e606bb0ecfd0c8688a39aaa9e52b0
Julian Schrenk <kjs@fastmail.com>
2016-04-21 14:52:56 +0100

    add gd python unittests

--- pele/optimize/_steepest_descent_cpp.pxd
+++ pele/optimize/_steepest_descent_cpp.pxd
@@ -1,3 +1,5 @@
+from libcpp cimport bool as cbool
+
 cimport pele.optimize._pele_opt as _pele_opt
 from pele.potentials cimport _pele
 from pele.potentials._pele cimport shared_ptr
@@ -5,4 +7,4 @@ from pele.potentials._pele cimport shared_ptr
 cdef extern from "pele/steepest_descent.h" namespace "pele":
     cdef cppclass cppSteepestDescent "pele::SteepestDescent":
         cppSteepestDescent(shared_ptr[_pele.cBasePotential],
-            _pele.Array[double], double, double) except+
+            _pele.Array[double], double, double, cbool) except+
--- pele/optimize/_steepest_descent_cpp.pyx
+++ pele/optimize/_steepest_descent_cpp.pyx
@@ -1,5 +1,6 @@
 # distutils: language = c++
 # distutils: sources = modified_fire.cpp
+from cpython cimport bool as cbool
 import numpy as np
 cimport numpy as np
 
@@ -8,13 +9,13 @@ from pele.potentials._pythonpotential import as_cpp_potential
 
 cdef class _Cdef_SteepestDescent(_pele_opt.GradientOptimizer):
     cdef _pele.BasePotential pot
-    def __cinit__(self, x0, potential, double eta=1, double tol=1e-4, size_t nsteps=10000):
+    def __cinit__(self, x0, potential, double eta=1e-1, double tol=1e-4, size_t nsteps=10000, cbool verbose=False):
         potential = as_cpp_potential(potential)
         cdef _pele.BasePotential pot = potential
         cdef np.ndarray[double, ndim=1] x0c = np.array(x0, dtype=float)
         self.thisptr = shared_ptr[_pele_opt.cGradientOptimizer](<_pele_opt.cGradientOptimizer*>
             new cppSteepestDescent(pot.thisptr, _pele.Array[double](<double*> x0c.data, x0c.size),
-            eta, tol))
+            eta, tol, verbose))
         self.thisptr.get().set_max_iter(nsteps)
         self.pot = pot
 
