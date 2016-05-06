71e313ae107ed1c7b2286f75cfd746a419961477
Julian Schrenk <kjs@fastmail.com>
2016-05-06 16:09:35 +0100

    add online xy potential, tests

--- cpp_tests/source/test_base_potential_online.cpp
+++ cpp_tests/source/test_base_potential_online.cpp
@@ -1,28 +1,82 @@
 #include <gtest/gtest.h>
 
+#include "pele/base_potential.h"
+#include "pele/steepest_descent.h"
 #include "pele/xy_model_online.h"
 
 class BasePotentialOnlineTest : public ::testing::Test {
 public:
     double etrue;
-    pele::Array<double> x, g, g2, gtrue, g2true;
+    pele::Array<double> x, g, gn, g2, gtrue;
+    pele::Array<size_t> head, tail;
     virtual void SetUp()
     {
-        x = {1, 1, 1, 1};
-        g = {1, 1, 1, 1};
-        g2 = {1, 1, 1, 1};
-        gtrue = {1, 1, 1, 1};
-        g2true = {0, 0, 0, 0};
-        etrue = 42;
+        head = {0, 0, 1};
+        tail = {1, 2, 2};
+        x = {M_PI, 0, 0, 0};
+        g = {42, 42, 42, 42};
+        gn = g.copy();
+        g2 = {42, 42, 42, 42};
+        gtrue = {0, 0, 0, 0};
+        etrue = 2;
     }
 };
 
-TEST_F(BasePotentialOnlineTest, OnlyEnergy_Works)
+TEST_F(BasePotentialOnlineTest, Basic_Works)
 {
-    pele::XYModelOnline pot(42);
-    /*
-    const double e = pot.get_energy(x);
-    EXPECT_EQ(1u, pot.call_count);
-    EXPECT_NEAR(e, etrue, 1e-10);
-    */
+    std::shared_ptr<pele::BasePotentialOnline> pot = std::make_shared<pele::XYModelOnline>(x.size(), head, tail);
+    const double e = pot->get_energy(x);
+    EXPECT_DOUBLE_EQ(e, etrue);
+    EXPECT_DOUBLE_EQ(pot->get_energy(x, 0), 2);
+    EXPECT_DOUBLE_EQ(pot->get_energy(x, 1), 0);
+    EXPECT_DOUBLE_EQ(pot->get_energy(x, 2), 0);
+    EXPECT_DOUBLE_EQ(pot->get_energy(x, 3), 0);
+    pot->get_energy_ogradient(x, 0, g);
+    pot->numerical_ogradient(x, 0, gn);
+    for (size_t i = 0; i < g.size(); ++i) {
+        EXPECT_NEAR(g[i], gtrue[i], 1e-10);
+        EXPECT_NEAR(gn[i], gtrue[i], 1e-10);
+    }
+}
+
+class BaseWrapE : public pele::BasePotential {
+    std::shared_ptr<pele::BasePotentialOnline> pot;
+public:
+    BaseWrapE(const size_t N, pele::Array<size_t> h, pele::Array<size_t> t)
+        : pot(std::make_shared<pele::XYModelOnline>(N, h, t))
+    {}
+    double get_energy(pele::Array<double> x)
+    {
+        return pot->get_energy(x);
+    }
+};
+
+TEST_F(BasePotentialOnlineTest, XY1D8_Works)
+{
+    pele::Array<size_t> h8 = {0, 0, 1, 2, 3, 4, 5, 6};
+    pele::Array<size_t> t8 = {7, 1, 2, 3, 4, 5, 6, 7};
+    std::shared_ptr<pele::BasePotentialOnline> pot = std::make_shared<pele::XYModelOnline>(8, h8, t8);
+    std::shared_ptr<pele::BasePotential> pot_batch = std::make_shared<BaseWrapE>(8, h8, t8);
+    x = pele::Array<double>(8);
+    // global minimum
+    x.assign(0);
+    EXPECT_NEAR(pot->get_energy(x), -16, 1e-10);
+    EXPECT_NEAR(pot_batch->get_energy(x), -16, 1e-10);
+    EXPECT_NEAR(pot->get_energy(x, 3), -2, 1e-10);
+    // local minimum
+    for (size_t i = 0; i < x.size(); ++i) {
+        x[i] = i * (M_PI / 4);
+    }
+    EXPECT_NEAR(pot->get_energy(x), -8 * std::sqrt(2), 1e-10);
+    // opimisation, deterministic (batch)
+    // TODO: add stochastic optimizer 1
+    // TODO: add stochastic optimizer 2
+    x = {0, 1, 2, 3, 4, 5, 6, 7};
+    pele::SteepestDescent optimizer_deterministic(pot_batch, x, 0.1, 1e-5);
+    optimizer_deterministic.run();
+    const pele::Array<double> x_deterministic_minimum = optimizer_deterministic.get_x();
+    EXPECT_NEAR(pot->get_energy(x_deterministic_minimum), -8 * std::sqrt(2), 1e-8);
+    for (size_t i = 1; i < x_deterministic_minimum.size(); ++i) {
+        EXPECT_NEAR(std::fabs(x_deterministic_minimum[i] - x_deterministic_minimum[i - 1]), M_PI / 4, 1e-5);
+    }
 }
--- source/pele/xy_model_online.h
+++ source/pele/xy_model_online.h
@@ -1,15 +1,206 @@
 #ifndef _PELE_XY_MODEL_ONLINE_H
 #define _PELE_XY_MODEL_ONLINE_H
 
+#include <fstream>
+#include <iostream>
+#include <vector>
+
 #include "pele/base_potential_online.h"
 
 namespace pele {
 
-class XYModelOnline : public BasePotentialOnline {
-public:
-    XYModelOnline(const size_t nr_spins)
-        : BasePotentialOnline(nr_spins)
+/**
+ * Adjacency list graph implementation according to S. S. Skiena,
+ * Algorithm Design Manual 
+ */
+
+struct edgenode {
+    const unsigned int y;
+    edgenode* next;
+    edgenode(const unsigned int y_, edgenode*const& next_)
+        : y(y_),
+          next(next_)
     {}
+};
+
+class graph {
+    std::vector<edgenode*> edges;
+    std::vector<unsigned int> degree;
+    unsigned int nvertices_;
+    unsigned int nedges_;
+    const bool directed_;
+public:
+    graph(const unsigned int maxv_=1000, const bool directed__=false)
+        : directed_(directed__)
+    {
+        resize(maxv_);
+    }
+    graph(const unsigned int nvertices_input, pele::Array<size_t> x,
+        pele::Array<size_t> y)
+        : directed_(false)
+    {
+        resize(nvertices_input);
+        loadvec(nvertices_input, x, y);
+    }
+    ~graph()
+    {
+        for (unsigned int i = 0; i < nvertices_; ++i) {
+            edgenode* p = edges.at(i);
+            while (p != NULL) {
+                edgenode* tmp = p;
+                p = p->next;
+                delete tmp;
+            }
+        }
+    }
+    void loadtxt(const char* name)
+    {
+        /**
+         * File format (number of lines == nedges + 1): 
+         * First Line: nvertices and nedges
+         * Following nedges lines: two numbers:
+         * 1. vertex A, 2. vertex B
+         */
+        std::ifstream inp(name);
+        unsigned int nvertices_file;
+        unsigned int nedges_file;
+        inp >> nvertices_file;
+        inp >> nedges_file;
+        std::cout << "number of vertices in file: " << nvertices_file << "\n";
+        std::cout << "number of edges in file: " << nedges_file << "\n";
+        for (unsigned int i = 0; i < nedges_file; ++i) {
+            unsigned int x;
+            unsigned int y;
+            inp >> x;
+            inp >> y;
+            std::cout << "insert edge (" << x << ", " << y << ")\n";
+            insert_edge(x, y, directed());
+        }
+        nvertices_ = nvertices_file;
+    }
+    void loadvec(const size_t nvertices_input, pele::Array<size_t> x, pele::Array<size_t> y)
+    {
+        if (x.size() != y.size()) {
+            throw std::runtime_error("graph: illegal input: x.size() != y.size()");
+        }
+        for (size_t i = 0; i < x.size(); ++i) {
+            insert_edge(x[i], y[i], directed());
+        }
+        if (nvertices_input > maxv()) {
+            throw std::runtime_error("graph: illegal input: too many vertices");
+        }
+        nvertices_ = nvertices_input;
+    }
+    void insert_edge(const unsigned int x, const unsigned int y, bool no_mirror)
+    {
+        edgenode* p = new edgenode(y, edges.at(x));
+        edges.at(x) = p;
+        ++degree.at(x);
+        if (!no_mirror) {
+            insert_edge(y, x, true);
+        }
+        else {
+            ++nedges_;
+        }
+    }
+    void resize(const unsigned int maxv_)
+    {
+        edges.assign(maxv_, NULL);
+        degree.assign(maxv_, 0);
+        nvertices_ = 0;
+        nedges_ = 0;
+    }
+    void print(std::ostream& stm)
+    {
+        stm << "number of vertices in graph: " << nvertices() << "\n";
+        stm << "number of edges in graph: " << nedges() << "\n";
+        stm << "adjacency list:\n";
+        for (unsigned int i = 0; i < nvertices_; ++i) {
+            stm << i << " ==>> ";
+            edgenode* p = edges.at(i);
+            unsigned int tmp = 0;
+            while (p != NULL) {
+                if (tmp++) {
+                    stm << " --> ";
+                }
+                stm << p->y;
+                p = p->next;
+            }
+            stm << "\n";
+        }
+    }
+    unsigned int nvertices() const
+    {
+        return nvertices_;
+    }
+    unsigned int nedges() const
+    {
+        return nedges_;
+    }
+    bool directed() const
+    {
+        return directed_;
+    }
+    unsigned int maxv() const
+    {
+        return edges.size();
+    }
+    edgenode* get_edges(const unsigned int v) const
+    {
+        return edges.at(v);
+    }
+};
+
+/**
+ * XY model implementation where the energy is computed by summing each
+ * linked list in the adjacency list vector in tun. 
+ */
+class XYModelOnline : public BasePotentialOnline {
+    graph m_topology;
+public:
+    XYModelOnline(const size_t nr_spins, pele::Array<size_t> head, pele::Array<size_t> tail)
+        : BasePotentialOnline(nr_spins),
+          m_topology(nr_spins, head, tail)
+    {}
+    double get_energy(Array<double> x, const size_t index)
+    {
+        /**
+         * Return ith term of the potential energy, i.e., the sum of 
+         * -cos(x[i] - x[j]) for all j which are linked to i
+         */
+        if (x.size() != m_topology.nvertices()) {
+            throw std::runtime_error("XYModelOnline: x.size() != nr vertices in graph");
+        }
+        double energy = 0;
+        edgenode* en = m_topology.get_edges(index);
+        while (en) {
+            energy += -std::cos(x[index] - x[en->y]);
+            en = en->next;
+        }
+        return energy;
+    }
+    double get_energy_ogradient(Array<double> x, const size_t index, Array<double> ograd)
+    {
+        /**
+         * Return ith term of potential energy as above. Compute
+         * gradient of that term too
+         */
+        if (x.size() != m_topology.nvertices()) {
+            throw std::runtime_error("XYModelOnline: x.size() != nr vertices in graph");
+        }
+        double energy = 0;
+        ograd.assign(0);
+        edgenode* en = m_topology.get_edges(index);
+        while (en) {
+            energy += -std::cos(x[index] - x[en->y]);
+            const double tmp = std::sin(x[index] - x[en->y]);
+            for (size_t k = 0; k < x.size(); ++k) {
+                ograd[k] += ((k == index) - (k == en->y)) * tmp;
+            }
+            en = en->next;
+        }
+        return energy;
+    }
 };
     
 } // namespace pele
