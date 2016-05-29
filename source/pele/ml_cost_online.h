e6f9e1d0a43f813c91ea289d44d0661e5640b02a
Julian Schrenk <kjs@fastmail.com>
2016-05-29 17:59:18 +0100

    change function name

--- source/pele/ml_cost_online.h
+++ source/pele/ml_cost_online.h
@@ -14,26 +14,14 @@ public:
         : BasePotentialOnline(data.size()),
           m_data(data.copy())
     {}
-    virtual double get_logp(const double datum, const Array<double>& parameters) const = 0;
+    virtual double get_log_probability(const double datum, const Array<double>& parameters) const = 0;
     double get_energy(Array<double> x, const size_t index)
     {
         if (index >= m_data.size()) {
             throw std::runtime_error("MLCostOnline: illegal index input");
         }
-        return -get_logp(m_data[index], x);
+        return -get_log_probability(m_data[index], x);
     }
-    /*
-    double get_energy_ogradient(Array<double> x, const size_t index,
-        Array<double> ograd)
-    {
-        if (x.size() != ograd.size()) {
-            throw std::runtime_error("MLCostOnline: illegal input x and grad size");
-        }
-        ograd.assign(0);
-        //////////
-        return BasePotentialOnline::get_energy(x);
-    }
-    */
 };
 
 class MLCostOnlineGauss : public MLCostOnline {
@@ -41,7 +29,7 @@ public:
     MLCostOnlineGauss(Array<double> data)
         : MLCostOnline(data)
     {}
-    double get_logp(const double datum, const Array<double>& parameters) const
+    double get_log_probability(const double datum, const Array<double>& parameters) const
     {
         return -0.5 * (std::log(2 * M_PI * parameters[1]) + pos_int_pow<2>(datum - parameters[0]) / parameters[1]);
     }
