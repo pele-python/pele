24102269a2c1a824b76cebb903a6c4881a0db14e
Johannes Klicpera <j.klicpera@mailbox.org>
2016-11-24 00:53:17 +0000

    Started implementing Lees-Edwards boundary conditions by adapting periodic_distance.

--- source/pele/distance.h
+++ source/pele/distance.h
@@ -76,7 +76,7 @@ struct cartesian_distance {
 */
 
 template<size_t IDX>
-struct meta_periodic_distance {
+struct  meta_periodic_distance {
     static void f(double * const r_ij, double const * const r1,
                  double const * const r2, const double* _box, const double* _ibox)
     {
@@ -164,6 +164,100 @@ public:
 
     inline void put_atom_in_box(double * const x) const
     {
+        meta_image<ndim>::f(x, _ibox, _box);
+    }
+    inline void put_in_box(Array<double>& coords) const
+    {
+        const size_t N = coords.size();
+        for (size_t i = 0; i < N; i += _ndim){
+            put_atom_in_box(&coords[i]);
+        }
+    }
+};
+
+/**
+* periodic boundary conditions in rectangular box
+*/
+
+template<size_t IDX>
+struct  meta_leesedwards_distance {
+    static void f(double * const r_ij, double const * const r1,
+                 double const * const r2, const double* _box, const double* _ibox)
+    {
+        const static size_t k = IDX - 1;
+        r_ij[k] = r1[k] - r2[k];
+        r_ij[k] -= round(r_ij[k] * _ibox[k]) * _box[k];
+        meta_leesedwards_distance<k>::f(r_ij, r1, r2, _box, _ibox);
+    }
+};
+
+template<>
+struct meta_leesedwards_distance<2> {
+    static void f(double * const r_ij, double const * const r1,
+                 double const * const r2, const double* _box, const double* _ibox)
+    {
+        r_ij[0] = r1[0] - r2[0];
+        r_ij[0] -= round(r_ij[0] * _ibox[0]) * _box[0];
+
+        // TODO: Check if x-dx and y-Y shorter than x and y
+        r_ij[1] = r1[1] - r2[1];
+        r_ij[1] -= round(r_ij[1] * _ibox[1]) * _box[1];
+    }
+};
+
+/**
+ * meta_image applies the nearest periodic image convention to the
+ * coordinates of one particle.
+ * In particular, meta_image is called by put_in_box once for each
+ * particle.
+ * x points to the first coodinate of the particle that should be put in
+ * the box.
+ * The template meta program expands to apply the nearest image
+ * convention to all ndim coordinates in the range [x, x + ndim).
+ * The function f of meta_image translates x[k] such that its new value
+ * is in the range [-box[k]/2, box[k]/2].
+ * To see this, consider the behavior of std::round.
+ * E.g. if the input is x[k] == -0.7 _box[k], then
+ * round(x[k] * _ibox[k]) == -1 and finally
+ * x[k] -= -1 * _box[k] == 0.3 * _box[k]
+ */
+template<size_t IDX>
+struct meta_image {
+    static void f(double *const x, const double* _ibox, const double* _box)
+    {
+        const static size_t k = IDX - 1;
+        x[k] -= round(x[k] * _ibox[k]) * _box[k];
+        meta_image<k>::f(x, _ibox, _box);
+    }
+};
+
+template<>
+struct meta_image<1> {
+    static void f(double *const x, const double* _ibox, const double* _box)
+    {
+        x[0] -= round(x[0] * _ibox[0]) * _box[0];
+    }
+};
+
+template<size_t ndim>
+class leesedwards_distance : periodic_distance { // TODO: Inheritance sensible?
+public:
+
+    leesedwards_distance(Array<double> const box)
+        : periodic_distance(box)
+    {
+        static_assert(ndim > 1, "box dimension must be greater 2 for lees-edwards");
+    }
+
+    inline void get_rij(double * const r_ij, double const * const r1,
+                 double const * const r2) const
+    {
+        meta_leesedwards_distance<ndim>::f(r_ij, r1, r2, _box, _ibox);
+    }
+
+    inline void put_atom_in_box(double * const x) const
+    {
+        //TODO: What is this?
         meta_image<ndim>::f(x, _ibox, _box);
     }
     inline void put_in_box(Array<double>& coords) const
