f6f9ff3b0ece19a414c8031b5fa36ce4995a2c1f
Johannes Klicpera <j.klicpera@mailbox.org>
2017-03-07 11:02:22 +0000

    Use integer values for the Distance enum.

--- pele/distance/distance_enum.py
+++ pele/distance/distance_enum.py
@@ -3,6 +3,6 @@ from enum import Enum, unique  # Package enum34
 
 @unique
 class Distance(Enum):
-    CARTESIAN = 'cartesian'
-    PERIODIC = 'periodic'
-    LEES_EDWARDS = 'lees-edwards'
+    CARTESIAN = 1
+    PERIODIC = 2
+    LEES_EDWARDS = 3
