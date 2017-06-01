c124073fdca36cf60f4fd060d87992e695342a8d
Johannes Klicpera <j.klicpera@mailbox.org>
2017-06-01 13:32:25 +0100

    Define function signatures in BaseInteraction

--- source/pele/base_interaction.h
+++ source/pele/base_interaction.h
@@ -5,6 +5,21 @@
 namespace pele {
 
 struct BaseInteraction {
+    virtual double energy(const double r2, const double radius_sum) const
+    {
+        throw std::runtime_error("BaseInteraction::energy must be overloaded");
+    }
+
+    virtual double energy_gradient(const double r2, double *const gij, const double radius_sum) const
+    {
+        throw std::runtime_error("BaseInteraction::energy must be overloaded");
+    }
+
+    virtual double energy_gradient_hessian(const double r2, double *const gij,
+            double *const hij, const double radius_sum) const
+    {
+        throw std::runtime_error("BaseInteraction::energy must be overloaded");
+    }
 };
 
 }
