6c18e12488ac051d7223260b51a4fde01dd4bbd8
James Daniel Farrell <j.d.farrell00@gmail.com>
2018-12-12 15:31:10 +0800

    redefined basestring for python3

--- pele/storage/database.py
+++ pele/storage/database.py
@@ -23,6 +23,23 @@ verbose=False
 
 Base = declarative_base()
 
+# the following taken from http://www.rfk.id.au/blog/entry/preparing-pyenchant-for-python-3/
+try:
+    unicode = unicode
+except NameError:
+    # 'unicode' is undefined, must be Python 3
+    str = str
+    unicode = str
+    bytes = bytes
+    basestring = (str,bytes)
+else:
+    # 'unicode' exists, must be Python 2
+    str = str
+    unicode = unicode
+    bytes = str
+    basestring = basestring
+
+
 class Minimum(Base):
     """
     The Minimum class represents a minimum in the database.
